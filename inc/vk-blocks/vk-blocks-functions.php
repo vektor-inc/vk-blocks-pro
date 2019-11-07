03da52343496d5b2ed4744bcb63bf6579085e250
kurudrive <kurudrive@gmail.com>
2019-11-08 00:53:49 +0900

    Beta表記削除

--- inc/vk-blocks/vk-blocks-functions.php
+++ inc/vk-blocks/vk-blocks-functions.php
@@ -188,12 +188,12 @@ if ( ! function_exists( 'vkblocks_blocks_categories' ) ) {
 			array(
 				array(
 					'slug'  => 'vk-blocks-cat',
-					'title' => $vk_blocks_prefix . __( 'Blocks（Beta）', 'vk-blocks' ),
+					'title' => $vk_blocks_prefix . __( 'Blocks', 'vk-blocks' ),
 					'icon'  => '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M19 13H5v-2h14v2z" /></svg>',
 				),
 				array(
 					'slug'  => 'vk-blocks-cat-layout',
-					'title' => $vk_blocks_prefix . __( 'Blocks Layout（Beta）', 'vk-blocks' ),
+					'title' => $vk_blocks_prefix . __( 'Blocks Layout', 'vk-blocks' ),
 					'icon'  => '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M19 13H5v-2h14v2z" /></svg>',
 				),
 			)

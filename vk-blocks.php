0bc52ab947fc2d5cc5e767ff66bea0dea9910b5e
kurudrive <kurudrive@gmail.com>
2019-11-08 00:40:35 +0900

    無料のVK Blocks停止処理

--- vk-blocks.php
+++ vk-blocks.php
@@ -21,3 +21,35 @@ add_action(
 		load_plugin_textdomain( 'vk-blocks', false, 'vk-blocks/inc/vk-blocks/build/languages' );
 	}
 );
+
+function vkblocks_deactivate_plugin( $plugin_path ) {
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	if ( is_plugin_active( $plugin_path ) ) {
+		$active_plugins = get_option( 'active_plugins' );
+		//delete item
+		$active_plugins = array_diff( $active_plugins, array( $plugin_path ) );
+		//re index
+		$active_plugins = array_values( $active_plugins );
+		update_option( 'active_plugins', $active_plugins );
+	}
+}
+
+/*-------------------------------------------*/
+/*	Deactive VK Blocks
+/*-------------------------------------------*/
+add_action( 'init', 'vkblocks_deactive_plugins' );
+function vkblocks_deactive_plugins() {
+
+	// Deactive Plugin VK Blocks
+	$plugin_path = 'vk-blocks/vk-blocks.php';
+	if ( function_exists( 'vkblocks_deactivate_plugin' ) ) {
+		vkblocks_deactivate_plugin( $plugin_path );
+	}
+
+	// Deactive ExUnit included VK Blocks
+	$options = get_option( 'vkExUnit_common_options' );
+	if ( ! empty( $options['active_vk-blocks'] ) ) {
+		$options['active_vk-blocks'] = false;
+		update_option( 'vkExUnit_common_options', $options );
+	}
+}

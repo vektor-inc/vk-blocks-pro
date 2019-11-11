03892e88fdd4884be8a38252088d25205add658d
naoki ohashi <n.globe.us@gmail.com>
2019-11-11 05:22:31 +0000

    vk-blocks.php を Bitbucket 上でオンラインで編集しました。

--- vk-blocks.php
+++ vk-blocks.php
@@ -60,7 +60,7 @@ function vkblocks_deactive_plugins() {
 if(file_exists(dirname(__FILE__).'/inc/plugin-update-checker/plugin-update-checker.php')){
 	require dirname(__FILE__).'/inc/plugin-update-checker/plugin-update-checker.php';
 	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-		'https://dev.vws.vektor-inc.co.jp/updates/?action=get_metadata&slug=vk-blocks-pro',
+		'https://vws.vektor-inc.co.jp/updates/?action=get_metadata&slug=vk-blocks-pro',
 		__FILE__,
 		'vk-blocks-pro'
 	);

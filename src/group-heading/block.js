1a0a0be828b76e403e27fd760b41bc72f810e368
goutetsuguma <goutetsuguma@gmail.com>
2019-11-26 16:19:08 +0900

    [グループブロック]枠にタイルを追加

--- src/group-heading/block.js
+++ src/group-heading/block.js
@@ -1,7 +1,22 @@
 /**
- * list-style block type
+ * group-heading block type
  *
  */
+const {assign} = lodash;
+const {__} = wp.i18n;
+const {Fragment} = wp.element;
+const {addFilter} = wp.hooks;
+const {
+    PanelBody,
+} = wp.components;
+
+const {
+    InspectorControls,
+    ColorPalette
+} = wp.blockEditor;
+
+const {createHigherOrderComponent} = wp.compose;
+
 
 wp.blocks.registerBlockStyle('core/group',
     [
@@ -11,7 +26,31 @@ wp.blocks.registerBlockStyle('core/group',
             isDefault: true
         },
         {
-            name: 'chevron-mark',
-            label: __('Chevron', 'vk-blocks'),
-        }
+            name: 'solid',
+            label: __('Solid', 'vk-blocks')
+        },
+        {
+            name: 'dotted',
+            label: __('Dotted', 'vk-blocks')
+        },
+        {
+            name: 'dashed',
+            label: __('Dashed', 'vk-blocks')
+        },
+        {
+            name: 'double',
+            label: __('Double', 'vk-blocks')
+        },       
+        {
+            name: 'stitch',
+            label: __('Stitch', 'vk-blocks')
+        },
+        {
+            name: 'top-bottom-border',
+            label: __('top-bottom-border', 'vk-blocks')
+        },
+        {
+            name: 'shadow',
+            label: __('Shadow', 'vk-blocks')
+        }  
     ]);

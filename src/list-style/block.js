2c23985f3230c2a2c972236857f524c0fd9bf247
naoki <n.globe.us@gmail.com>
2019-11-27 11:18:02 +0900

    split color-code-class to the file

--- src/list-style/block.js
+++ src/list-style/block.js
@@ -16,6 +16,8 @@ const {
 } = wp.blockEditor;
 
 const {createHigherOrderComponent} = wp.compose;
+import {convertColorClass} from "../_helper/color-code-to-class.js";
+
 
 const isValidBlockType = (name) => {
     const validBlockTypes = [
@@ -36,46 +38,6 @@ export const addAttribute = (settings) => {
     return settings;
 };
 addFilter('blocks.registerBlockType', 'vk-blocks/list-style', addAttribute);
-
-export const convertColorClass = (color) => {
-    switch (color) {
-        case '#f78da7':
-            return 'vk-has-pale-pink-color';
-
-        case '#cf2e2e':
-            return 'vk-has-vivid-red-color';
-
-        case '#ff6900':
-            return 'vk-has-luminous-vivid-orange-color';
-
-        case '#fcb900':
-            return 'vk-has-luminous-vivid-amber-color';
-
-        case '#7bdcb5':
-            return 'vk-has-light-green-cyan-color';
-
-        case '#00d084':
-            return 'vk-has-vivid-green-cyan-color';
-
-        case '#8ed1fc':
-            return 'vk-has-pale-cyan-blue-color';
-
-        case '#0693e3':
-            return 'vk-has-vivid-cyan-blue-color';
-
-        case '#9b51e0':
-            return 'vk-has-vivid-purple-color';
-
-        case '#eee':
-            return 'vk-has-very-light-gray-color';
-
-        case '#abb8c3':
-            return 'vk-has-cyan-bluish-gray-color';
-
-        case '#313131':
-            return 'vk-has-very-dark-gray-color';
-    }
-};
 
 export const addBlockControl = createHigherOrderComponent((BlockEdit) => {
 

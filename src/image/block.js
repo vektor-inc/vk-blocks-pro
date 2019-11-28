05cefeccb0d857d30edf8de67737beb85eac0a6e
kurudrive <kurudrive@gmail.com>
2019-11-28 14:00:56 +0900

    [ 画像 ] 枠 & フォトフレーム追加

--- src/image/block.js
+++ src/image/block.js
@@ -1,4 +1,12 @@
-wp.blocks.registerBlockStyle( 'core/image', {
-    name: 'vk-image-border',
-    label: 'Border'
-} );
+wp.blocks.registerBlockStyle('core/image',
+[
+	{
+		name: 'vk-image-border',
+		label: 'Border'
+	},
+	{
+		name: 'vk-image-photoFrame',
+		label: 'Photo frame'
+	},
+]
+);

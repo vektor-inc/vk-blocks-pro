b34bc530f4f9d4b8d212c5bf66ae0405563d9d7e
goutetsuguma <goutetsuguma@gmail.com>
2019-11-29 10:50:47 +0900

    [見出し]キーカラー変更機能

--- inc/vk-blocks/functions-color.php
+++ inc/vk-blocks/functions-color.php
@@ -3,8 +3,8 @@
 //ハンドル名登録
 function heading_style_method() {
     wp_enqueue_style(
-        'heading-style',
-        get_template_directory_uri() . '/src/heading-style/style.css'
+        'heading_style',
+        get_template_directory_uri() 
     );
 }
 add_action( 'wp_enqueue_scripts', 'heading_style_method' );
@@ -35,12 +35,11 @@ function lightning_print_css_vk_heading_style() {
 		.is-style-vk-heading-solid_bottomborder_leftkeycolor::after{ border-bottom-color:' . $color_key . ';}
 		.is-style-vk-heading-leftborder,
 		.is-style-vk-heading-leftborder_nobackground{ border-left-color:' . $color_key . ';}
-		.is-style-vk-heading-diagonal_stripe_bottomborder::after{background: linear-gradient(-45deg,rgba(255,255,255,0.1) 25%, ' . $color_key . ' 25%,' . $color_key . ' 50%, rgba(255,255,255,0.1) 50%,rgba(255,255,255,0.1) 75%, ' . $color_key . ' 75%,' . $color_key . '); background-size: 5px 5px;}
+		.is-style-vk-heading-diagonal_stripe_bottomborder::after{background: linear-gradient(-45deg,rgba(255,255,255,0.1) 25%, ' . $color_key . ' 25%,' . $color_key . ' 50%, rgba(255,255,255,0.1) 50%,rgba(255,255,255,0.1) 75%, ' . $color_key . ' 75%,' . $color_key . ');background-size: 5px 5px; }
 		.is-style-vk-heading-brackets::before{ border-top-color:' . $color_key . '; border-bottom-color:' . $color_key . '; border-left-color:' . $color_key . '; }
 		.is-style-vk-heading-brackets::after{ border-top-color:' . $color_key . '; border-bottom-color:' . $color_key . '; border-right-color:' . $color_key . '; }
 		';
 
-var_dump($color_key);
 
 		// delete before after space
 		$dynamic_css = trim( $dynamic_css );
@@ -48,6 +47,8 @@ var_dump($color_key);
 		$dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
 		// Change multiple spaces to single space
 		$dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
-		wp_add_inline_style( 'heading-style', $dynamic_css );
+		wp_add_inline_style( 'heading_style', $dynamic_css );
+
+		
 	} // if ( isset($options['color_key'] && isset($options['color_key_dark'] ) {
 }

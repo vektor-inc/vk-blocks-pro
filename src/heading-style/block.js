a240635b90d6de988d762a07e80d11f3601a9a92
kurudrive <kurudrive@gmail.com>
2019-12-03 15:52:01 +0900

    [ headding-style ] 色指定の見出しデザインを一旦コメントアウト

--- src/heading-style/block.js
+++ src/heading-style/block.js
@@ -40,107 +40,107 @@ addFilter('blocks.registerBlockType', 'vk-blocks/heading-style', addAttribute);
 
 
 
-wp.blocks.registerBlockStyle( 'core/heading', 
+wp.blocks.registerBlockStyle( 'core/heading',
 	[
         {
             name: 'vk-heading-default',
             label: __('Default', 'vk-blocks'),
             isDefault:true
-        },	
+        },
 		{
 			name: 'vk-heading-plain',
 			label: __('Plain', 'vk-blocks')
 		},
-		{
-			name: 'vk-heading-speech_balloon_fill',
-			label: __('Speech_balloon_fill', 'vk-blocks')
-		},
-		{
-			name: 'vk-heading-background_fill',
-			label: __('Background_fill', 'vk-blocks')
-		},
-		{
-			name: 'vk-heading-background_fill_stitch',
-			label: __('Background_fill_stitch', 'vk-blocks')
-		},
+		// {
+		// 	name: 'vk-heading-speech_balloon_fill',
+		// 	label: __('Speech balloon fill', 'vk-blocks')
+		// },
+		// {
+		// 	name: 'vk-heading-background_fill',
+		// 	label: __('Background fill', 'vk-blocks')
+		// },
+		// {
+		// 	name: 'vk-heading-background_fill_stitch',
+		// 	label: __('Background fill stitch', 'vk-blocks')
+		// },
 		{
 			name: 'vk-heading-background_fill_lightgray',
-			label: __('Background_fill_lightgray', 'vk-blocks')
+			label: __('Background fill lightgray', 'vk-blocks')
 		},
-		{
-			name: 'vk-heading-topborder_background_fill_none',
-			label: __('Topborder_background_fill_none', 'vk-blocks')
-		},
-		{
-			name: 'vk-heading-topborder_background_fill_black',
-			label: __('Topborder_background_fill_black', 'vk-blocks')
-		},
-		{
-			name: 'vk-heading-double',
-			label: __('Double', 'vk-blocks')
-		},
+		// {
+		// 	name: 'vk-heading-topborder_background_fill_none',
+		// 	label: __('Topborder background fill none', 'vk-blocks')
+		// },
+		// {
+		// 	name: 'vk-heading-topborder_background_fill_black',
+		// 	label: __('Topborder background fill black', 'vk-blocks')
+		// },
+		// {
+		// 	name: 'vk-heading-double',
+		// 	label: __('Double', 'vk-blocks')
+		// },
 		{
 			name: 'vk-heading-double_black',
-			label: __('Double_black', 'vk-blocks')
+			label: __('Double black', 'vk-blocks')
 		},
-		{
-			name: 'vk-heading-double_bottomborder',
-			label: __('Double_bottomborder', 'vk-blocks')
-		},
+		// {
+		// 	name: 'vk-heading-double_bottomborder',
+		// 	label: __('Double border-bottom', 'vk-blocks')
+		// },
 		{
 			name: 'vk-heading-double_bottomborder_black',
-			label: __('Double_bottomborder_black', 'vk-blocks')
+			label: __('Double border-bottom black', 'vk-blocks')
 		},
-		{
-			name: 'vk-heading-solid',
-			label: __('Solid', 'vk-blocks')
-		},
+		// {
+		// 	name: 'vk-heading-solid',
+		// 	label: __('Solid', 'vk-blocks')
+		// },
 		{
 			name: 'vk-heading-solid_black',
-			label: __('Solid_black', 'vk-blocks')
+			label: __('Solid black', 'vk-blocks')
 		},
-		{
-			name: 'vk-heading-solid_bottomborder',
-			label: __('Solid_bottomborder', 'vk-blocks')
-		},
+		// {
+		// 	name: 'vk-heading-solid_bottomborder',
+		// 	label: __('Solid border-bottom', 'vk-blocks')
+		// },
 		{
 			name: 'vk-heading-solid_bottomborder_black',
-			label: __('Solid_bottomborder_black', 'vk-blocks')
+			label: __('Solid border-bottom black', 'vk-blocks')
 		},
-		{
-			name: 'vk-heading-solid_bottomborder_leftkeycolor',
-			label: __('Solid_bottomborder_leftkeycolor', 'vk-blocks')
-		},
+		// {
+		// 	name: 'vk-heading-solid_bottomborder_leftkeycolor',
+		// 	label: __('Solid border-bottom border-left-keycolor', 'vk-blocks')
+		// },
 		{
 			name: 'vk-heading-dotted_bottomborder_black',
-			label: __('Dotted_bottomborder_black', 'vk-blocks')
+			label: __('Dotted border-bottom black', 'vk-blocks')
 		},
 		{
 			name: 'vk-heading-both_ends',
-			label: __('Both_ends', 'vk-blocks')
+			label: __('Both ends', 'vk-blocks')
 		},
-		{
-			name: 'vk-heading-leftborder',
-			label: __('Leftborder', 'vk-blocks')
-		},
-		{
-			name: 'vk-heading-leftborder_nobackground',
-			label: __('Leftborder_nobackground', 'vk-blocks')
-		},
- 		{
-			name: 'vk-heading-diagonal_stripe_bottomborder',
-			label: __('Diagonal_stripe_bottomborder', 'vk-blocks')
-		},
-  		{
-			name: 'vk-heading-brackets',
-			label: __('Brackets', 'vk-blocks')
-		},
+		// {
+		// 	name: 'vk-heading-leftborder',
+		// 	label: __('Border-left', 'vk-blocks')
+		// },
+		// {
+		// 	name: 'vk-heading-leftborder_nobackground',
+		// 	label: __('Border-left nobackground', 'vk-blocks')
+		// },
+ 		// {
+		// 	name: 'vk-heading-diagonal_stripe_bottomborder',
+		// 	label: __('Diagonal stripe border-bottom', 'vk-blocks')
+		// },
+  	// 	{
+		// 	name: 'vk-heading-brackets',
+		// 	label: __('Brackets', 'vk-blocks')
+		// },
   		{
 			name: 'vk-heading-brackets_black',
-			label: __('Brackets_black', 'vk-blocks')
+			label: __('Brackets black', 'vk-blocks')
 		},
-  		{
-			name: 'vk-heading-small_bottomborder',
-			label: __('Small_bottomborder', 'vk-blocks')
-		},
+  	// 	{
+		// 	name: 'vk-heading-small_bottomborder',
+		// 	label: __('Small border-bottom', 'vk-blocks')
+		// },
     ]);

84db4305739ae8b88a6cde805d86fd6d586fd44a
naoki <n.globe.us@gmail.com>
2020-01-09 14:03:31 +0900

    Fix undefined index name problem

--- inc/vk-blocks/view/post-list.php
+++ inc/vk-blocks/view/post-list.php
@@ -12,7 +12,13 @@ class VkBlocksPostList {
 	 */
 	public function render_post_list( $attributes ) {
 
-		$name = esc_html( $attributes['name'] );
+
+		if(isset($attributes['name'])){
+			$name = esc_html( $attributes['name'] );
+		}else{
+			return '<div>' . __("I'm sorry, This block is broken. Please remove this and reinsert it.", 'vk-blocks') . '</div>';
+		}
+
 		if ( $name === 'vk-blocks/post-list' ) {
 			$wp_query = $this->get_loop_query( $attributes ); 
 		} elseif ( $name === 'vk-blocks/child-page' ) {

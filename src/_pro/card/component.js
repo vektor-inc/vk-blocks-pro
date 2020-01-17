eee3beff0867065408c28e25a0f760c5ebd34c36
naoki <n.globe.us@gmail.com>
2020-01-17 16:13:08 +0900

    Add function to convert col number to grid number

--- src/_pro/card/component.js
+++ src/_pro/card/component.js
@@ -5,25 +5,47 @@ export class Component extends React.Component {
   render() {
     let for_ = this.props.for_;
     let attributes = this.props.attributes;
-    let innerClass = '';
+    let innerClass = "";
     let className = this.props.className;
     let containerClass = " vk_card";
     let elm;
     const ALLOWED_BLOCKS = ["vk-blocks/card-item"];
     const TEMPLATE = [ALLOWED_BLOCKS];
 
+    const convertToGrid = colNum => {
+      return 12 / colNum;
+    };
+
     //編集画面とサイト上の切り替え
     if (for_ === "edit") {
+      innerClass = "editting";
+      innerClass = innerClass + " vk_posts-edit";
+      innerClass =
+        innerClass +
+        " vk_posts-edit-col-xs-" +
+        convertToGrid(attributes.col_xs);
+      innerClass =
+        innerClass +
+        " vk_posts-edit-col-sm-" +
+        convertToGrid(attributes.col_sm);
+      innerClass =
+        innerClass +
+        " vk_posts-edit-col-md-" +
+        convertToGrid(attributes.col_md);
+      innerClass =
+        innerClass +
+        " vk_posts-edit-col-lg-" +
+        convertToGrid(attributes.col_lg);
+      innerClass =
+        innerClass +
+        " vk_posts-edit-col-xl-" +
+        convertToGrid(attributes.col_xl);
 
-      innerClass = 'editting';
-      innerClass = innerClass + ' vk_posts-edit';
-      innerClass = innerClass + ' vk_posts-edit-col-xs-' + attributes.col_xs;
-      innerClass = innerClass + ' vk_posts-edit-col-sm-' + attributes.col_sm;
-      innerClass = innerClass + ' vk_posts-edit-col-md-' + attributes.col_md;
-      innerClass = innerClass + ' vk_posts-edit-col-lg-' + attributes.col_lg;
-      innerClass = innerClass + ' vk_posts-edit-col-xl-' + attributes.col_xl;
-
-      elm = <div className={innerClass}><InnerBlocks template={TEMPLATE} allowedBlocks={ALLOWED_BLOCKS} /></div>;
+      elm = (
+        <div className={innerClass}>
+          <InnerBlocks template={TEMPLATE} allowedBlocks={ALLOWED_BLOCKS} />
+        </div>
+      );
     } else if ("save") {
       elm = <InnerBlocks.Content />;
     }

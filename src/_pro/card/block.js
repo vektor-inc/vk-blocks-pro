ff2f29c11952829022bdc5f56b9e5689a22707d2
naoki <n.globe.us@gmail.com>
2020-02-04 11:30:50 +0900

    Enable to add reuseable block

--- src/_pro/card/block.js
+++ src/_pro/card/block.js
@@ -107,7 +107,8 @@ registerBlockType("vk-blocks/card", {
     let afterLength;
 
     if (
-      currentBlock !== undefined ||
+      currentBlock !== undefined &&
+      currentBlock[0] !== null &&
       currentBlock[0].innerBlocks !== undefined
     ) {
       let innerBlocks = currentBlock[0].innerBlocks;

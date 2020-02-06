6ea38162417e1adb99c80719e4ebbe282a9aeecf
naoki <n.globe.us@gmail.com>
2020-02-06 16:33:14 +0900

    temp

--- plugin/menu/block-template-panel.js
+++ plugin/menu/block-template-panel.js
@@ -1,4 +1,4 @@
-"use strict";
+import { Fragment } from "react";
 
 // import ScreenshotImg from "./screenshot-img";
 
@@ -6,137 +6,141 @@ const { first, last } = window.lodash;
 
 const { parse } = wp.blocks;
 
-const { Button, Spinner } = wp.components;
+const { Button, Spinner, BlockPreview } = wp.components;
 
-const { useState } = wp.element;
+const { useState, useMemo } = wp.element;
 
 const { dispatch, select } = wp.data;
+
+const { insertBlocks, replaceBlocks, multiSelect } = dispatch("core/editor");
+
+const {
+  getBlocks,
+  getBlockCount,
+  getSelectedBlock,
+  getBlockInsertionPoint
+} = select("core/block-editor");
 
 export const vkbFetchReuseableBlocks = () => {
   return wp.apiFetch({ path: `/wp/v2/blocks?per_page=-1` });
 };
 
-export default function({ slug }) {
-  const [parts, setParts] = useState(null);
-  const [resultParts, setResultParts] = useState(null);
+export default ({ slug }) => {
+  // const [parts, setParts] = useState(null);
+  // const [resultParts, setResultParts] = useState(null);
 
-  const setupParts = () => {
-    if (parts) {
-      return;
-    }
+  // const setupParts = () => {
+  //   if (parts) {
+  //     return;
+  //   }
 
-    vkbFetchReuseableBlocks().then(result => {
-      console.log(result);
-      const filterd = result.filter(value => {
-        return value.custom_fields.is_registerd_vkb_template[0] === "1";
-      });
-      console.log(filterd);
-      setParts(filterd);
-    });
-  };
+  //   vkbFetchReuseableBlocks().then(result => {
+  //     console.log(result);
+  //     const filterd = result.filter(value => {
+  //       return value.custom_fields.is_registerd_vkb_template[0] === "1";
+  //     });
+  //     console.log(filterd);
+  //     setParts(filterd);
+  //   });
+  // };
 
-  const setupResultParts = () => {
-    if (resultParts) {
-      return;
-    }
+  // const advancedInsertBlock = insertedBlocks => {
+  //   if (insertedBlocks.length) {
+  //     const selectedBlock = getSelectedBlock();
+  //     if (null === selectedBlock) {
+  //       const lastRootBlock = last(getBlocks());
+  //       const isEmpty =
+  //         undefined !== lastRootBlock &&
+  //         null === lastRootBlock.rootClientId &&
+  //         (!getBlockCount(lastRootBlock.clientId) ||
+  //           ("core/paragraph" === lastRootBlock.name &&
+  //             "" === lastRootBlock.attributes.content));
+  //       if (isEmpty) {
+  //         replaceBlocks(lastRootBlock.clientId, insertedBlocks);
+  //       } else {
+  //         insertBlocks(insertedBlocks);
+  //       }
+  //     } else {
+  //       const isEmpty =
+  //         "core/paragraph" === selectedBlock.name &&
+  //         "" === selectedBlock.attributes.content;
+  //       if (!isEmpty) {
+  //         const insertionPoint = getBlockInsertionPoint();
+  //         insertBlocks(
+  //           insertedBlocks,
+  //           insertionPoint.index,
+  //           insertionPoint.rootClientId
+  //         );
+  //       } else {
+  //         replaceBlocks(selectedBlock.clientId, insertedBlocks);
+  //       }
+  //     }
+  //     multiSelect(
+  //       first(insertedBlocks).clientId,
+  //       last(insertedBlocks).clientId
+  //     );
+  //   }
+  // };
 
-    const { insertBlocks, replaceBlocks, multiSelect } = dispatch(
-      "core/editor"
-    );
+  // const setupResultParts = () => {
+  //   if (resultParts) {
+  //     return;
+  //   }
 
-    const {
-      getBlocks,
-      getBlockCount,
-      getSelectedBlock,
-      getBlockInsertionPoint
-    } = select("core/block-editor");
+  //   setupParts();
+  //   if (!parts) {
+  //     return;
+  //   }
 
-    setupParts();
-    if (!parts) {
-      return;
-    }
+  //   const newResultParts = parts.map(part => {
+  //     // const parsedBlock = parse(part.content.raw);
+  //     const parsedBlock = parse(`<!-- wp:paragraph -->
+  //       <p><a href="tel:5555551234">(555) 555 1234</a></p>
+  //       <!-- /wp:paragraph -->`);
+  //     console.log(parsedBlock);
+  //     return (
+  //       <li>
+  //         <Button
+  //           className="vkb-menu__template-part__button"
+  //           // onClick={advancedInsertBlock(parsedBlock)}
+  //         >
+  //           {/* <BlockPreview blocks={parsedBlock} /> */}
+  //           {/* <ScreenshotImg
+  //             className="vkb-menu__template-part__button__screenshot"
+  //             src={part.screenshot}
+  //             loader={
+  //               <div className="vkb-menu__template-part__button__screenshot__loading">
+  //                 <Spinner />
+  //               </div>
+  //             }
+  //           /> */}
+  //           <span className="vkb-menu__template-part__button__title">
+  //             {part.title.raw}
+  //           </span>
+  //         </Button>
+  //       </li>
+  //     );
+  //   });
+  //   setResultParts(newResultParts.filter(resultPart => resultPart));
+  // };
 
-    const newResultParts = parts.map(part => {
-      return (
-        <li>
-          <Button
-            className="smb-menu__template-part__button"
-            onClick={() => {
-              const parsedBlocks = parse(part.content.raw);
-              if (parsedBlocks.length) {
-                const selectedBlock = getSelectedBlock();
-                if (null === selectedBlock) {
-                  // when not selected block
-                  // get last root block
-                  const lastRootBlock = last(getBlocks());
-                  const isEmpty =
-                    undefined !== lastRootBlock &&
-                    null === lastRootBlock.rootClientId &&
-                    (!getBlockCount(lastRootBlock.clientId) ||
-                      ("core/paragraph" === lastRootBlock.name &&
-                        "" === lastRootBlock.attributes.content));
-                  if (isEmpty) {
-                    // Replace when last block is empty
-                    replaceBlocks(lastRootBlock.clientId, parsedBlocks);
-                  } else {
-                    // Insert at the end when block is not empty
-                    insertBlocks(parsedBlocks);
-                  }
-                } else {
-                  // when selected block
-                  // isEmpty is true when blocktype is paragraph and content is empty
-                  const isEmpty =
-                    "core/paragraph" === selectedBlock.name &&
-                    "" === selectedBlock.attributes.content;
-                  if (!isEmpty) {
-                    // Insert after block
-                    const insertionPoint = getBlockInsertionPoint();
-                    insertBlocks(
-                      parsedBlocks,
-                      insertionPoint.index,
-                      insertionPoint.rootClientId
-                    );
-                  } else {
-                    // Replace at the block when block is empty
-                    replaceBlocks(selectedBlock.clientId, parsedBlocks);
-                  }
-                }
-                multiSelect(
-                  first(parsedBlocks).clientId,
-                  last(parsedBlocks).clientId
-                );
-              }
-            }}
-          >
-            {/* <ScreenshotImg
-              className="smb-menu__template-part__button__screenshot"
-              src={part.screenshot}
-              loader={
-                <div className="smb-menu__template-part__button__screenshot__loading">
-                  <Spinner />
-                </div>
-              }
-            /> */}
-            <span className="smb-menu__template-part__button__title">
-              {part.title.raw}
-            </span>
-          </Button>
-        </li>
-      );
-    });
+  // setupResultParts();
 
-    setResultParts(newResultParts.filter(resultPart => resultPart));
-  };
+  // if (resultParts) {
+  //   return <ul>{resultParts}</ul>;
+  // }
 
-  setupResultParts();
-
-  if (resultParts) {
-    return <ul>{resultParts}</ul>;
-  }
+  const parsedBlock = parse(`<!-- wp:paragraph -->
+        <p><a href="tel:5555551234">(555) 555 1234</a></p>
+        <!-- /wp:paragraph -->`);
+  console.log(parsedBlock);
+  const renderedBlocks = useMemo(() => castArray(parsedBlock), [parsedBlock]);
+  console.log(renderedBlocks);
 
   return (
-    <div className="smb-menu__template-part__loading">
-      <Spinner />
-    </div>
+    <BlockPreview blocks={renderedBlocks} />
+    // <div className="vkb_menu__template-part__loading">
+    //   <Spinner />
+    // </div>
   );
-}
+};

d40fac8f4759cc37c8ea25b4bf7cc22805f0d30e
naoki <n.globe.us@gmail.com>
2020-03-02 11:50:57 +0900

    temp

--- src/blocks/_helper/checkbox.js
+++ src/blocks/_helper/checkbox.js
@@ -1,41 +1,47 @@
-'use strict';
-const {CheckboxControl} = wp.components;
+"use strict";
+const { CheckboxControl } = wp.components;
 
 /**
  *
  * @param args
  * @returns {*}
  */
-const addCheckBox = (args) => {
+const addCheckBox = args => {
+  if (!args.originData) {
+    return false;
+  }
 
-    if (!args.originData) {
-        return false
+  const name = args.name;
+  const originData = args.originData;
+  const checkedData = args.checkedData;
+  const setAttributes = args.setAttributes;
+  const reactDomToRender = [];
+
+  //投げるデータによりiは,数字やオブジェクトに変わる。
+  for (let i in originData) {
+    switch (name) {
+      case "postTypes":
+        renderPostTypes(
+          reactDomToRender,
+          originData[i].name,
+          originData[i].slug,
+          checkedData,
+          setAttributes
+        );
+        break;
+
+      case "taxonomy":
+        renderTaxonomy(
+          reactDomToRender,
+          i,
+          originData[i],
+          checkedData,
+          setAttributes
+        );
+        break;
     }
-
-    const name = args.name;
-    const originData = args.originData;
-    const checkedData = args.checkedData;
-    const setAttributes = args.setAttributes;
-    const reactDomToRender = [];
-
-    //投げるデータによりiは,数字やオブジェクトに変わる。
-    for (let i in originData) {
-
-        switch (name) {
-            case 'postTypes':
-                renderPostTypes(reactDomToRender, originData[i].name, originData[i].slug, checkedData, setAttributes);
-                break;
-
-            case 'taxonomy':
-                renderTaxonomy(reactDomToRender, i, originData[i], checkedData, setAttributes);
-                break;
-        }
-    }
-    return (
-        <ul>
-            {reactDomToRender}
-        </ul>
-    );
+  }
+  return <ul>{reactDomToRender}</ul>;
 };
 
 export default addCheckBox;
@@ -49,55 +55,66 @@ export default addCheckBox;
  * @param setAttributes
  * @returns {*}
  */
-const renderPostTypes = (reactDomToRender, label, slug, checkedData, setAttributes) => {
+const renderPostTypes = (
+  reactDomToRender,
+  label,
+  slug,
+  checkedData,
+  setAttributes
+) => {
+  if (!Array.isArray(checkedData)) {
+    return false;
+  }
 
-    if (!Array.isArray(checkedData)) {
-        return false
-    }
-
-    if (checkedData)
-        return (reactDomToRender.push(
-            <CheckboxControl
-                label={label}
-                checked={checkedData.some(item => item === slug)}
-                onChange={async (value) => {
-                    if (value) {
-                        checkedData.push(slug);
-                    } else {
-                        checkedData = checkedData.filter(elm => elm !== slug);
-                    }
-                    setAttributes({isCheckedPostType: JSON.stringify(checkedData)});
-                }}
-            />));
+  if (checkedData)
+    return reactDomToRender.push(
+      <CheckboxControl
+        label={label}
+        checked={checkedData.some(item => item === slug)}
+        onChange={async value => {
+          if (value) {
+            checkedData.push(slug);
+          } else {
+            checkedData = checkedData.filter(elm => elm !== slug);
+          }
+          setAttributes({ isCheckedPostType: JSON.stringify(checkedData) });
+        }}
+      />
+    );
 };
 
-const renderTaxonomy = (reactDomToRender, tax, slug, checkedData, setAttributes) => {
+const renderTaxonomy = (
+  reactDomToRender,
+  tax,
+  slug,
+  checkedData,
+  setAttributes
+) => {
+  if (checkedData[tax] === undefined) {
+    checkedData[tax] = [];
+  }
 
-    if (checkedData[tax] === undefined) {
-        checkedData[tax] = [];
-    }
+  if (slug) {
+    slug.forEach(term => {
+      let slug = term[0];
+      let label = term[1];
 
-    if(slug){
-        slug.forEach(term => {
+      reactDomToRender.push(
+        <CheckboxControl
+          label={label}
+          checked={checkedData[tax].some(termItem => termItem === slug)}
+          onChange={value => {
+            if (value) {
+              checkedData[tax].push(slug);
+            } else {
+              checkedData[tax] = checkedData[tax].filter(elm => elm !== slug);
+            }
+            setAttributes({ isCheckedTerms: JSON.stringify(checkedData) });
+          }}
+        />
+      );
+    });
+  }
 
-            let slug = term[0];
-            let label = term[1];
-
-            reactDomToRender.push(
-                <CheckboxControl
-                    label={label}
-                    checked={checkedData[tax].some(termItem => termItem === slug)}
-                    onChange={(value) => {
-                        if (value) {
-                            checkedData[tax].push(slug);
-                        } else {
-                            checkedData[tax] = checkedData[tax].filter(elm => elm !== slug);
-                        }
-                        setAttributes({isCheckedTerms: JSON.stringify(checkedData)});
-                    }}
-                />)
-        })
-    }
-
-    return (reactDomToRender);
+  return reactDomToRender;
 };

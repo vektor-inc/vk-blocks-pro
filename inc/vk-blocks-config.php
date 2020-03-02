41682a09b9910df708478536d27d8deb8e4e3872
kurudrive <kurudrive@gmail.com>
2020-03-02 18:14:23 +0900

    phpcs

--- inc/vk-blocks-config.php
+++ inc/vk-blocks-config.php
@@ -1,7 +1,10 @@
 <?php
-/*-------------------------------------------*/
-/*  Load modules
-/*-------------------------------------------*/
+/*
+-------------------------------------------*/
+/*
+  Load modules
+/*
+-------------------------------------------*/
 // このファイルは基本 /vk-blocks/vk-blocks-functions.phpを読み込むファイルだけ
 // vk-blocks
 
@@ -15,18 +18,19 @@ if ( ! function_exists( 'vkblocks_active' ) ) {
 	global $vk_blocks_prefix;
 	$vk_blocks_prefix = apply_filters( 'vk_blocks_prefix', 'VK ' );
 
-	require_once( 'vk-blocks/helpers.php' );
-	require_once( 'vk-components/vk-components-config.php' );
-	require_once( 'vk-blocks/load-vk-components.php' );
-	require_once( 'font-awesome/font-awesome-config.php' );
-	require_once( 'term-color/term-color-config.php' );
-	
+	require_once 'vk-blocks/helpers.php';
+	require_once 'vk-components/vk-components-config.php';
+	require_once 'vk-blocks/load-vk-components.php';
+	require_once 'font-awesome/font-awesome-config.php';
+	require_once 'term-color/term-color-config.php';
+
 	if ( ! vkblocks_is_lightning() ) {
-		require_once( 'vk-blocks/load-bootstrap.php' );
+		require_once 'vk-blocks/load-bootstrap.php';
 	}
 
-	require_once( 'admin-notices.php' );
-	require_once( 'vk-blocks/vk-blocks-functions.php' );
-	/* 出力するCSSが多すぎるので一旦コメントアウト */
+	require_once 'admin-notices.php';
+	require_once 'vk-blocks/vk-blocks-functions.php';
+	/*
+	 出力するCSSが多すぎるので一旦コメントアウト */
 	// require_once( 'vk-blocks/functions-color.php' );
 }

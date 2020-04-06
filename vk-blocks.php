6ff9fbc0cfe989ef6ed69cd6221711c172db3f05
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-04-06 18:32:17 +0900

    自動停止機能の修正

--- vk-blocks.php
+++ vk-blocks.php
@@ -12,6 +12,20 @@
 // Do not load directly.
 defined( 'ABSPATH' ) || die();
 
+if ( is_admin() ) {
+	$options = get_option( 'vkExUnit_common_options' );
+	if ( !empty( $options['active_vk-blocks'] ) ) {
+		$options['active_vk-blocks'] = true;
+		update_option( 'vkExUnit_common_options', $options );
+
+		add_action( 'admin_notices', function(){
+			echo '<div class="updated notice"><p>';
+			echo "VK-Blocksと競合するため、VK All in One Expansion UnitのBlock機能を停止しました。";
+			echo '</p></div>';
+		} );
+	}
+}
+
 require_once 'inc/vk-blocks-config.php';
 
 add_action(
@@ -19,56 +33,6 @@ add_action(
 	function () {
 		// Load language files.
 		load_plugin_textdomain( 'vk-blocks', false, 'vk-blocks/inc/vk-blocks/build/languages' );
-	}
-);
-
-/*
-  Helpers ( Plugin only )
-/*-------------------------------------------*/
-if ( ! function_exists( 'vkblocks_deactivate_plugin' ) ) {
-	/**
-	 * Plugin deactive function
-	 *
-	 * @param  [type] $plugin_path [description]
-	 * @return [type]              [description]
-	 */
-	function vkblocks_deactivate_plugin( $plugin_path ) {
-		include_once ABSPATH . 'wp-admin/includes/plugin.php';
-		if ( is_plugin_active( $plugin_path ) ) {
-			$active_plugins = get_option( 'active_plugins' );
-			// delete item
-			$active_plugins = array_diff( $active_plugins, array( $plugin_path ) );
-			// re index
-			$active_plugins = array_values( $active_plugins );
-			update_option( 'active_plugins', $active_plugins );
-		}
-	}
-}
-
-/*
--------------------------------------------*/
-/*
-  Deactive VK Blocks ( Free )
-/*
--------------------------------------------*/
-/* 関数名入れると無料版の宣言と被ってエラーになるので一時的な回避処理で無名関数を利用 */
-add_action(
-	'init',
-	function() {
-		$plugin_base_dir = dirname( __FILE__ );
-		include_once ABSPATH . 'wp-admin/includes/plugin.php';
-		if ( is_plugin_active( 'vk-blocks-pro/vk-blocks.php' ) ) {
-			// Deactive Plugin VK Blocks ( free )
-			if ( function_exists( 'vkblocks_deactivate_plugin' ) ) {
-				vkblocks_deactivate_plugin( 'vk-blocks/vk-blocks.php' );
-			}
-			// Deactive ExUnit included VK Blocks
-			$options = get_option( 'vkExUnit_common_options' );
-			if ( ! empty( $options['active_vk-blocks'] ) ) {
-				$options['active_vk-blocks'] = false;
-				update_option( 'vkExUnit_common_options', $options );
-			}
-		}
 	}
 );
 

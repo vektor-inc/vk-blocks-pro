168600ca43a3ad3594c2c5a1f54c95df154a53a0
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-04-07 19:01:29 +0900

    翻訳

--- src/translation_dummy.js
+++ src/translation_dummy.js
@@ -1,4 +1,4 @@
 () => {
-    __("Disabled Blocks module. Because VK-Blocks Plugin running.", "vk-blocks")
-    __("Deactivated VK-Blocks Plugin. Because VK-Blocks Pro running.", "vk-blocks")
+    __("Disabled Blocks module. Because VK-Blocks Plugin running.")
+    __("Deactivated VK-Blocks Plugin. Because VK-Blocks Pro running.")
 }

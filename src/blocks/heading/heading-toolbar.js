e66e0fc27e8d4e55db02fc7a4111ef8ea609e369
naoki <n.globe.us@gmail.com>
2020-05-01 16:39:17 +0900

    見出しのiconを修正

--- src/blocks/heading/heading-toolbar.js
+++ src/blocks/heading/heading-toolbar.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 /**
  * WordPress dependenciess
  */
@@ -8,25 +6,31 @@ const { range } = lodash;
 const { __, sprintf } = wp.i18n;
 const { Component } = wp.element;
 const { Toolbar } = wp.components;
+import HeadingLevelIcon from './heading-level-icon';
 
 class HeadingToolbar extends Component {
 	createLevelControl(targetLevel, selectedLevel, onChange) {
+		const isActive = targetLevel === selectedLevel;
 		return {
-			icon: 'heading',
+			icon: (
+				<HeadingLevelIcon
+					level={targetLevel}
+					isPressed={isActive}
+				/>
+			),
 			// translators: %s: heading level e.g: "1", "2", "3"
-			title: sprintf( __( 'Heading %d' ), targetLevel ),
-			isActive: targetLevel === selectedLevel,
-			onClick: () => onChange( targetLevel ),
-			subscript: String( targetLevel ),
+			title: sprintf(__('Heading %d'), targetLevel),
+			isActive,
+			onClick: () => onChange(targetLevel),
+			subscript: String(targetLevel),
 		};
 	}
 
-
 	render() {
-		const {minLevel, maxLevel, selectedLevel, onChange} = this.props;
+		const { minLevel, maxLevel, selectedLevel, onChange } = this.props;
 
 		return (
-			<Toolbar controls={range(minLevel, maxLevel).map((index) => this.createLevelControl(index, selectedLevel, onChange))}/>
+			<Toolbar controls={range(minLevel, maxLevel).map((index) => this.createLevelControl(index, selectedLevel, onChange))} />
 		);
 	}
 }

ec033734aad909a4290f29ff761e6e69bef6a088
naoki <n.globe.us@gmail.com>
2020-05-13 16:54:02 +0900

    ステップブロックリアルタイム番号追加+h4テンプレートを追加

--- src/blocks/_pro/step/component.js
+++ src/blocks/_pro/step/component.js
@@ -2,24 +2,24 @@ import { vkbBlockEditor } from "./../../_helper/depModules";
 const { InnerBlocks } = vkbBlockEditor;
 
 export class Component extends React.Component {
-    render() {
-        let for_ = this.props.for_;
-        let className = this.props.className;
-        let containerClass = " vk_step";
-        let elm;
-        const ALLOWED_BLOCKS = ['vk-blocks/step-item'];
-        const TEMPLATE = [ALLOWED_BLOCKS];
+	render() {
+		let for_ = this.props.for_;
+		let className = this.props.className;
+		let containerClass = " vk_step";
+		let elm;
+		const ALLOWED_BLOCKS = ['vk-blocks/step-item'];
+		const TEMPLATE = [ALLOWED_BLOCKS];
 
-        //編集画面とサイト上の切り替え
-        if (for_ === "edit") {
-            elm = <InnerBlocks template={TEMPLATE} allowedBlocks={ALLOWED_BLOCKS} />;
-        } else if ("save") {
-            elm = <InnerBlocks.Content />;
-        }
-        return (
-            <div className={className + containerClass}>
-                {elm}
-            </div>
-        );
-    }
+		//編集画面とサイト上の切り替え
+		if (for_ === "edit") {
+			elm = <InnerBlocks allowedBlocks={ALLOWED_BLOCKS} template={TEMPLATE} />;
+		} else if ("save") {
+			elm = <InnerBlocks.Content />;
+		}
+		return (
+			<div className={className + containerClass}>
+				{elm}
+			</div>
+		);
+	}
 }

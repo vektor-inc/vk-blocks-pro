144211b23dca983e3ebfaaa64c3b9cda233647d7
naoki <n.globe.us@gmail.com>
2020-05-20 11:02:55 +0900

    目次ブロックのサーバーサイドレンダリングを廃止

--- src/blocks/_pro/table-of-contents/block.js
+++ src/blocks/_pro/table-of-contents/block.js
@@ -4,24 +4,22 @@
 
 import { schema } from "./schema";
 import {
-  isAllowedBlock,
-  getBlocksByName,
-  returnHtml,
-  getAllHeadings,
-  removeUnnecessaryElements,
-  asyncGetBlocksByName,
+	isAllowedBlock,
+	getBlocksByName,
+	returnHtml,
+	getAllHeadings,
+	removeUnnecessaryElements,
+	asyncGetBlocksByName,
 } from "./toc-utils";
 import BlockIcon from "./icon.svg";
 
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const {
-  PanelBody,
-  SelectControl,
-  BaseControl,
+	PanelBody,
+	SelectControl,
+	BaseControl,
 } = wp.components;
-import { depServerSideRender } from "../../_helper/depModules";
-const ServerSideRender = depServerSideRender();
 const { Fragment } = wp.element;
 import { vkbBlockEditor } from "../../_helper/depModules";
 const { InspectorControls } = vkbBlockEditor;
@@ -30,124 +28,123 @@ const { createHigherOrderComponent } = wp.compose;
 const { dispatch } = wp.data;
 import { hiddenNewBlock } from "../../_helper/hiddenNewBlock"
 const inserterVisible = hiddenNewBlock(5.3);
+import TocBody from './TocBody'
 
 
 registerBlockType("vk-blocks/table-of-contents", {
-  title: __("Table of Contents", "vk-blocks"),
-  icon: <BlockIcon />,
-  category: "vk-blocks-cat",
-  attributes: schema,
-  supports: {
-    inserter: inserterVisible
-  },
+	title: __("Table of Contents", "vk-blocks"),
+	icon: <BlockIcon />,
+	category: "vk-blocks-cat",
+	attributes: schema,
+	supports: {
+		inserter: inserterVisible
+	},
 
-  edit({ attributes, setAttributes }) {
-    const { style, open } = attributes;
-    return (
-      <Fragment>
-        <InspectorControls>
-          <PanelBody>
-            <BaseControl label={__("Style", "vk-blocks")}>
-              <SelectControl
-                value={style}
-                onChange={(value) => setAttributes({ style: value })}
-                options={[
-                  {
-                    value: "default",
-                    label: __("Default", "vk-blocks"),
-                  },
-                  {
-                    value: "",
-                    label: __("No frame", "vk-blocks"),
-                  },
-                ]}
-              />
-            </BaseControl>
-            <BaseControl label={__("Default Display Status", "vk-blocks")}>
-              <SelectControl
-                value={open}
-                onChange={(value) => setAttributes({ open: value })}
-                options={[
-                  {
-                    value: "open",
-                    label: __("OPEN", "vk-blocks"),
-                  },
-                  {
-                    value: "close",
-                    label: __("CLOSE", "vk-blocks"),
-                  },
-                ]}
-              />
-            </BaseControl>
-          </PanelBody>
-        </InspectorControls>
-        <ServerSideRender
-          block="vk-blocks/table-of-contents"
-          attributes={attributes}
-        />
-      </Fragment>
-    );
-  },
+	edit(props) {
+		const { attributes, setAttributes } = props;
+		const { style, open } = attributes;
+		return (
+			<Fragment>
+				<InspectorControls>
+					<PanelBody>
+						<BaseControl label={__("Style", "vk-blocks")}>
+							<SelectControl
+								value={style}
+								onChange={(value) => setAttributes({ style: value })}
+								options={[
+									{
+										value: "default",
+										label: __("Default", "vk-blocks"),
+									},
+									{
+										value: "",
+										label: __("No frame", "vk-blocks"),
+									},
+								]}
+							/>
+						</BaseControl>
+						<BaseControl label={__("Default Display Status", "vk-blocks")}>
+							<SelectControl
+								value={open}
+								onChange={(value) => setAttributes({ open: value })}
+								options={[
+									{
+										value: "open",
+										label: __("OPEN", "vk-blocks"),
+									},
+									{
+										value: "close",
+										label: __("CLOSE", "vk-blocks"),
+									},
+								]}
+							/>
+						</BaseControl>
+					</PanelBody>
+				</InspectorControls>
+				<TocBody {...props} />
+			</Fragment>
+		);
+	},
 
-  save() {
-    return null;
-  },
+	save(props) {
+		return <TocBody {...props} />;
+	},
 });
 
 const getHeadings = (props) => {
-  const { className, name, clientId, attributes } = props;
-  const { anchor } = attributes;
-  const allowedBlocks = [
-    "vk-blocks/heading",
-    "vk-blocks/outer",
-    "core/heading",
-    "core/cover",
-    "core/group",
-  ];
+	const { className, name, clientId, attributes } = props;
+	const { anchor } = attributes;
+	const allowedBlocks = [
+		"vk-blocks/heading",
+		"vk-blocks/outer",
+		"core/heading",
+		"core/cover",
+		"core/group",
+	];
 
-  const headingList = ["core/heading", "vk-blocks/heading"];
+	const headingList = ["core/heading", "vk-blocks/heading"];
 
-  if (isAllowedBlock(name, allowedBlocks)) {
-    const tocs = getBlocksByName("vk-blocks/table-of-contents");
-    const tocClientId = tocs[0] ? tocs[0].clientId : "";
-    const tocAttributes = tocs[0] ? tocs[0].attributes : "";
+	if (isAllowedBlock(name, allowedBlocks)) {
+		const tocs = getBlocksByName("vk-blocks/table-of-contents");
+		const tocClientId = tocs[0] ? tocs[0].clientId : "";
+		const tocAttributes = tocs[0] ? tocs[0].attributes : "";
 
-    const { updateBlockAttributes } = dispatch("core/block-editor") ? dispatch("core/block-editor") : dispatch("core/editor");
-    if (
-      anchor === undefined &&
-      isAllowedBlock(name, headingList) != undefined
-    ) {
-      updateBlockAttributes(clientId, {
-        anchor: `vk-htags-${clientId}`,
-      });
-    }
+		const { updateBlockAttributes } = dispatch("core/block-editor") ? dispatch("core/block-editor") : dispatch("core/editor");
+		if (
+			anchor === undefined &&
+			isAllowedBlock(name, headingList) != undefined
+		) {
+			updateBlockAttributes(clientId, {
+				anchor: `vk-htags-${clientId}`,
+			});
+		}
 
-    const asyncToc = asyncGetBlocksByName("vk-blocks/table-of-contents");
-    const open = asyncToc[0] ? asyncToc[0].attributes.open : "";
+		const asyncToc = asyncGetBlocksByName("vk-blocks/table-of-contents");
+		const open = asyncToc[0] ? asyncToc[0].attributes.open : "";
 
-    let headingsRaw = getAllHeadings(headingList);
-    let headings = removeUnnecessaryElements(headingsRaw);
-    let render = returnHtml(headings, tocAttributes, className, open);
+		let headingsRaw = getAllHeadings(headingList);
+		let headings = removeUnnecessaryElements(headingsRaw);
+		let render = returnHtml(headings, tocAttributes, className, open);
 
-    if (isAllowedBlock(name, headingList) != undefined) {
-      updateBlockAttributes(tocClientId, {
-        renderHtml: render,
-      });
-    }
-  }
+		if (isAllowedBlock(name, headingList) != undefined) {
+			updateBlockAttributes(tocClientId, {
+				renderHtml: render,
+			});
+		}
+	}
 };
 
 const updateTableOfContents = createHigherOrderComponent((BlockListBlock) => {
-  return (props) => {
-    getHeadings(props);
-    return <BlockListBlock {...props} />;
-  };
+	return (props) => {
+		getHeadings(props);
+		return <BlockListBlock {...props} />;
+	};
 }, "updateTableOfContents");
 
 if (5.3 <= parseFloat(wpVersion)) {
-  addFilter(
-    "editor.BlockListBlock",
-    "vk-blocks/table-of-contents",
-    updateTableOfContents
-  );
+	addFilter(
+		"editor.BlockListBlock",
+		"vk-blocks/table-of-contents",
+		updateTableOfContents
+	);
 }

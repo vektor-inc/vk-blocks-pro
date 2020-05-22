59d7fc28605127c039be2b38f5bf1d57712f656b
naoki <n.globe.us@gmail.com>
2020-05-22 16:31:26 +0900

    add color pulldown

--- src/blocks/border-box/block.js
+++ src/blocks/border-box/block.js
@@ -8,10 +8,10 @@ import { FontAwesome } from "./../_helper/font-awesome";
 
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { PanelBody, BaseControl } = wp.components;
+const { PanelBody, BaseControl,SelectControl } = wp.components;
 const { Fragment } = wp.element;
 import { vkbBlockEditor } from "./../_helper/depModules";
-const { InspectorControls, ColorPalette } = vkbBlockEditor;
+const { InspectorControls } = vkbBlockEditor;
 
 registerBlockType('vk-blocks/border-box', {
 	title: __('Border Box', 'vk-blocks'),
@@ -37,9 +37,31 @@ registerBlockType('vk-blocks/border-box', {
 						</BaseControl>
 					</PanelBody>
 					<PanelBody title={__('Color', 'vk-blocks')}>
-						<ColorPalette
+						<SelectControl
 							value={color}
-							onChange={(value) => setAttributes({ color: value })}
+							onChange={value => setAttributes({ color: value })}
+							options={[
+								{
+									value: "red",
+									label: __("Red", "vk-blocks")
+								},
+								{
+									value: "orange",
+									label: __("Orange", "vk-blocks")
+								},
+								{
+									value: "blue",
+									label: __("Blue", "vk-blocks")
+								},
+								{
+									value: "green",
+									label: __("Green", "vk-blocks")
+								},
+								{
+									value: "black",
+									label: __("Black", "vk-blocks")
+								}
+							]}
 						/>
 					</PanelBody>
 				</InspectorControls>

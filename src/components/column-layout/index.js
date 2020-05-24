3c95520f18f9354294b72146cf5a41907a6c1667
kurudrive <kurudrive@gmail.com>
2020-05-24 09:14:59 +0900

    jscs

--- src/components/column-layout/index.js
+++ src/components/column-layout/index.js
@@ -16,47 +16,47 @@ export const ColumnLayout = (props) => {
 	return (
 		<Fragment>
 			<BaseControl
-				label={__("Column ( Screen size : Extra small )", "vk-blocks")}
+				label={ __("Column ( Screen size : Extra small )", "vk-blocks") }
 			>
 				<RangeControl
-					value={col_xs}
-					onChange={value => setAttributes({ col_xs: formatNum(value, col_xs) })}
-					min={defaultMinMax.min}
-					max={defaultMinMax.max}
+					value={ col_xs }
+					onChange={ value => setAttributes({ col_xs: formatNum(value, col_xs) }) }
+					min={ defaultMinMax.min }
+					max={ defaultMinMax.max }
 				/>
 			</BaseControl>
-			<BaseControl label={__("Column ( Screen size : Small )", "vk-blocks")}>
+			<BaseControl label={ __("Column ( Screen size : Small )", "vk-blocks") }>
 				<RangeControl
-					value={col_sm}
-					onChange={value => setAttributes({ col_sm: formatNum(value, col_sm) })}
-					min={defaultMinMax.min}
-					max={defaultMinMax.max}
+					value={ col_sm }
+					onChange={ value => setAttributes({ col_sm: formatNum(value, col_sm) }) }
+					min={ defaultMinMax.min }
+					max={ defaultMinMax.max }
 				/>
 			</BaseControl>
-			<BaseControl label={__("Column ( Screen size : Medium )", "vk-blocks")}>
+			<BaseControl label={ __("Column ( Screen size : Medium )", "vk-blocks") }>
 				<RangeControl
-					value={col_md}
-					onChange={value => setAttributes({ col_md: formatNum(value, col_md) })}
-					min={defaultMinMax.min}
-					max={defaultMinMax.max}
+					value={ col_md }
+					onChange={ value => setAttributes({ col_md: formatNum(value, col_md) }) }
+					min={ defaultMinMax.min }
+					max={ defaultMinMax.max }
 				/>
 			</BaseControl>
-			<BaseControl label={__("Column ( Screen size : Large )", "vk-blocks")}>
+			<BaseControl label={ __("Column ( Screen size : Large )", "vk-blocks") }>
 				<RangeControl
-					value={col_lg}
-					onChange={value => setAttributes({ col_lg: formatNum(value, col_lg) })}
-					min={defaultMinMax.min}
-					max={defaultMinMax.max}
+					value={ col_lg }
+					onChange={ value => setAttributes({ col_lg: formatNum(value, col_lg) }) }
+					min={ defaultMinMax.min }
+					max={ defaultMinMax.max }
 				/>
 			</BaseControl>
 			<BaseControl
-				label={__("Column ( Screen size : Extra large )", "vk-blocks")}
+				label={ __("Column ( Screen size : Extra large )", "vk-blocks") }
 			>
 				<RangeControl
-					value={col_xl}
-					onChange={value => setAttributes({ col_xl: formatNum(value, col_xl) })}
-					min={defaultMinMax.min}
-					max={defaultMinMax.max}
+					value={ col_xl }
+					onChange={ value => setAttributes({ col_xl: formatNum(value, col_xl) }) }
+					min={ defaultMinMax.min }
+					max={ defaultMinMax.max }
 				/>
 			</BaseControl>
 		</Fragment>

915e67c3f0c8cf12b302d92e54d52cc4a1c36d1b
kurudrive <kurudrive@gmail.com>
2020-06-01 21:11:01 +0900

    ↓ ならこうかなー。ま、テーマ側の方が後から読み込むから問題はないはずだけど念の為

--- inc/font-awesome/font-awesome-config.php
+++ inc/font-awesome/font-awesome-config.php
@@ -3,13 +3,16 @@
   Load modules
 /*-------------------------------------------*/
 if ( ! class_exists( 'Vk_Font_Awesome_Versions' ) ) {
+
 	if ( ! vkblocks_is_lightning() ) {
+
+		global $font_awesome_directory_uri;
+		$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/package/';
+
+		global $set_enqueue_handle_style;
+		$set_enqueue_handle_style = 'vkExUnit_common_style';
+
 		require_once 'package/class-vk-font-awesome-versions.php';
 	}
 
-	global $font_awesome_directory_uri;
-	$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/package/';
-
-	global $set_enqueue_handle_style;
-	$set_enqueue_handle_style = 'vkExUnit_common_style';
 }

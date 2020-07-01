b448627f50a55613279e6a8c7f84f57a087667b5
naoki <n.globe.us@gmail.com>
2020-07-01 17:59:16 +0900

    update faq

--- src/blocks/faq/block.js
+++ src/blocks/faq/block.js
@@ -7,7 +7,10 @@ import { vkbBlockEditor } from "./../_helper/depModules";
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
-const { RichText } = vkbBlockEditor;
+const { PanelBody, BaseControl } = wp.components;
+const { RichText, InnerBlocks, InspectorControls, ColorPalette } = vkbBlockEditor;
+const { Fragment } = wp.element;
+
 const BlockIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -38,21 +41,8 @@ const BlockIcon = (
     />
   </svg>
 );
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
+
 registerBlockType("vk-blocks/faq", {
-  // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
   title: __("FAQ", "vk-blocks"), // Block title.
   icon: BlockIcon, // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
   category: "vk-blocks-cat", // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
@@ -60,72 +50,85 @@ registerBlockType("vk-blocks/faq", {
     heading: {
       type: "string",
       source: "html",
-      selector: "dt"
+      selector: "dd"
     },
     content: {
       type: "string",
       source: "html",
       selector: "dd"
-    }
+		},
+		colorQ:{
+			type:"string",
+			default:"#e50000"
+		},
+		colorA:{
+			type:"string",
+			default:"#337ab7"
+		}
   },
   supports: {
     anchor: true
   },
 
-  /**
-   * The edit function describes the structure of your block in the context of the editor.
-   * This represents what the editor will render when the block is used.
-   *
-   * The "edit" property must be a valid function.
-   *
-   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-   */
   edit({ attributes, setAttributes, className }) {
-    const { heading, content } = attributes;
+	const { heading, content, colorQ, colorA } = attributes;
+
+    return (
+			<Fragment>
+			<InspectorControls>
+				<PanelBody title={__("Color Settings", "vk-blocks")}>
+					<BaseControl label={__('Question Color', 'vk-blocks')}>
+						<ColorPalette
+							value={colorQ}
+							onChange={(value) => setAttributes({ colorQ: value })}
+						/>
+					</BaseControl>
+					<BaseControl label={__('Answer Color', 'vk-blocks')}>
+						<ColorPalette
+							value={colorA}
+							onChange={(value) => setAttributes({ colorA: value })}
+						/>
+					</BaseControl>
+				</PanelBody>
+			</InspectorControls>
+			<dl className={`${className} vk_faq`}>
+				<div className={"vk_faq_inner_section"}>
+				<dt className={"vk_faq_label"} style={{color:colorQ}}>{__('Q', 'vk-blocks')}</dt>
+				<RichText
+					tagName="dd"
+					className={"vk_faq_title"}
+					onChange={value => setAttributes({ heading: value })}
+					value={heading}
+					placeholder={__("Please enter a question.", "vk-blocks")}
+				/>
+				</div>
+				<div className={"vk_faq_inner_section vk_faq_inner_section_answer"}>
+				<dt className={"vk_faq_label"} style={{color:colorA}}>{__('A', 'vk-blocks')}</dt>
+				<dd className={"vk_faq_content"}>
+					<InnerBlocks/>
+				</dd>
+				</div>
+			</dl>
+			</Fragment>
+		);
+	},
+
+	save({ attributes, className }) {
+	const { heading, content, colorQ, colorA } = attributes;
 
     return (
       <dl className={`${className} vk_faq`}>
-        <RichText
-          tagName="dt"
-          className={"vk_faq_title"}
-          onChange={value => setAttributes({ heading: value })}
-          value={heading}
-          placeholder={__("Please enter a question.", "vk-blocks")}
-        />
-        <RichText
-          tagName="dd"
-          className={"vk_faq_content"}
-          onChange={value => setAttributes({ content: value })}
-          value={content}
-          placeholder={__("Please enter a answer.", "vk-blocks")}
-        />
-      </dl>
-    );
-  },
-
-  /**
-   * The save function defin className }> which the different attributes should be combined
-   * into the final markup, which is then serialized by Gutenberg into post_content.
-   *
-   * The "save" property must be specified and must be a valid function.
-   *
-   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-   */
-  save({ attributes, className }) {
-    const { heading, content } = attributes;
-
-    return (
-      <dl className={`${className} vk_faq`}>
-        <RichText.Content
-          tagName="dt"
-          className={"vk_faq_title"}
-          value={heading}
-        />
-        <RichText.Content
-          tagName="dd"
-          className={"vk_faq_content"}
-          value={content}
-        />
+		  <div className={"vk_faq_inner_section"}>
+			  <dt className={"vk_faq_label"} style={{color:colorQ}}>{__('Q', 'vk-blocks')}</dt>
+			  <RichText.Content
+					tagName="dd"
+					className={"vk_faq_title"}
+					value={heading}/>
+			</div>
+			<div className={"vk_faq_inner_section vk_faq_inner_section_answer"}>
+				<dt className={"vk_faq_label"} style={{color:colorA}}>{__('A', 'vk-blocks')}</dt>
+				<dd className={"vk_faq_content"}><InnerBlocks.Content/></dd>
+			</div>
       </dl>
     );
   },

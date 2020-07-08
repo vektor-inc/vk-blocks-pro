5a2ad10fbdf7c4f19d55da5970611d624ebfe80a
rick-addison <rickaddison7634@gmail.com>
2020-07-08 16:37:30 +0900

    途中まで

---
+++ inc/vk-admin/vk-admin-config.php
@@ -0,0 +1,122 @@
+<?php
+
+/*-------------------------------------------*/
+/*  Load modules
+/*-------------------------------------------*/
+if ( ! class_exists( 'Vk_Admin' ) ) {
+	require_once dirname( __FILE__ ) . '/package/class-vk-admin.php';
+}
+
+$admin_pages = array( 'settings_page_vk_blocks_plugin_options' );
+Vk_Admin::admin_scripts( $admin_pages );
+
+global $vk_blocks_prefix;
+$vk_blocks_prefix = 'VK Blocks Pro ';
+
+function vk_blocks_setting_menu() {
+	$custom_page = add_options_page(
+		__( 'VK Blocks setting', 'vk-blocks' ),       // Name of page
+		_x( 'VK Blocks', 'label in admin menu', 'vk-blocks' ),                // Label in menu
+		'edit_theme_options',               // Capability required　このメニューページを閲覧・使用するために最低限必要なユーザーレベルまたはユーザーの種類と権限。
+		'vk_blocks_options',               // ユニークなこのサブメニューページの識別子
+		'vk_blocks_setting'         // メニューページのコンテンツを出力する関数
+	);
+	if ( ! $custom_page ) {
+		return;
+	}
+}
+add_action( 'admin_menu', 'vk_blocks_setting_menu' );
+
+function vk_blocks_setting_init() {
+	register_setting(
+		'vk_blocks_options',
+		'vk_blocks_options'
+	);
+}
+add_action( 'admin_init', 'vk_blocks_setting_init' );
+
+function vk_blocks_setting() {
+    $options = get_option( 'vk_blocks_balloon_meta' );
+    $image_number = 15;
+    $image_number = apply_filters( 'vk_blocks_image_number', $image_number );
+	?>
+
+	<form method="post" action="options.php">
+        <div>
+            <section>
+                <h3><?php _e( 'Balloon Image Setting', 'vk-blocks' ); ?></h3>
+                <table class="form-table">
+                    <?php for( $i = 1; $i <= $image_number; $i++ ) : ?>
+                        <tr>
+							<th><?php _e( 'Ballon Image Name', 'vk-post-author-display' ); ?></th>
+                            <?php
+                            $name = '';
+                            if ( ! empty( $options['default_icons'][$i]['name'] ) ) {
+                                $name = $options['default_icons'][$i]['name'];
+                            }
+                            ?>
+							<td><input type="text" name="vk_blocks_balloon_meta[default_icons][<?php echo $i; ?>][name]" id="author_box_title" value="<?php echo esc_attr( $name ); ?>" style="width:50%;" /></td>
+						</tr>
+						<tr>
+                            <th><?php _e( 'Ballon Image', 'vk-blocks' ) . '[' . $i . ']'; ?></th>
+								<td>
+								<?php
+                                // 現在保存されている画像idを取得して表示
+                                $image = '';
+                                if ( ! empty( $options['default_icons'][$i]['src'] ) ) {
+                                    $image = $options['default_icons'][$i]['src'];
+                                }
+								?>
+								<div class="_display<?php echo $i; ?>" style="height:auto">
+									<?php if ( $image ) : ?>
+										<img src="<?php echo $image; ?>" style="width:200px;height:auto;" />
+									<?php endif; ?>
+								</div>
+
+								<button class="button button-default button-block" style="display:block;width:200px;text-align: center; margin:4px 0;" onclick="javascript:veu_default_image_additional(this);return false;">
+									<?php _e( 'Set image', 'vk-blocks' ); ?>
+								</button>
+
+								<input type="hidden" class="__id<?php echo $i; ?>" name="vk_blocks_balloon_meta[default_icons][<?php echo $i; ?>][src]" value="<?php echo esc_attr( $image ); ?>" />
+
+								<script type="text/javascript">
+									if(veu_default_image_additional == undefined){
+										var veu_default_image_additional = function(e){
+											var d=jQuery(e).parent().children("._display<?php echo $i; ?>");
+											var w=jQuery(e).parent().children('.__id<?php echo $i; ?>')[0];
+											var u=wp.media({library:{type:'image'},multiple:false}).on('select', function(e){
+												u.state().get('selection').each(function(f){
+													// もともと表示されてた img タグを削除
+													d.children().remove();
+													// 新しく画像タグを挿入
+													d.append(jQuery('<img style="width:200px;mheight:auto">').attr('src',f.toJSON().url));
+													jQuery(w).val(f.toJSON().url).change(); });
+											});
+											u.open();
+										};
+									}
+								</script>
+                            </td>
+                        </tr>
+                    <?php endfor; ?>
+                </table>
+            </section>
+        </div>
+    </form>
+
+	<?php
+}
+
+
+/*-------------------------------------------*/
+/*	Setting Page
+/*-------------------------------------------*/
+function vk_blocks_setting_page() {
+	$get_page_title = $vk_blocks_prefix . __( 'Main Setting', 'vk-post-author-display' );
+
+	$get_logo_html = '';
+
+	$get_menu_html  = '<li><a href="#baloon-image-setting">' . __( 'Baloon Image Setting', 'vk-blocks' ) . '</a></li>';
+
+	Vk_Admin::admin_page_frame( $get_page_title, 'vk_blocks_setting', $get_logo_html, $get_menu_html );
+}

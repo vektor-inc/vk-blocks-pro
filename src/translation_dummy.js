357b74921330b276ac6267a6d1b7ef78474a26c3
kurudrive <kurudrive@gmail.com>
2020-07-10 10:43:38 +0900

    翻訳 ... 効かない ...

--- src/translation_dummy.js
+++ src/translation_dummy.js
@@ -1,4 +1,8 @@
 () => {
     __("Disabled Blocks module. Because VK-Blocks Plugin running.")
-    __("Deactivated VK-Blocks Plugin. Because VK-Blocks Pro running.")
+	__("Deactivated VK-Blocks Plugin. Because VK-Blocks Pro running.")
+	__("You can register frequently used icon images for speech bubble blocks.")
+	__("If you change image or name that please click Save Changes button.")
+	__("Select")
+	__("Delete")
 }

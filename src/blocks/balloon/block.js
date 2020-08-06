a184e7dc27f57c8ae4ab79da202ea894e321892b
rick-addison <rickaddison7634@gmail.com>
2020-08-06 14:28:59 +0900

    templateLock を（一応解除）

--- src/blocks/balloon/block.js
+++ src/blocks/balloon/block.js
@@ -272,6 +272,7 @@ registerBlockType("vk-blocks/balloon", {
 		  <div className={ `vk_balloon_content_outer` }>
 			  <div className={"vk_balloon_content"} style={{ background: balloonBgColor, border: balloonBgColor }} >
 				<InnerBlocks
+					templateLock={ false }
 				   template={ [
 						[ 'core/paragraph', { content: content} ],
 					] }

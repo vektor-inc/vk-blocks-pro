a00fdf0457a64ecfe602843d5109cc23d9393b19
naoki <n.globe.us@gmail.com>
2020-08-11 15:58:38 +0900

    [ Pro版 ] 読み込みファイルを分離

--- inc/vk-blocks-config.php
+++ inc/vk-blocks-config.php
@@ -33,12 +33,12 @@ if ( ! function_exists( 'vkblocks_active' ) ) {
 
 	require_once 'admin-notices.php';
 	require_once 'vk-blocks/vk-blocks-functions.php';
-    require_once 'vk-blocks/load-swiper.php';
-    require_once plugin_dir_path( __FILE__ ) . '/vk-blocks/vk-blocks-pro-template.php';
-    $path = dirname(dirname(__FILE__)) .'/inc/vk-blocks/App/RestAPI/BlockMeta/EntryPoint.php';
-	require_once $path;
-	//BlockMeta用のAPIルートを設定
-	new EntryPoint();
+
+	//プロ版の設定ファイルを読み込み
+	if(file_exists('vk-blocks-pro-config.php')){
+		require_once 'vk-blocks-pro-config.php';
+	}
+
 	/*
 	 出力するCSSが多すぎるので一旦コメントアウト */
 	// require_once( 'vk-blocks/functions-color.php' );

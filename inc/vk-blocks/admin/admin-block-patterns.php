39c010ba01e38907c4e71540c6a7814b257b08e6
rick-addison <rickaddison7634@gmail.com>
2020-08-27 11:15:12 +0900

    少し修正

--- inc/vk-blocks/admin/admin-block-patterns.php
+++ inc/vk-blocks/admin/admin-block-patterns.php
@@ -5,22 +5,22 @@ $vk_blocks_options  = wp_parse_args( $vk_blocks_options, $vk_blocks_default_opti
 $vk_blocks_template = file_exists( VK_BLOCKS_PATH . 'vk-blocks-pro-template.php' ) ? VK_BLOCKS_PATH . 'vk-blocks-pro-template.php' : '';
 ?>
 <section>
-<h3 id="block-template-setting"><?php echo __( 'Block Template Setting', 'vk-blocks' ); ?></h3>
+<h3 id="block-template-setting"><?php esc_html_e( 'Block Template Setting', 'vk-blocks' ); ?></h3>
 <ul>
 	<li>
 		<label>
 			<input name="vk_blocks_options[hide_wp_block_template]" type="checkbox" <?php checked( isset( $vk_blocks_options['hide_wp_block_template'] ) ); ?> ></input>
-			<?php echo esc_html_e( 'Hide WordPress Block Template', 'vk-blocks' ) ?>
+			<?php esc_html_e( 'Hide WordPress Block Template', 'vk-blocks' ); ?>
 		</label>
 	</li>
 	<?php if ( ! empty( $vk_blocks_template ) ) : ?>
 		<li>
 			<label>
 				<input name="vk_blocks_options[hide_vk_block_template]" type="checkbox" <?php checked( isset( $vk_blocks_options['hide_vk_block_template'] ) ); ?> ></input>
-				<?php echo esc_html_e( 'Hide VK Block Template', 'vk-blocks' ) ?>
+				<?php esc_html_e( 'Hide VK Block Template', 'vk-blocks' ); ?>
 			</label>
 		</li>
-	<?php endif;?>
+	<?php endif; ?>
 </ul>
 <?php submit_button(); ?>
 </section>

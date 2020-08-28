e352a32f5cb068f7bc58aef2ec167231da1a416a
kurudrive <kurudrive@gmail.com>
2020-08-28 13:59:28 +0900

    コアのブロックテンプレートがデフォルトで非表示＆設定画面でチェックが入るように

--- inc/vk-blocks/admin/admin-block-patterns.php
+++ inc/vk-blocks/admin/admin-block-patterns.php
@@ -1,5 +1,5 @@
 <?php
-$vk_blocks_options  = get_option( 'vk_blocks_options' );
+$vk_blocks_options  = vk_blocks_get_options();
 $vk_blocks_template = file_exists( VK_BLOCKS_PATH . 'vk-blocks-pro-template.php' ) ? VK_BLOCKS_PATH . 'vk-blocks-pro-template.php' : '';
 ?>
 <section>
@@ -7,14 +7,14 @@ $vk_blocks_template = file_exists( VK_BLOCKS_PATH . 'vk-blocks-pro-template.php'
 <ul>
 	<li>
 		<label>
-			<input name="vk_blocks_options[hide_wp_block_template]" type="checkbox" <?php checked( isset( $vk_blocks_options['hide_wp_block_template'] ) ); ?> ></input>
+			<input name="vk_blocks_options[hide_wp_block_template]" type="checkbox" <?php checked( $vk_blocks_options['hide_wp_block_template'] ); ?> ></input>
 			<?php esc_html_e( 'Hide WordPress Block Template', 'vk-blocks' ); ?>
 		</label>
 	</li>
 	<?php if ( ! empty( $vk_blocks_template ) ) : ?>
 		<li>
 			<label>
-				<input name="vk_blocks_options[hide_vk_block_template]" type="checkbox" <?php checked( isset( $vk_blocks_options['hide_vk_block_template'] ) ); ?> ></input>
+				<input name="vk_blocks_options[hide_vk_block_template]" type="checkbox" <?php checked( $vk_blocks_options['hide_vk_block_template'] ); ?> ></input>
 				<?php esc_html_e( 'Hide VK Block Template', 'vk-blocks' ); ?>
 			</label>
 		</li>

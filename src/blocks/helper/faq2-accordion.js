84e9009c90c8aa399b7b2b3d335e4bf0fb34db90
drill-lancer <rickaddison7634@gmail.com>
2020-09-15 20:13:19 +0900

    修正

--- src/blocks/helper/faq2-accordion.js
+++ src/blocks/helper/faq2-accordion.js
@@ -2,20 +2,18 @@
 const vkFaq2Container = document.getElementsByClassName('vk_faq-accordion-on');
 let vkFaq2Q;
 let vkFaq2A;
-let vkFaq2AH = [];
 
 let toggleLoop = (i) => {
 
-    vkFaq2AH[i] = vkFaq2Container[i].querySelector('.vk_faq_content').clientHeight;
-    vkFaq2Container[i].querySelector('.vk_faq_content').style.height = '0px';
+    vkFaq2Container[i].querySelector('.vk_faq_content').style.display = 'none';
 
 
     vkFaq2Container[i].querySelector('.vk_faq_title').addEventListener('click', () => {
         vkFaq2A = vkFaq2Container[i].querySelector('.vk_faq_content');
-        if (vkFaq2A.style.height !== '0px') {
-            vkFaq2A.style.height = '0px';
+        if (vkFaq2A.style.display !== 'none') {
+            vkFaq2A.style.display = 'none';
         } else {
-            vkFaq2A.style.height = vkFaq2AH[i] + 'px';
+			vkFaq2A.style.display = 'block';
         }
 
     }, false);

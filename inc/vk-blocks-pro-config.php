8fd61ff82b2aac9d768ce6d0cf46e5d3b15f1725
drill-lancer <rickaddison7634@gmail.com>
2020-09-17 15:54:15 +0900

    途中経過

--- inc/vk-blocks-pro-config.php
+++ inc/vk-blocks-pro-config.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'vk-blocks/load-swiper.php';
-require_once 'vk-blocks/load-pro-scripts.php';
+require_once 'vk-blocks/vk-blocks-pro-functions.php';
 
 $vk_blocks_options  = vkblocks_get_options();
 if ( "display" === $vk_blocks_options['display_vk_block_template'] ) {

2d3e1c318cafd0483ba1918bb974eba68addcd63
kurudrive <kurudrive@gmail.com>
2020-10-07 15:47:25 +0900

    [ Add Function ][ old faq ] cope with accordion

--- inc/vk-blocks-pro/admin-pro/admin-pro.php
+++ inc/vk-blocks-pro/admin-pro/admin-pro.php
@@ -25,7 +25,7 @@ add_action( 'vk_blocks_pro_template_setting', 'vk_blocks_pro_template_setting' )
  * Vk Blocks Pro Menu
  */
 function vk_blocks_pro_menu( $pro_menu_html ) {
-	$pro_menu_html = '<li><a href="#new-faq-setting">' . __( 'New FAQ Setting', 'vk-blocks' ) . '</a></li>';
+	$pro_menu_html = '<li><a href="#faq-setting">' . __( 'FAQ Setting', 'vk-blocks' ) . '</a></li>';
 	return $pro_menu_html;
 }
 add_action( 'vk_blocks_pro_menu', 'vk_blocks_pro_menu' );

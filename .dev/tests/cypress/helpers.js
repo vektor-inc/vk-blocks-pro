cf38dd23f88f3532ef81434518525e3c8669743f
naoki <n.globe.us@gmail.com>
2020-10-23 10:50:11 +0900

    Check GitHub Actions

--- .dev/tests/cypress/helpers.js
+++ .dev/tests/cypress/helpers.js
@@ -30,7 +30,7 @@ export function loginToSite() {
  */
 export function goTo( path = '/wp-admin' ) {
 	cy.visit( Cypress.env( 'testURL' ) + path );
-	return cy.window().then( win );
+	return cy.window();
 }
 
 /**

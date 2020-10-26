6546aa58cc7b07406be2059267217ee1cc381e61
naoki <n.globe.us@gmail.com>
2020-10-26 14:39:11 +0900

    [Fix] e2e test

--- .dev/tests/cypress/helpers.js
+++ .dev/tests/cypress/helpers.js
@@ -7,7 +7,8 @@ import { startCase } from 'lodash';
  * Login to our test WordPress site
  */
 export function loginToSite() {
-	goTo( '/wp-admin/post-new.php?post_type=post' )
+	// goTo( '/wp-admin/post-new.php?post_type=post' )
+	goTo( '/wp-admin/plugins.php' )
 	.then( ( window ) => {
 		if ( window.location.pathname === '/wp-login.php' ) {
 			// WordPress has a wp_attempt_focus() function that fires 200ms after the wp-login.php page loads.
@@ -17,6 +18,7 @@ export function loginToSite() {
 			cy.get( '#wp-submit' ).click();
 		}
 	} );
+	goTo( '/wp-admin/post-new.php?post_type=post' )
 
 	cy.on('uncaught:exception', (err, runnable) => {
 		expect(err.message).to.include('something about the error')

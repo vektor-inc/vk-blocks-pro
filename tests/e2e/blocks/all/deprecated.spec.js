38b6613e4b39800abb2e38b58a196b882b2b1012
naoki <n.globe.us@gmail.com>
2020-11-13 11:59:52 +0900

    [Fix][e2e] get tag list automaticaly

--- tests/e2e/blocks/all/deprecated.spec.js
+++ tests/e2e/blocks/all/deprecated.spec.js
@@ -7,13 +7,17 @@ const timeout =  5000;
 
 describe( `All Deprecated Blocks Test`, () => {
 
+	//全テストの前に実行
 	beforeAll( async () => {
 		await enablePageDialogAccept();
 	} );
+
+	//各テストの前に実行
 	beforeEach( async () => {
+		//新規投稿
 		await createNewPost();
+		//投稿一覧に移動
 		await helper.movePostsListPage()
-		// await page.screenshot({path: `./tests/e2e/screenshot/beforeeach.png`});
 	} );
 
 	jest.setTimeout(20000);
@@ -48,24 +52,23 @@ describe( `All Deprecated Blocks Test`, () => {
 
 	blocks.forEach( ( block ) => {
 
-		it( `Test Deprecated Javascript: ${block.label}`, async () => {
+		it( `Deprecatedテスト: ${block.label}`, async () => {
 
-			// await page.screenshot({path: `./tests/e2e/screenshot/beforeeach.png`});
-
-			// 投稿一覧で、ブロック名で保存されている投稿検索
+			//投稿一覧で、ブロック名で保存されている投稿検索
 			await helper.searchPost(block.label);
 
-			// 編集画面を開く
+			//編集画面を開くボタンを取得
 			const editButton = (
 				await page.$x( `//strong//a[contains(text(), '${ block.label }')]` )
 			 )[0];
 
+			//編集画面を開く
 			await editButton.click();
 
-			// 編集画面ロードされるのを待つ
+			//編集画面ロードされるのを待つ
 			await page.waitForSelector('#editor');
 
-			// Check if block was inserted and no error.
+			//ブロックのエラーが無いか確認
 			await helper.checkForBlockErrors( block.slug );
 
 		});

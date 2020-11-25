9c72d03ae8df3ed0d9087aa03f092c31e403841f
naoki <n.globe.us@gmail.com>
2020-11-25 14:20:01 +0900

    fix some

--- src/blocks/_pro/slider/deprecated/index.js
+++ src/blocks/_pro/slider/deprecated/index.js
@@ -1,7 +1,7 @@
-import Depreated0_49_1 from "./0.49.1"
 import Depreated0_56_3 from "./0.56.3"
+import Depreated0_49_1 from "./0.49.1"
 
 export default [
-	Depreated0_49_1,
-	Depreated0_56_3
+	Depreated0_56_3,
+	Depreated0_49_1
 ];

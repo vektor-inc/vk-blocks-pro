bff2af001e17c42750ea902f44601bea91674cf4
drill-lancer <rickaddison7634@gmail.com>
2020-12-15 14:26:21 +0900

    from 0.58.7

--- src/blocks/balloon/deprecated/index.js
+++ src/blocks/balloon/deprecated/index.js
@@ -4,6 +4,7 @@ import save002 from './0.0.2/save';
 import save003 from './0.0.3/save';
 import save004 from './0.0.4/save';
 import save005 from './0.58.6/save';
+import save006 from './0.58.7/save';
 
 const blockAttributes = {
 	attributes: {
@@ -135,6 +136,40 @@ const deprecated = [
 			},
 		},
 		save: save005,
+	},
+	{
+		attributes: {
+			...blockAttributes,
+			balloonImageType: {
+				type: 'string',
+				default: 'normal', // no image by default!
+			},
+			balloonAnimation: {
+				type: 'string',
+				default: 'none', // no image by default!
+			},
+			balloonBorder: {
+				type: 'boolean',
+				default: false,
+			},
+			balloonImageBorder: {
+				type: 'boolean',
+				default: false,
+			},
+			balloonBorderWidth: {
+				type: 'string',
+				default: 'thin',
+			},
+			balloonBorderColor: {
+				type: 'string',
+				default: '#cccccc',
+			},
+			balloonBgColor: {
+				type: 'string',
+				default: '#f5f5f5',
+			},
+		},
+		save: save006,
 	},
 ];
 export default deprecated;

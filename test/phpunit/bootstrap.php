30eedb03b82725329279324557642d8edb75bd46
naoki <n.globe.us@gmail.com>
2020-12-23 10:16:09 +0900

    [PageContent] fix unit test

--- test/phpunit/bootstrap.php
+++ test/phpunit/bootstrap.php
@@ -39,9 +39,8 @@ define( 'GUTENBERG_LOAD_VENDOR_SCRIPTS', false );
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
+	require_once dirname( dirname( dirname( __FILE__ ) ) ) . '/inc/vk-blocks/vk-blocks-functions.php';
 	require_once dirname( dirname( dirname( __FILE__ ) ) ) . '/src/blocks/page-content/index.php';
-
-	register_theme_directory( dirname( __FILE__ ) . '/../../' ); switch_theme('lightning-pro'); search_theme_directories();
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 

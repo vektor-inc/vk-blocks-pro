2979e7e4c59d74692238f9ef032c6c1462e1c877
drill-lancer <rickaddison7634@gmail.com>
2020-12-23 15:35:30 +0900

    faq2_render_callback

--- src/blocks/faq2/index.php
+++ src/blocks/faq2/index.php
@@ -11,9 +11,30 @@ if( function_exists('register_block_type_from_metadata')) {
 			array(
 				'editor_style' => 'vk-blocks-build-editor-css',
 				'editor_script' => 'vk-blocks-build-js',
+				'render_callback' => 'vk_faq2_render_callback'
 			)
 		);
 	}
 	add_action( 'init', 'register_block_vk_faq2', 99 );
+}
+
+/**
+ * Render Callback of FAQ2 Block
+ *
+ * @param array $attributes attributes.
+ * @param html  $content content.
+ */
+function vk_page_content_render_callback( $attributes, $content = '' ) {
+	if ( has_block( 'vk-blocks/faq2', $content ) || has_block( 'vk-blocks/faq', $content ) ) {
+		if ( 'open' === $vk_blocks_options['new_faq_accordion'] ) {
+			$content = str_replace( '[accordion_trigger_switch]', 'vk_faq-accordion vk_faq-accordion-open', $content );
+		} elseif ( 'close' === $vk_blocks_options['new_faq_accordion'] ) {
+			$content = str_replace( '[accordion_trigger_switch]', 'vk_faq-accordion vk_faq-accordion-close', $content );
+		} else {
+			$content = str_replace( '[accordion_trigger_switch]', '', $content );
+		}
+	}
+	vk_blocks_pro_load_scripts();
+	return $content;
 }
 

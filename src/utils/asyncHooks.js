60703ca9471f8e029913f9837b1faf6790a76cc5
naoki <n.globe.us@gmail.com>
2020-12-25 17:27:40 +0900

    [Utils] fix lint

--- src/utils/asyncHooks.js
+++ src/utils/asyncHooks.js
@@ -1,7 +1,7 @@
 import { useSelect } from '@wordpress/data';
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
-export const AsyncGetInnerBlocks = (clientId) =>
+export const asyncGetInnerBlocks = (clientId) =>
 	useSelect((select) => {
 		const { getBlocks } = select('core/block-editor');
 		//getBlocks(clientId)で、innerBlocksを取得

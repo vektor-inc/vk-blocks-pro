56bdf76126acad58860d435b1e775189f11bec5e
drill-lancer <rickaddison7634@gmail.com>
2021-01-05 13:25:18 +0900

    lint timeline-item

--- src/blocks/_pro/timeline-item/component.js
+++ src/blocks/_pro/timeline-item/component.js
@@ -1,55 +1,49 @@
-import { vkbBlockEditor } from "./../../../utils/depModules";
+import { vkbBlockEditor } from './../../../utils/depModules';
 const { InnerBlocks } = vkbBlockEditor;
-const { __ } = wp.i18n; // Import __() from wp.i18n
 
+// eslint-disable-next-line no-undef
 export class Component extends React.Component {
-    render() {
-        const {
-            label,
-            color,
-            style,
-            styleLine
-        } = this.props.attributes;
-        const for_ = this.props.for_;
-        const className = this.props.className;
-        const containerClass = " vk_timeline_item";
-        let elm;
-        let styleClass;
-        let styleLineClass;
-        let inlineStyle;
-        let marker;
-        const TEMPLATE = [['core/heading', { level: 4 }]];
+	render() {
+		const { label, color, style, styleLine } = this.props.attributes;
+		const for_ = this.props.for_;
+		const className = this.props.className;
+		const containerClass = ' vk_timeline_item';
+		let elm;
+		let styleClass;
+		let styleLineClass;
+		let inlineStyle;
+		const TEMPLATE = [['core/heading', { level: 4 }]];
 
-        //編集画面とサイト上の切り替え
-        if (for_ === "edit") {
-            elm = <InnerBlocks template={ TEMPLATE } />;
-        } else if ("save") {
-            elm = <InnerBlocks.Content />;
-        }
+		//編集画面とサイト上の切り替え
+		if (for_ === 'edit') {
+			elm = <InnerBlocks template={TEMPLATE} />;
+		} else if ('save') {
+			elm = <InnerBlocks.Content />;
+		}
 
-        if (style === "solid") {
-            styleClass = ' vk_timeline_item_style-default';
-            inlineStyle = { backgroundColor: `${color}` };
-        } else if (style === "outlined") {
-            styleClass = ' vk_timeline_item_style-outlined'
-            inlineStyle = { border: `3px solid ${color}` };
-        }
+		if (style === 'solid') {
+			styleClass = ' vk_timeline_item_style-default';
+			inlineStyle = { backgroundColor: `${color}` };
+		} else if (style === 'outlined') {
+			styleClass = ' vk_timeline_item_style-outlined';
+			inlineStyle = { border: `3px solid ${color}` };
+		}
 
-        if (styleLine === "default") {
-            styleLineClass = ' vk_timeline_item_lineStyle-default';
-        } else if (styleLine === "none") {
-            styleLineClass = ' vk_timeline_item_lineStyle-none'
-        }
+		if (styleLine === 'default') {
+			styleLineClass = ' vk_timeline_item_lineStyle-default';
+		} else if (styleLine === 'none') {
+			styleLineClass = ' vk_timeline_item_lineStyle-none';
+		}
 
-        return (
-	<div className={ className + containerClass + styleLineClass }>
-		<div className={ "vk_timeline_item_caption" }>{ label }</div>
-		<div className={ "vk_timeline_item_contentnpm" }>{ elm }</div>
-		<div
-			className={ 'vk_timeline_item_style' + styleClass }
-			style={ inlineStyle }
-                />
-	</div>
-        );
-    }
+		return (
+			<div className={className + containerClass + styleLineClass}>
+				<div className={'vk_timeline_item_caption'}>{label}</div>
+				<div className={'vk_timeline_item_contentnpm'}>{elm}</div>
+				<div
+					className={'vk_timeline_item_style' + styleClass}
+					style={inlineStyle}
+				/>
+			</div>
+		);
+	}
 }
--- src/blocks/_pro/timeline-item/schema.js
+++ src/blocks/_pro/timeline-item/schema.js
@@ -1,18 +1,18 @@
 export const schema = {
-    label: {
-        type: 'string',
-        default:  '6:00AM',
-    },
-    color: {
-        type: 'string',
-        default: '#337ab7',
-    },
-    style: {
-        type: 'string',
-        default: 'outlined',
-    },
-    styleLine: {
-        type: 'string',
-        default: 'default',
-    },
+	label: {
+		type: 'string',
+		default: '6:00AM',
+	},
+	color: {
+		type: 'string',
+		default: '#337ab7',
+	},
+	style: {
+		type: 'string',
+		default: 'outlined',
+	},
+	styleLine: {
+		type: 'string',
+		default: 'default',
+	},
 };

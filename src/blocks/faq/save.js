428154487c8c787836dff100f06eb0fa44360711
naoki <n.globe.us@gmail.com>
2021-01-06 16:29:35 +0900

    [Faq2] fix save .js

--- src/blocks/faq/save.js
+++ src/blocks/faq/save.js
@@ -3,8 +3,11 @@ import { RichText, InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 export default function save({ attributes }) {
 	const { heading } = attributes;
 	return (
-		<dl {...useBlockProps.save({ className: `vk_faq` })}>
-			<RichText.Content
+		<dl
+			{...useBlockProps.save({
+				className: `vk_faq [accordion_trigger_switch]`,
+			})}
+		>			<RichText.Content
 				tagName="dt"
 				className={'vk_faq_title'}
 				value={heading}

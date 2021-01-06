a434da81ad657c1938071010e432617895637f83
naoki <n.globe.us@gmail.com>
2021-01-06 16:04:58 +0900

    [Faq/Faq2] fix accordion

--- inc/vk-blocks-pro/vk-blocks-pro-functions.php
+++ inc/vk-blocks-pro/vk-blocks-pro-functions.php
@@ -20,23 +20,47 @@ function vk_blocks_pro_get_options( $defaults ) {
 }
 add_filter( 'vk_blocks_default_options', 'vk_blocks_pro_get_options' );
 
+
+function vk_blocks_pro_load_scripts_faq2(){
+	wp_enqueue_script( 'vk-blocks-faq2', VK_BLOCKS_URL . 'build/faq2.min.js', array(), VK_BLOCKS_VERSION, true );
+}
+function vk_blocks_pro_load_scripts_animation(){
+	wp_enqueue_script( 'vk-blocks-animation', VK_BLOCKS_URL . 'build/vk-animation.min.js', array(), VK_BLOCKS_VERSION, true );
+}
+function vk_blocks_pro_load_scripts_slider(){
+	wp_enqueue_style( 'vk-blocks-swiper', VK_BLOCKS_URL . 'build/swiper.min.css', array(), VK_BLOCKS_VERSION );
+	wp_enqueue_script( 'vk-blocks-swiper', VK_BLOCKS_URL . 'build/swiper.min.js', array(), VK_BLOCKS_VERSION, true );
+	wp_enqueue_script( 'vk-blocks-slider', VK_BLOCKS_URL . 'build/vk-slider.min.js', array( 'vk-blocks-swiper' ), VK_BLOCKS_VERSION, true );
+}
+
 /**
  * Pro 専用のスクリプトの読み込み
  */
 function vk_blocks_pro_load_scripts() {
 
-	if ( has_block( 'vk-blocks/faq2' ) || has_block( 'vk-blocks/faq' ) ) {
-		wp_enqueue_script( 'vk-blocks-faq2', VK_BLOCKS_URL . 'build/faq2.min.js', array(), VK_BLOCKS_VERSION, true );
-	}
+	// フロントページが投稿一覧の場合はpostIDを取得できないので、強制読み込み。
+	// TODO: 速度改善のためにウィジェットの有無で読み込む
+	if(is_front_page() && is_home() ){
 
-	if ( has_block( 'vk-blocks/animation' ) ) {
-		wp_enqueue_script( 'vk-blocks-animation', VK_BLOCKS_URL . 'build/vk-animation.min.js', array(), VK_BLOCKS_VERSION, true );
-	}
+		vk_blocks_pro_load_scripts_faq2();
+		vk_blocks_pro_load_scripts_animation();
+		vk_blocks_pro_load_scripts_slider();
 
-	if ( has_block( 'vk-blocks/slider' ) ) {
-		wp_enqueue_style( 'vk-blocks-swiper', VK_BLOCKS_URL . 'build/swiper.min.css', array(), VK_BLOCKS_VERSION );
-		wp_enqueue_script( 'vk-blocks-swiper', VK_BLOCKS_URL . 'build/swiper.min.js', array(), VK_BLOCKS_VERSION, true );
-		wp_enqueue_script( 'vk-blocks-slider', VK_BLOCKS_URL . 'build/vk-slider.min.js', array( 'vk-blocks-swiper' ), VK_BLOCKS_VERSION, true );
+	}else{
+
+		$current_post_id = get_the_ID();
+
+		if ( has_block( 'vk-blocks/faq2', $current_post_id ) || has_block( 'vk-blocks/faq', $current_post_id ) ) {
+			vk_blocks_pro_load_scripts_faq2();
+		}
+
+		if ( has_block( 'vk-blocks/animation', $current_post_id ) ) {
+			vk_blocks_pro_load_scripts_animation();
+		}
+
+		if ( has_block( 'vk-blocks/slider', $current_post_id ) ) {
+			vk_blocks_pro_load_scripts_slider();
+		}
 	}
 
 }
--- src/blocks/faq2/index.php
+++ src/blocks/faq2/index.php
@@ -10,35 +10,26 @@ if( function_exists('register_block_type_from_metadata')) {
 			__DIR__,
 			array(
 				'editor_style' => 'vk-blocks-build-editor-css',
-				'editor_script' => 'vk-blocks-build-js',
-				'render_callback' => 'vk_faq2_render_callback'
+				'editor_script' => 'vk-blocks-build-js'
 			)
 		);
 	}
 	add_action( 'init', 'register_block_vk_faq2', 99 );
 }
 
-/**
- * Render Callback of FAQ2 Block
- *
- * @param array $attributes attributes.
- * @param html  $content content.
- */
-function vk_faq2_render_callback( $attributes, $content = '' ) {
+function vk_faq2_render_callback( $block_content, $block ) {
+
 	$vk_blocks_options  = vkblocks_get_options();
-	if ( has_block( 'vk-blocks/faq2', $content ) ) {
-		if ( ! empty( $vk_blocks_options['new_faq_accordion'] ) && 'open' === $vk_blocks_options['new_faq_accordion'] ) {
-			$content = str_replace( '[accordion_trigger_switch]', 'vk_faq-accordion vk_faq-accordion-open', $content );
-		} elseif ( ! empty( $vk_blocks_options['new_faq_accordion'] ) && 'close' === $vk_blocks_options['new_faq_accordion'] ) {
-			$content = str_replace( '[accordion_trigger_switch]', 'vk_faq-accordion vk_faq-accordion-close', $content );
+	if ( 'vk-blocks/faq2' === $block['blockName'] ) {
+		if ( 'open' === $vk_blocks_options['new_faq_accordion'] ) {
+			$block_content = str_replace( '[accordion_trigger_switch]', 'vk_faq-accordion vk_faq-accordion-open', $block_content );
+		} elseif ( 'close' === $vk_blocks_options['new_faq_accordion'] ) {
+			$block_content = str_replace( '[accordion_trigger_switch]', 'vk_faq-accordion vk_faq-accordion-close', $block_content );
 		} else {
-			$content = str_replace( '[accordion_trigger_switch]', '', $content );
+			$block_content = str_replace( '[accordion_trigger_switch]', '', $block_content );
 		}
 	}
-	if ( function_exists( 'vk_blocks_pro_load_scripts' ) ) {
-		vk_blocks_pro_load_scripts();
-	}
-
-	return $content;
+	return $block_content;
 }
 
+add_filter( 'render_block', 'vk_faq2_render_callback', 10, 2 );

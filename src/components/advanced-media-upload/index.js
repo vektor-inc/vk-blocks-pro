c1d92693afd9b6f43f662caaacc2adcf727c6d96
drill-lancer <rickaddison7634@gmail.com>
2021-01-07 16:01:41 +0900

    一時

--- src/components/advanced-media-upload/index.js
+++ src/components/advanced-media-upload/index.js
@@ -4,47 +4,57 @@ const { Fragment } = wp.element;
 const { MediaUpload } =
 	wp.blockEditor && wp.blockEditor.BlockEdit ? wp.blockEditor : wp.editor;
 const { dispatch } = wp.data;
-import noImage from "../../../inc/vk-blocks/images/no-image.svg";
+import noImage from '../../../inc/vk-blocks/images/no-image.svg';
 
 export const AdvancedMediaUpload = (props) => {
 	const { schema, clientId, setAttributes, attributes } = props;
 
 	const deleteImgBtn = () => {
-		dispatch("core/editor").updateBlockAttributes(clientId, {
+		dispatch('core/editor').updateBlockAttributes(clientId, {
 			[schema]: null,
 		});
 	};
 
 	return (
 		<MediaUpload
-			onSelect={ (value) => setAttributes({ [schema]: value.url }) }
+			onSelect={(value) => setAttributes({ [schema]: value })}
 			type="image"
-			value={ attributes[schema] }
-			render={ ({ open }) => (
+			value={attributes[schema]}
+			render={({ open }) => (
 				<Fragment>
-					{ attributes[schema] ? (
+					{attributes[schema] ? (
 						<Fragment>
-							<img className={ "icon-image" } src={ attributes[schema] } />
+							<img
+								className={'icon-image'}
+								src={attributes[schema].url}
+								alt={attributes[schema].alt}
+							/>
 							<Button
-								onClick={ deleteImgBtn }
-								className={ "image-button button button-delete" }
+								onClick={deleteImgBtn}
+								className={'image-button button button-delete'}
 							>
-								{ __("Delete Image", "vk-blocks") }
+								{__('Delete Image', 'vk-blocks')}
 							</Button>
 						</Fragment>
 					) : (
 						<Fragment>
-							<img className={ "icon-image" } src={ noImage } />
+							<img
+								className={'icon-image'}
+								src={noImage}
+								alt={''}
+							/>
 							<Button
-								onClick={ open }
-								className={ "button button-large components-button" }
-								>
-								{ __("Select image", "vk-blocks") }
+								onClick={open}
+								className={
+									'button button-large components-button'
+								}
+							>
+								{__('Select image', 'vk-blocks')}
 							</Button>
 						</Fragment>
-						) }
+					)}
 				</Fragment>
-			) }
+			)}
 		/>
 	);
 };

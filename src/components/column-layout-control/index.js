d0a39a1d49745dce7a85fc77947ef6c49e3cd357
drill-lancer <rickaddison7634@gmail.com>
2021-01-07 16:50:37 +0900

    lint clc

--- src/components/column-layout-control/index.js
+++ src/components/column-layout-control/index.js
@@ -2,79 +2,88 @@ import { __ } from '@wordpress/i18n';
 import { PanelBody, BaseControl, SelectControl } from '@wordpress/components';
 import { Component } from '@wordpress/element';
 import { createHigherOrderComponent, compose } from '@wordpress/compose';
-import { ColumnLayout } from "@vkblocks/components/column-layout";
+import { ColumnLayout } from '@vkblocks/components/column-layout';
 
-export const setOptions = name => {
+export const setOptions = (name) => {
 	const options = {
-		"vk-blocks/card": [
+		'vk-blocks/card': [
 			{
-				value: "card",
-				label: __("Card", "vk-blocks")
+				value: 'card',
+				label: __('Card', 'vk-blocks'),
 			},
 			{
-				value: "card-noborder",
-				label: __("Card (No border)", "vk-blocks")
+				value: 'card-noborder',
+				label: __('Card (No border)', 'vk-blocks'),
 			},
 			{
-				value: "card-imageRound",
-				label: __("Card (Image Round)", "vk-blocks")
-			}
+				value: 'card-imageRound',
+				label: __('Card (Image Round)', 'vk-blocks'),
+			},
 		],
-		"vk-blocks/else": [
+		'vk-blocks/else': [
 			{
-				value: "card",
-				label: __("Card", "vk-blocks")
+				value: 'card',
+				label: __('Card', 'vk-blocks'),
 			},
 			{
-				value: "card-noborder",
-				label: __("Card (No border)", "vk-blocks")
+				value: 'card-noborder',
+				label: __('Card (No border)', 'vk-blocks'),
 			},
 			{
-				value: "card-horizontal",
-				label: __("Card (Horizontal)", "vk-blocks")
+				value: 'card-horizontal',
+				label: __('Card (Horizontal)', 'vk-blocks'),
 			},
 			{
-				value: "media",
-				label: __("Media", "vk-blocks")
+				value: 'media',
+				label: __('Media', 'vk-blocks'),
 			},
 			{
-				value: "postListText",
-				label: __("Text 1 Column", "vk-blocks")
-			}
-		]
+				value: 'postListText',
+				label: __('Text 1 Column', 'vk-blocks'),
+			},
+		],
 	};
 
-	if (name === "vk-blocks/card") {
+	if (name === 'vk-blocks/card') {
 		return options[name];
 	}
-	return options["vk-blocks/else"];
-
+	return options['vk-blocks/else'];
 };
 
+// eslint-disable-next-line no-shadow,no-unused-vars
 const ColumnLayoutControlRaw = (ColumnLayout) =>
 	createHigherOrderComponent(
-		(ColumnLayout) => class extends Component {
-			render() {
-				const { setAttributes, attributes } = this.props;
-				const { layout, name } = attributes;
-				return (
-					<PanelBody
-						title={ __("Display type and columns", "vk-blocks") }
-						initialOpen={ false }
-					>
-						<BaseControl label={ __("Display type", "vk-blocks") }>
-							<SelectControl
-								value={ layout }
-								onChange={ value => setAttributes({ layout: value }) }
-								options={ setOptions(name) }
-							/>
-						</BaseControl>
-						<ColumnLayout { ...this.props } />
-					</PanelBody>
-				);
-			}
-		},
-		"ColumnLayoutControlRaw"
+		// eslint-disable-next-line no-shadow
+		(ColumnLayout) =>
+			class extends Component {
+				render() {
+					const { setAttributes, attributes } = this.props;
+					const { layout, name } = attributes;
+					return (
+						<PanelBody
+							title={__('Display type and columns', 'vk-blocks')}
+							initialOpen={false}
+						>
+							<BaseControl
+								label={__('Display type', 'vk-blocks')}
+								id={'vk_column-displayType'}
+							>
+								<SelectControl
+									value={layout}
+									onChange={(value) =>
+										setAttributes({ layout: value })
+									}
+									options={setOptions(name)}
+								/>
+							</BaseControl>
+							<ColumnLayout {...this.props} />
+						</PanelBody>
+					);
+				}
+			},
+		'ColumnLayoutControlRaw'
 	);
 
-export const ColumnLayoutControl = compose(ColumnLayoutControlRaw())(ColumnLayout);
+export const ColumnLayoutControl = compose(ColumnLayoutControlRaw())(
+	ColumnLayout
+);

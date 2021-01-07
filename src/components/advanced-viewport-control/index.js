c3a98eceb590ecea58faee47ac0bebe81d203789
drill-lancer <rickaddison7634@gmail.com>
2021-01-07 16:23:42 +0900

    lint avc

--- src/components/advanced-viewport-control/index.js
+++ src/components/advanced-viewport-control/index.js
@@ -1,44 +1,47 @@
-
-const { __ } = wp.i18n;
-const { Fragment } = wp.element;
-const { TextControl } = wp.components;
+import { __ } from '@wordpress/i18n';
+import { Fragment } from '@wordpress/element';
+import { TextControl } from '@wordpress/components';
 
 const AdvancedViewportControl = (props) => {
 	const { attributes, setAttributes, initial } = props;
-	let { pc,tablet,mobile } = attributes
-	const { iPc, iTablet, iMobile } = initial
+	let { pc, tablet, mobile } = attributes;
+	const { iPc, iTablet, iMobile } = initial;
 
-	if(!pc){
-		pc = iPc
+	if (!pc) {
+		pc = iPc;
 	}
-	if(!tablet){
-		tablet = iTablet
+	if (!tablet) {
+		tablet = iTablet;
 	}
-	if(!mobile){
-		mobile = iMobile
+	if (!mobile) {
+		mobile = iMobile;
 	}
 
 	return (
 		<Fragment>
 			<TextControl
-				label={ __('PC', 'vk-blocks') }
-				value={ pc }
-				onChange={ (value) => setAttributes({ pc: parseFloat(value) }) }
-				type={ "number" }
-		/>
+				label={__('PC', 'vk-blocks')}
+				value={pc}
+				onChange={(value) => setAttributes({ pc: parseFloat(value) })}
+				type={'number'}
+			/>
 			<TextControl
-				label={ __('Tablet', 'vk-blocks') }
-				value={ tablet }
-				onChange={ (value) => setAttributes({ tablet: parseFloat(value) }) }
-				type={ "number" }
-		/>
+				label={__('Tablet', 'vk-blocks')}
+				value={tablet}
+				onChange={(value) =>
+					setAttributes({ tablet: parseFloat(value) })
+				}
+				type={'number'}
+			/>
 			<TextControl
-				label={ __('Mobile', 'vk-blocks') }
-				value={ mobile }
-				onChange={ (value) => setAttributes({ mobile: parseFloat(value) }) }
-				type={ "number" }
-		/>
+				label={__('Mobile', 'vk-blocks')}
+				value={mobile}
+				onChange={(value) =>
+					setAttributes({ mobile: parseFloat(value) })
+				}
+				type={'number'}
+			/>
 		</Fragment>
-  );
+	);
 };
-export default AdvancedViewportControl
+export default AdvancedViewportControl;

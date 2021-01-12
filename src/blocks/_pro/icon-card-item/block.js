58cb13e96f3fce4b3576f274518cff98b166d765
drill-lancer <rickaddison7634@gmail.com>
2021-01-12 14:29:41 +0900

    整理

--- src/blocks/_pro/icon-card-item/block.js
+++ src/blocks/_pro/icon-card-item/block.js
@@ -2,10 +2,16 @@
  * pr-card-item block type
  *
  */
-import { PRcarditem } from './component';
 import { deprecated } from './deprecated';
 import { FontAwesome } from '@vkblocks/utils/font-awesome-new';
 import { ReactComponent as Icon } from './icon.svg';
+
+import { __ } from '@wordpress/i18n';
+import { fixBrokenUnicode } from '@vkblocks/utils/depModules';
+import { RichText } from '@wordpress/block-editor';
+import ReactHtmlParser from 'react-html-parser';
+import { convertToGrid } from '@vkblocks/utils/convert-to-grid';
+
 
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
@@ -91,10 +97,59 @@ registerBlockType('vk-blocks/icon-card-item', {
 	supports: {
 		className: true,
 	},
-
+	/* eslint camelcase: 0 */
 	edit(props) {
-		const { setAttributes, attributes } = props;
-		const { url, urlOpenType, color, bgType } = attributes;
+		const { setAttributes, attributes, className } = props;
+		let {
+			color,
+			heading,
+			content,
+			faIcon,
+			url,
+			icon,
+			urlOpenType,
+			bgType,
+			col_xs,
+			col_sm,
+			col_md,
+			col_lg,
+			col_xl,
+			col_xxl,
+			activeControl,
+		} = attributes;
+		const align = JSON.parse(fixBrokenUnicode(activeControl));
+
+		let style;
+		let iconColor;
+
+		if (bgType === '0') {
+			style = {
+				backgroundColor: `${color}`,
+				border: `1px solid ${color}`,
+			};
+			iconColor = `#ffffff`;
+		} else {
+			style = {
+				backgroundColor: `transparent`,
+				border: `1px solid ${color}`,
+			};
+			iconColor = `${color}`;
+		}
+
+		//過去バージョンをリカバリーした時にiconを正常に表示する
+		if (faIcon && !faIcon.match(/<i/)) {
+			faIcon = `<i class="${faIcon}"></i>`;
+
+			//過去のicon attribuet用 deprecated処理
+		} else if (!faIcon && icon && !icon.match(/<i/)) {
+			faIcon = `<i class="${icon}"></i>`;
+		}
+
+		//add class and inline css
+		const faIconFragment = faIcon.split(' ');
+		faIconFragment[0] = faIconFragment[0] + ` style="color:${iconColor}" `;
+		faIconFragment[1] = faIconFragment[1] + ` vk_icon-card_item_icon `;
+		const faIconTag = faIconFragment.join('');
 
 		return (
 			<>
@@ -156,13 +211,161 @@ registerBlockType('vk-blocks/icon-card-item', {
 						</BaseControl>
 					</PanelBody>
 				</InspectorControls>
-				<PRcarditem {...props} for_={'edit'} />
+
+				<div
+					className={`${className} vk_post vk_icon-card_item vk_post-col-xs-${convertToGrid(
+						col_xs
+					)} vk_post-col-sm-${convertToGrid(
+						col_sm
+					)} vk_post-col-md-${convertToGrid(
+						col_md
+					)} vk_post-col-lg-${convertToGrid(
+						col_lg
+					)} vk_post-col-xl-${convertToGrid(
+						col_xl
+					)} vk_post-col-xxl-${convertToGrid(col_xxl)}`}
+				>
+					<div className="vk_icon-card_item_icon_outer" style={style}>
+						{ReactHtmlParser(faIconTag)}
+					</div>
+					<RichText
+						className={`vk_icon-card_item_title vk_icon-card_item_title has-text-align-${align.title}`}
+						tagName={'h3'}
+						onChange={(value) =>
+							props.setAttributes({ heading: value })
+						}
+						value={heading}
+						allowedFormats={[
+							'core/bold', // 太字
+							'core/code', // インラインコード
+							'core/image', // インライン画像
+							'core/italic', // イタリック
+							// 'core/link', // リンク
+							'core/strikethrough', // 取り消し線
+							'core/underline', // 下線（未実装？）
+							'core/text-color', // 文字色
+							'core/superscript', // 上付き
+							'core/subscript', // 下付き
+							'vk-blocks/highlighter', // 蛍光マーカー
+							'vk-blocks/responsive-br', // Select a direction (レスポンシブ改行)
+						]}
+						placeholder={__('Input Title', 'vk-blocks')}
+					/>
+					<RichText
+						className={`vk_icon_card_item_summary vk_icon_card_item_summary has-text-align-${align.text}`}
+						tagName={'p'}
+						onChange={(value) => setAttributes({ content: value })}
+						value={content}
+						allowedFormats={[
+							'core/bold', // 太字
+							'core/code', // インラインコード
+							'core/image', // インライン画像
+							'core/italic', // イタリック
+							// 'core/link', // リンク
+							'core/strikethrough', // 取り消し線
+							'core/underline', // 下線（未実装？）
+							'core/text-color', // 文字色
+							'core/superscript', // 上付き
+							'core/subscript', // 下付き
+							'vk-blocks/highlighter', // 蛍光マーカー
+							'vk-blocks/responsive-br', // Select a direction (レスポンシブ改行)
+						]}
+						placeholder={__('Input Content', 'vk-blocks')}
+					/>
+				</div>
 			</>
 		);
 	},
 
 	save(props) {
-		return <PRcarditem {...props} for_={'save'} />;
+		const { attributes, className } = props;
+		let {
+			color,
+			heading,
+			content,
+			faIcon,
+			url,
+			icon,
+			urlOpenType,
+			bgType,
+			col_xs,
+			col_sm,
+			col_md,
+			col_lg,
+			col_xl,
+			col_xxl,
+			activeControl,
+		} = attributes;
+		const align = JSON.parse(fixBrokenUnicode(activeControl));
+
+		let style;
+		let iconColor;
+
+		if (bgType === '0') {
+			style = { backgroundColor: `${color}`, border: `1px solid ${color}` };
+			iconColor = `#ffffff`;
+		} else {
+			style = {
+				backgroundColor: `transparent`,
+				border: `1px solid ${color}`,
+			};
+			iconColor = `${color}`;
+		}
+
+		//過去バージョンをリカバリーした時にiconを正常に表示する
+		if (faIcon && !faIcon.match(/<i/)) {
+			faIcon = `<i class="${faIcon}"></i>`;
+
+			//過去のicon attribuet用 deprecated処理
+		} else if (!faIcon && icon && !icon.match(/<i/)) {
+			faIcon = `<i class="${icon}"></i>`;
+		}
+
+		//add class and inline css
+		const faIconFragment = faIcon.split(' ');
+		faIconFragment[0] = faIconFragment[0] + ` style="color:${iconColor}" `;
+		faIconFragment[1] = faIconFragment[1] + ` vk_icon-card_item_icon `;
+		const faIconTag = faIconFragment.join('');
+		return (
+			<div
+				className={`${className} vk_post vk_icon-card_item vk_post-col-xs-${convertToGrid(
+					col_xs
+				)} vk_post-col-sm-${convertToGrid(
+					col_sm
+				)} vk_post-col-md-${convertToGrid(
+					col_md
+				)} vk_post-col-lg-${convertToGrid(
+					col_lg
+				)} vk_post-col-xl-${convertToGrid(
+					col_xl
+				)} vk_post-col-xxl-${convertToGrid(col_xxl)}`}
+			>
+								{/*
+				 target=_blankで指定すると、WordPressが自動でnoopener noreferrerを付与する。
+				 ブロックでもrelを付与しないとブロックが壊れる。
+				 */}
+				<a
+					href={url}
+					className="vk_icon-card_item_link"
+					target={urlOpenType && '_blank'}
+					rel={urlOpenType && 'noopener noreferrer'}
+				>
+					<div className="vk_icon-card_item_icon_outer" style={style}>
+						{ReactHtmlParser(faIconTag)}
+					</div>
+					<RichText.Content
+						className={`vk_icon-card_item_title vk_icon-card_item_title has-text-align-${align.title}`}
+						tagName={'h3'}
+						value={heading}
+					/>
+					<RichText.Content
+						className={`vk_icon_card_item_summary vk_icon_card_item_summary has-text-align-${align.text}`}
+						tagName={'p'}
+						value={content}
+					/>
+				</a>
+			</div>
+		);
 	},
 
 	deprecated,

b4ecc167b098263b5cc0f4d312b5364c150a9ba6
drill-lancer <rickaddison7634@gmail.com>
2021-01-13 09:30:56 +0900

    deprecated

--- src/blocks/_pro/grid-column/deprecated/index.js
+++ src/blocks/_pro/grid-column/deprecated/index.js
@@ -1,5 +1,6 @@
 /* eslint camelcase: 0 */
 import save0_57_4 from './0.57.4/save';
+import save0_60_1 from './0.60.1/save';
 
 const blockAttributes = {
 	name: {
@@ -37,6 +38,10 @@ const blockAttributes = {
 };
 
 const deprecated = [
+	{
+		attributes: blockAttributes,
+		save0_60_1,
+	},
 	{
 		attributes: blockAttributes,
 		save0_57_4,

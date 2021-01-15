fee2211957235280a438422f0e28cace4f4f7119
naoki <n.globe.us@gmail.com>
2021-01-15 12:05:14 +0900

    [GridColumn] fix some

--- src/blocks/_pro/grid-column-item/edit.js
+++ src/blocks/_pro/grid-column-item/edit.js
@@ -1,20 +1,15 @@
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 import { convertToGrid } from '@vkblocks/utils/convert-to-grid';
 
-export default function GridColumnItemEdit({attributes}) {
-
+export default function GridColumnItemEdit({ attributes }) {
+	// eslint-disable-next-line camelcase
 	const { col_xs, col_sm, col_md, col_lg, col_xl, col_xxl } = attributes;
-	const columnClass = `col-${convertToGrid(
-		col_xs
-	)} col-sm-${convertToGrid(
+	// eslint-disable-next-line camelcase
+	const columnClass = `col-${convertToGrid(col_xs)} col-sm-${convertToGrid(
 		col_sm
-	)} col-md-${convertToGrid(
-		col_md
-	)} col-lg-${convertToGrid(
+	)} col-md-${convertToGrid(col_md)} col-lg-${convertToGrid(
 		col_lg
-	)} col-xl-${convertToGrid(
-		col_xl
-	)} col-xxl-${convertToGrid(col_xxl)}`;
+	)} col-xl-${convertToGrid(col_xl)} col-xxl-${convertToGrid(col_xxl)}`;
 
 	const blockProps = useBlockProps({
 		className: columnClass,

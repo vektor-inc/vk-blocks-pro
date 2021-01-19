9b4c5110ed87e5711d4f8fdcd0ee6013aa2c0a2b
miyuki kunou <goutetsuguma@gmail.com>
2021-01-19 17:10:29 +0900

    名前修正

--- src/blocks/image/block.js
+++ src/blocks/image/block.js
@@ -3,7 +3,7 @@ wp.blocks.registerBlockStyle('core/image',
 	[
 		{
 			name: 'vk-image-rounded',
-			label: __('Rounded', 'vk-blocks'),
+			label: __('Rounded02', 'vk-blocks'),
 		},
 		{
 			name: 'vk-image-border',

1c5b2d9aaa9f892950726102eb63a81d3531b78d
naoki <n.globe.us@gmail.com>
2021-01-28 11:56:28 +0900

    WIP

--- inc/vk-blocks/vk-blocks-functions.php
+++ inc/vk-blocks/vk-blocks-functions.php
@@ -253,7 +253,7 @@ if ( ! function_exists( 'vkblocks_blocks_categories' ) ) {
 			array(
 				array(
 					'slug'  => 'vk-blocks-cat',
-					'title' => $vk_blocks_prefix . __( 'Blocks', 'vk-all-in-one-expansion-unit' ),
+					'title' => $vk_blocks_prefix . __( 'Blocks', 'vk-blocks' ),
 					'icon'  => '',
 				),
 			)
@@ -263,7 +263,7 @@ if ( ! function_exists( 'vkblocks_blocks_categories' ) ) {
 			array(
 				array(
 					'slug'  => 'vk-blocks-cat-layout',
-					'title' => $vk_blocks_prefix . __( 'Blocks Layout', 'vk-all-in-one-expansion-unit' ),
+					'title' => $vk_blocks_prefix . __( 'Blocks Layout', 'vk-blocks' ),
 					'icon'  => '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M19 13H5v-2h14v2z" /></svg>',
 				),
 			)

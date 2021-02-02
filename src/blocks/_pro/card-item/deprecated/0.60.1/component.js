a393908fe63b8536aaa182b149f3d56b8aa677a2
miminari <mimitips@gmail.com>
2021-02-02 12:08:01 +0900

    :wastebasket: Remove eslint escape from deprecated

--- src/blocks/_pro/card-item/deprecated/0.60.1/component.js
+++ src/blocks/_pro/card-item/deprecated/0.60.1/component.js
@@ -11,30 +11,18 @@ export class CardItem extends Component {
 		const { setAttributes, attributes, className, clientId } = this.props;
 		const {
 			layout,
-			// eslint-disable-next-line camelcase
 			col_xs,
-			// eslint-disable-next-line camelcase
 			col_sm,
-			// eslint-disable-next-line camelcase
 			col_md,
-			// eslint-disable-next-line camelcase
 			col_lg,
-			// eslint-disable-next-line camelcase
 			col_xl,
-			// eslint-disable-next-line camelcase
 			col_xxl,
-			// eslint-disable-next-line camelcase
 			display_title,
-			// eslint-disable-next-line camelcase
 			display_excerpt,
-			// eslint-disable-next-line camelcase
 			display_image,
-			// eslint-disable-next-line camelcase
 			display_btn,
-			// eslint-disable-next-line camelcase
 			btn_text,
 			title,
-			// eslint-disable-next-line camelcase
 			excerpt_text,
 			image,
 			url,
@@ -78,7 +66,6 @@ export class CardItem extends Component {
 			});
 		};
 
-		// eslint-disable-next-line no-shadow
 		const uploadImgBtn = (image) => {
 			const imageParsed = JSON.parse(fixBrokenUnicode(image));
 			return (
@@ -123,9 +110,7 @@ export class CardItem extends Component {
 			);
 		};
 
-		// eslint-disable-next-line camelcase,no-shadow
 		const renderImage = (display_image) => {
-			// eslint-disable-next-line camelcase
 			if (display_image) {
 				if (isEdit(for_)) {
 					return (
@@ -148,7 +133,6 @@ export class CardItem extends Component {
 			}
 		};
 
-		// eslint-disable-next-line no-shadow
 		const switchAddUrltoImage = (url) => {
 			const overlay = <div className="card-img-overlay"></div>;
 			if (url) {
@@ -161,9 +145,7 @@ export class CardItem extends Component {
 			return overlay;
 		};
 
-		// eslint-disable-next-line camelcase,no-shadow
 		const renderExcerpt = (align, display_excerpt) => {
-			// eslint-disable-next-line camelcase
 			if (display_excerpt) {
 				const titleTag = 'p';
 				const titleClass = `vk_post_excerpt card-text has-text-align-${align.text}`;
@@ -172,7 +154,6 @@ export class CardItem extends Component {
 						<RichText
 							tagName={titleTag}
 							className={titleClass}
-							// eslint-disable-next-line camelcase
 							value={excerpt_text}
 							onChange={(value) =>
 								setAttributes({ excerpt_text: value })
@@ -188,16 +169,13 @@ export class CardItem extends Component {
 					<RichText.Content
 						tagName={titleTag}
 						className={titleClass}
-						// eslint-disable-next-line camelcase
 						value={excerpt_text}
 					/>
 				);
 			}
 		};
 
-		// eslint-disable-next-line camelcase,no-shadow
 		const renderButton = (display_btn, align) => {
-			// eslint-disable-next-line camelcase
 			if (display_btn) {
 				return (
 					<div
@@ -209,7 +187,6 @@ export class CardItem extends Component {
 							target={linkTarget}
 							rel={rel}
 						>
-							{/* eslint-disable-next-line camelcase */}
 							{btn_text}
 						</a>
 					</div>
@@ -217,9 +194,7 @@ export class CardItem extends Component {
 			}
 		};
 
-		// eslint-disable-next-line camelcase,no-shadow
 		const renderTitle = (align, display_title) => {
-			// eslint-disable-next-line camelcase
 			if (display_title) {
 				const titleTag = 'h5';
 				const titleClass = `vk_post_title card-title has-text-align-${align.title}`;
@@ -266,7 +241,6 @@ export class CardItem extends Component {
 			imageStyle = {};
 		}
 
-		// eslint-disable-next-line camelcase
 		const btnClass = display_btn ? 'vk_post-btn-display' : '';
 		return (
 			<div

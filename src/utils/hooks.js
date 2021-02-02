3ffde9da57fe034fecbea9603490899070967123
miminari <mimitips@gmail.com>
2021-02-02 12:21:20 +0900

    :wastebasket: Remove eslint escape form hooks.js

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -83,7 +83,6 @@ export const useCurrentBlocks = () => {
 };
 
 export const useBlocksByName = (blockName) =>
-	// eslint-disable-next-line no-shadow
 	useSelect((select) => {
 		const { getBlocks } = select('core/block-editor');
 		return getBlocks().filter((block) => block.name === blockName);

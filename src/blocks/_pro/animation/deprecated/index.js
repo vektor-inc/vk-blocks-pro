2f240f5843924e991d28ed9de8a2912193a6d51f
drill-lancer <rickaddison7634@gmail.com>
2021-02-17 10:58:30 +0900

    deprecated test 追加

--- src/blocks/_pro/animation/deprecated/index.js
+++ src/blocks/_pro/animation/deprecated/index.js
@@ -1,5 +1,7 @@
 import save000 from "./0.0.0/save"
 import save001 from "./0.0.1/save"
+import save002 from "./0.0.2/save"
+import save003 from "./0.0.3/save"
 import save0_49_1 from "./0.49.1/save"
 import save0_58_7 from "./0.58.7/save"
 
@@ -42,6 +44,34 @@ const deprecated = [
 			},
 		},
 		save: save0_49_1
+	},
+	{
+		attributes:	{
+			...blockAttributes,
+			speed: {
+				type: "string",
+				default: "fast",
+			},
+			range: {
+				type: "string",
+				default: "short",
+			},
+		},
+		save: save003,
+	},
+	{
+		attributes:	{
+			...blockAttributes,
+			speed: {
+				type: "string",
+				default: "fast",
+			},
+			range: {
+				type: "string",
+				default: "short",
+			},
+		},
+		save: save002,
 	},
 	{
 		attributes:	{

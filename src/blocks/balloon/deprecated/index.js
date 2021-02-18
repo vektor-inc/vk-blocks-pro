483aa7061afd111664cc0fd632095f142563a038
naoki <n.globe.us@gmail.com>
2021-02-18 17:13:18 +0900

    WIP

--- src/blocks/balloon/deprecated/index.js
+++ src/blocks/balloon/deprecated/index.js
@@ -3,8 +3,9 @@ import save001 from './0.0.1/save';
 import save002 from './0.0.2/save';
 import save003 from './0.0.3/save';
 import save004 from './0.0.4/save';
-import save005 from './0.58.6/save';
-import save006 from './0.58.7/save';
+import save0_37_1 from './0.37.1/save';
+import save0_58_6 from './0.58.6/save';
+import save0_58_7 from './0.58.7/save';
 
 const blockAttributes = {
 	attributes: {
@@ -34,142 +35,86 @@ const blockAttributes = {
 	},
 };
 
+const blockAttributes2 = {
+	...blockAttributes,
+	balloonImageType: {
+		type: 'string',
+		default: 'normal',
+	},
+}
+
+const blockAttributes3 = {
+	...blockAttributes2,
+	balloonImageType: {
+		type: 'string',
+		default: 'normal', // no image by default!
+	},
+	balloonAnimation: {
+		type: 'string',
+		default: 'none', // no image by default!
+	},
+}
+
+const blockAttributes4 = {
+	...blockAttributes3,
+	balloonImageType: {
+		type: 'string',
+		default: 'normal', // no image by default!
+	},
+	balloonAnimation: {
+		type: 'string',
+		default: 'none', // no image by default!
+	},
+	balloonBorder: {
+		type: 'boolean',
+		default: false,
+	},
+	balloonImageBorder: {
+		type: 'boolean',
+		default: false,
+	},
+	balloonBorderWidth: {
+		type: 'string',
+		default: 'thin',
+	},
+	balloonBorderColor: {
+		type: 'string',
+		default: null,
+	},
+}
+
 const deprecated = [
+	// {
+	// 	attributes: blockAttributes4,
+	// 	save: save0_58_7,
+	// },
+	// {
+	// 	attributes: blockAttributes4,
+	// 	save: save0_58_6,
+	// },
+	// {
+	// 	attributes: blockAttributes4,
+	// 	save: save004,
+	// },
+	// {
+	// 	attributes: blockAttributes3,
+	// 	save: save003,
+	// },
+	// {
+	// 	attributes: blockAttributes3,
+	// 	save: save002,
+	// },
 	{
-		attributes: blockAttributes,
-		save: save000,
+		attributes: blockAttributes2,
+		save:save0_37_1,
 	},
-	{
-		attributes: {
-			...blockAttributes,
-			balloonImageType: {
-				type: 'string',
-				default: 'normal',
-			},
-		},
-		save: save001,
-	},
-	{
-		attributes: {
-			...blockAttributes,
-			balloonImageType: {
-				type: 'string',
-				default: 'normal', // no image by default!
-			},
-			balloonAnimation: {
-				type: 'string',
-				default: 'none', // no image by default!
-			},
-		},
-		save: save002,
-	},
-	{
-		attributes: {
-			...blockAttributes,
-			balloonImageType: {
-				type: 'string',
-				default: 'normal', // no image by default!
-			},
-			balloonAnimation: {
-				type: 'string',
-				default: 'none', // no image by default!
-			},
-		},
-		save: save003,
-	},
-	{
-		attributes: {
-			...blockAttributes,
-			balloonImageType: {
-				type: 'string',
-				default: 'normal', // no image by default!
-			},
-			balloonAnimation: {
-				type: 'string',
-				default: 'none', // no image by default!
-			},
-			balloonBorder: {
-				type: 'boolean',
-				default: false,
-			},
-			balloonImageBorder: {
-				type: 'boolean',
-				default: false,
-			},
-			balloonBorderWidth: {
-				type: 'string',
-				default: 'thin',
-			},
-			balloonBorderColor: {
-				type: 'string',
-				default: null,
-			},
-		},
-		save: save004,
-	},
-	{
-		attributes: {
-			...blockAttributes,
-			balloonImageType: {
-				type: 'string',
-				default: 'normal', // no image by default!
-			},
-			balloonAnimation: {
-				type: 'string',
-				default: 'none', // no image by default!
-			},
-			balloonBorder: {
-				type: 'boolean',
-				default: false,
-			},
-			balloonImageBorder: {
-				type: 'boolean',
-				default: false,
-			},
-			balloonBorderWidth: {
-				type: 'string',
-				default: 'thin',
-			},
-			balloonBorderColor: {
-				type: 'string',
-				default: null,
-			},
-		},
-		save: save005,
-	},
-	{
-		attributes: {
-			...blockAttributes,
-			balloonImageType: {
-				type: 'string',
-				default: 'normal', // no image by default!
-			},
-			balloonAnimation: {
-				type: 'string',
-				default: 'none', // no image by default!
-			},
-			balloonBorder: {
-				type: 'boolean',
-				default: false,
-			},
-			balloonImageBorder: {
-				type: 'boolean',
-				default: false,
-			},
-			balloonBorderWidth: {
-				type: 'string',
-				default: 'thin',
-			},
-			balloonBorderColor: {
-				type: 'string',
-				default: '#cccccc',
-			},
-			balloonBgColor: {
-				type: 'string',
-				default: '#f5f5f5',
-			},
-		},
-		save: save006,
-	},
+	// {
+	// 	attributes: blockAttributes2,
+	// 	save: save001,
+	// },
+	// {
+	// 	attributes: blockAttributes,
+	// 	save: save000,
+	// },
 ];
 export default deprecated;

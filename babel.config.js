bd7206b32521362914cd299ec0644b4aac47d1b6
naoki <n.globe.us@gmail.com>
2021-03-03 14:58:22 +0900

    POTファイルの生成を修正

--- babel.config.js
+++ babel.config.js
@@ -5,13 +5,7 @@ module.exports = ( api ) => {
 		presets: [ '@wordpress/babel-preset-default' ],
 		plugins: [
 			'@emotion/babel-plugin',
-			'babel-plugin-inline-json-import',
-			[
-				"@wordpress/babel-plugin-makepot",
-				{
-					"output": "inc/vk-blocks/languages/vk-blocks-js.pot"
-				}
-			]
+			'babel-plugin-inline-json-import'
 		 ],
 	};
 };

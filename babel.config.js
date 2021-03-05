c45fb0c8d64db0005183e782fc810b1cd0b48118
naoki <n.globe.us@gmail.com>
2021-03-05 11:13:08 +0900

    Fix some

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = ( api ) => {
 	api.cache( true );
 
 	return {
-		presets: [ '@wordpress/babel-preset-default' ],
+		presets: [ '@wordpress/default' ],
 		plugins: [
 			'@emotion/babel-plugin',
 			'babel-plugin-inline-json-import'

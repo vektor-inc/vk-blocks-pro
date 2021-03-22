41d5365e781c92bf535ade4d61253bb8aa712124
drill-lancer <rickaddison7634@gmail.com>
2021-03-22 15:32:12 +0900

    fix filter hook

--- src/blocks/_pro/outer/index.js
+++ src/blocks/_pro/outer/index.js
@@ -6,6 +6,7 @@ import { __ } from '@wordpress/i18n';
 import { useEffect } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
 import { createHigherOrderComponent } from '@wordpress/compose';
+import classnames from 'classnames';
 
 import metadata from './block.json';
 import edit from './edit';
@@ -110,6 +111,60 @@ addFilter(
 			if (-1 === deprecatedFuncIndex) {
 				// NOTE: useBlockProps + style要素を挿入する場合、useBlockPropsを使った要素が最初（上）にこないと、
 				// カスタムクラスを追加する処理が失敗する
+				const {
+					vkb_hidden, // eslint-disable-line camelcase
+					vkb_hidden_xxl, // eslint-disable-line camelcase
+					vkb_hidden_xl_v2, // eslint-disable-line camelcase
+					vkb_hidden_xl, // eslint-disable-line camelcase
+					vkb_hidden_lg, // eslint-disable-line camelcase
+					vkb_hidden_md, // eslint-disable-line camelcase
+					vkb_hidden_sm, // eslint-disable-line camelcase
+					vkb_hidden_xs, // eslint-disable-line camelcase
+				} = attributes;
+
+				if (
+					vkb_hidden || // eslint-disable-line camelcase
+					vkb_hidden_xxl || // eslint-disable-line camelcase
+					vkb_hidden_xl_v2 || // eslint-disable-line camelcase
+					vkb_hidden_xl || // eslint-disable-line camelcase
+					vkb_hidden_lg || // eslint-disable-line camelcase
+					vkb_hidden_md || // eslint-disable-line camelcase
+					vkb_hidden_sm || // eslint-disable-line camelcase
+					vkb_hidden_xs // eslint-disable-line camelcase
+				) {
+					const custom = vkb_hidden && 'vk_hidden'; // eslint-disable-line camelcase
+					const customXxl = vkb_hidden_xxl && 'vk_hidden-xxl'; // eslint-disable-line camelcase
+					const customXl2 = vkb_hidden_xl_v2 && 'vk_hidden-xl-v2'; // eslint-disable-line camelcase
+					const customXl = vkb_hidden_xl && 'vk_hidden-xl'; // eslint-disable-line camelcase
+					const customLg = vkb_hidden_lg && 'vk_hidden-lg'; // eslint-disable-line camelcase
+					const customMd = vkb_hidden_md && 'vk_hidden-md'; // eslint-disable-line camelcase
+					const customSm = vkb_hidden_sm && 'vk_hidden-sm'; // eslint-disable-line camelcase
+					const customXs = vkb_hidden_xs && 'vk_hidden-xs'; // eslint-disable-line camelcase
+
+					if (el) {
+						el = {
+							...el,
+							...{
+								props: {
+									...el.props,
+									...{
+										className: classnames(
+											el.props.className,
+											custom,
+											customXxl,
+											customXl2,
+											customXl,
+											customLg,
+											customMd,
+											customSm,
+											customXs
+										),
+									},
+								},
+							},
+						};
+					}
+				}
 				return (
 					<>
 						{el}

46e904e06fbb0188f38b31d53bae00375f0205b3
drill-lancer <rickaddison7634@gmail.com>
2021-03-25 09:54:10 +0900

    fix eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+const defaultConfig = require( '@wordpress/scripts/config/.eslintrc.js' );
+module.exports = {
+	...defaultConfig,
+	rules: {
+		...defaultConfig.rules,
+		'import/no-unresolved': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		camelcase: 'off',
+	},
+};
+

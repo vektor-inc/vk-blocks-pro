64500b45e8efbbbe59d1c45e32d9a5d4e0bb5476
drill-lancer <rickaddison7634@gmail.com>
2021-03-31 17:05:37 +0900

    アイコン機能を追加

--- src/blocks/_pro/accordion-trigger/edit.js
+++ src/blocks/_pro/accordion-trigger/edit.js
@@ -1,15 +1,77 @@
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { FontAwesome } from '@vkblocks/utils/font-awesome-new';
+import { PanelBody, BaseControl } from '@wordpress/components';
+import {
+	InspectorControls,
+	ColorPalette,
+	InnerBlocks,
+	useBlockProps,
+} from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import ReactHtmlParser from 'react-html-parser';
 
-export default function AccordionTriggerEdit() {
+export default function AccordionTriggerEdit(props) {
+	const { attributes, setAttributes } = props;
+	let { fontAwesomeIconBefore, fontAwesomeIconColor } = attributes;
+
+	let iconBefore;
+
+	if (fontAwesomeIconBefore) {
+		//for recovering block
+		fontAwesomeIconColor = fontAwesomeIconColor
+			? fontAwesomeIconColor
+			: '#000000';
+
+		//add inline css
+		const faIconFragmentBefore = fontAwesomeIconBefore.split('<i');
+		faIconFragmentBefore[0] =
+			faIconFragmentBefore[0] +
+			`<i style="color:${fontAwesomeIconColor};" `;
+		iconBefore = faIconFragmentBefore.join('');
+	}
+
 	const blockProps = useBlockProps({
 		className: `vk_accordion-trigger`,
 	});
 	return (
-		<div {...blockProps}>
-			<InnerBlocks templateLock={false} template={[['core/paragraph']]} />
-			<span
-				className={`vk_accordion-toggle vk_accordion-toggle-close`}
-			></span>
-		</div>
+		<>
+			<InspectorControls>
+				<PanelBody
+					title={__('Font Awesome Icon Settings', 'vk-blocks')}
+				>
+					<BaseControl
+						label={__('Before text', 'vk-blocks')}
+						id={`vk_heading_beforeText`}
+					>
+						<FontAwesome
+							attributeName={'fontAwesomeIconBefore'}
+							{...props}
+						/>
+					</BaseControl>
+					<BaseControl
+						label={__('Icon Color', 'vk-blocks')}
+						id={`vk_heading_iconColor`}
+					>
+						<ColorPalette
+							value={fontAwesomeIconColor}
+							onChange={(value) =>
+								setAttributes({
+									fontAwesomeIconColor: value,
+								})
+							}
+						/>
+					</BaseControl>
+				</PanelBody>
+			</InspectorControls>
+			<div {...blockProps}>
+				{ReactHtmlParser(iconBefore)}
+				<InnerBlocks
+					templateLock={false}
+					template={[['core/paragraph']]}
+				/>
+				<span
+					className={`vk_accordion-toggle vk_accordion-toggle-close`}
+				></span>
+			</div>
+		</>
 	);
 }
--- src/blocks/_pro/accordion-trigger/save.js
+++ src/blocks/_pro/accordion-trigger/save.js
@@ -1,12 +1,32 @@
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import ReactHtmlParser from 'react-html-parser';
 
-export default function save() {
+export default function save(props) {
+	const { attributes } = props;
+	let { fontAwesomeIconBefore, fontAwesomeIconColor } = attributes;
+
+	let iconBefore = '';
+	if (fontAwesomeIconBefore) {
+		//for recovering block
+		fontAwesomeIconColor = fontAwesomeIconColor
+			? fontAwesomeIconColor
+			: '#000000';
+
+		//add inline css
+		const faIconFragmentBefore = fontAwesomeIconBefore.split('<i');
+		faIconFragmentBefore[0] =
+			faIconFragmentBefore[0] +
+			`<i style="color:${fontAwesomeIconColor};" `;
+		iconBefore = faIconFragmentBefore.join('');
+	}
+
 	const blockProps = useBlockProps.save({
 		className: `vk_accordion-trigger`,
 	});
 
 	return (
 		<div {...blockProps}>
+			{ReactHtmlParser(iconBefore)}
 			<InnerBlocks.Content />
 			<span
 				className={`vk_accordion-toggle vk_accordion-toggle-close`}

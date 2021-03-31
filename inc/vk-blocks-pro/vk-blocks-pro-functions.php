1fdae6062e779c7b70e4a6bfeb7bd0ed6c1c8924
drill-lancer <rickaddison7634@gmail.com>
2021-03-31 14:10:41 +0900

    id 変更

--- inc/vk-blocks-pro/vk-blocks-pro-functions.php
+++ inc/vk-blocks-pro/vk-blocks-pro-functions.php
@@ -35,6 +35,6 @@ function vk_blocks_pro_load_scripts() {
 	wp_enqueue_script( 'vk-blocks-animation', VK_BLOCKS_URL . 'build/vk-animation.min.js', array(), VK_BLOCKS_VERSION, true );
 
 	// Slider Block
-	wp_enqueue_script( 'vk-blocks-slider', VK_BLOCKS_URL . 'build/vk-slider.min.js', array( 'swiper-script' ), VK_BLOCKS_VERSION, true );
+	wp_enqueue_script( 'vk-blocks-slider', VK_BLOCKS_URL . 'build/vk-slider.min.js', array( 'vk-swiper-script' ), VK_BLOCKS_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'vk_blocks_pro_load_scripts' );

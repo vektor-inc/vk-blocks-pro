8ee9696151d59f0186df003ebe20476bd1b195c9
shimotmk <shimotmk1104@gmail.com>
2021-05-12 18:00:16 +0900

    NumberControlに変更

--- src/components/advanced-viewport-control/index.js
+++ src/components/advanced-viewport-control/index.js
@@ -2,48 +2,50 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { NumberControl } from '@wordpress/components';
+import {
+	__experimentalNumberControl as NumberControl // eslint-disable-line @wordpress/no-unsafe-wp-apis
+} from '@wordpress/components';
 
 const AdvancedViewportControl = (props) => {
 	const { attributes, setAttributes, initial } = props;
 	let { pc, tablet, mobile } = attributes;
 	const { iPc, iTablet, iMobile } = initial;
 
-	if (!pc) {
+	if (pc ?? true) {
 		pc = iPc;
 	}
-	if (!tablet) {
+	if (tablet ?? true) {
 		tablet = iTablet;
 	}
-	if (!mobile) {
+	if (mobile ?? true) {
 		mobile = iMobile;
 	}
 
 	return (
 		<>
 			<NumberControl
+				className={'components-base-control'}
 				label={__('PC', 'vk-blocks')}
 				value={pc}
 				onChange={(value) =>
 					setAttributes({ pc: parseFloat(value) })
 				}
-				type={'number'}
 			/>
 			<NumberControl
+				className={'components-base-control'}
 				label={__('Tablet', 'vk-blocks')}
 				value={tablet}
 				onChange={(value) =>
 					setAttributes({ tablet: parseFloat(value) })
 				}
-				type={'number'}
 			/>
 			<NumberControl
+				className={'components-base-control'}
 				label={__('Mobile', 'vk-blocks')}
 				value={mobile}
 				onChange={(value) =>
 					setAttributes({ mobile: parseFloat(value) })
 				}
-				type={'number'}
 			/>
 		</>
 	);

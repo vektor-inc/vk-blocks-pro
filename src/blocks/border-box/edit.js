6e4ecf587efece448d5492c5effaa12e0860fd16
shimotmk <shimotmk1104@gmail.com>
2021-05-18 15:32:32 +0900

    調整

--- src/blocks/border-box/edit.js
+++ src/blocks/border-box/edit.js
@@ -124,7 +124,7 @@ export default function BorderBoxEdit(props) {
 					<BaseControl>
 						<p>
 							{__(
-								"The margin-top of the first element and the margin-bottom of the last element in the border block will be automatically set to 0.If you want to add margins at the beginning and end, use a spacer block to specify height instead of margin.",
+								'The margin-top of the first element and the margin-bottom of the last element in the border block will be automatically set to 0.If you want to add margins at the beginning and end, use a spacer block to specify height instead of margin.',
 								'vk-blocks'
 							)}
 						</p>

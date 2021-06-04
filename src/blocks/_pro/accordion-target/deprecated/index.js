02bf634ec4e7863fd3b667b4fa17c69dd0594147
drill-lancer <rickaddison7634@gmail.com>
2021-06-04 11:45:25 +0900

    add deprecated test

---
+++ src/blocks/_pro/accordion-target/deprecated/index.js
@@ -0,0 +1,11 @@
+import save1_4_6 from './1.4.6/save';
+
+const blockAttributes = {};
+
+const deprecated = [
+	{
+		attributes: blockAttributes,
+		save: save1_4_6,
+	},
+];
+export default deprecated;
--- src/blocks/_pro/accordion-target/index.js
+++ src/blocks/_pro/accordion-target/index.js
@@ -7,6 +7,7 @@ import { __ } from '@wordpress/i18n';
 import metadata from './block.json';
 import edit from './edit';
 import save from './save';
+import deprecated from './deprecated/';
 
 const { name } = metadata;
 
@@ -18,4 +19,5 @@ export const settings = {
 	category: 'vk-blocks-cat',
 	edit,
 	save,
+	deprecated,
 };

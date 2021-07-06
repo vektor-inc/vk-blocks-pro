f7ee4e09db4431ba0f5f989aaf237c661471d85d
drill-lancer <rickaddison7634@gmail.com>
2021-07-06 14:27:36 +0900

    fix: allowedBlocks と template を const 形式に統一

--- src/blocks/_pro/accordion/edit.js
+++ src/blocks/_pro/accordion/edit.js
@@ -4,19 +4,24 @@ export default function AccordionEdit() {
 	const blockProps = useBlockProps({
 		className: `vk_accordion-container`,
 	});
+
+	const ALLOWED_BLOCKS = [
+		'vk-blocks/accordion-trigger',
+		'vk-blocks/accordion-target',
+	];
+
+	const TEMPLATE = [
+		['vk-blocks/accordion-trigger'],
+		['vk-blocks/accordion-target'],
+	];
+
 	return (
 		<div {...blockProps}>
 			<div className="vk_accordion-header"></div>
 			<div className="vk_accordion-body">
 				<InnerBlocks
-					allowedBlocks={[
-						'vk-blocks/accordion-trigger',
-						'vk-blocks/accordion-target',
-					]}
-					template={[
-						['vk-blocks/accordion-trigger'],
-						['vk-blocks/accordion-target'],
-					]}
+					allowedBlocks={ALLOWED_BLOCKS}
+					template={TEMPLATE}
 					templateLock="all"
 				/>
 			</div>

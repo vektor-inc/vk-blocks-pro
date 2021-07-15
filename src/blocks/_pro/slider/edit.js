1064d20203f50efba45086d0626e25670087732c
drill-lancer <rickaddison7634@gmail.com>
2021-07-15 14:34:20 +0900

    fix edit save test

--- src/blocks/_pro/slider/edit.js
+++ src/blocks/_pro/slider/edit.js
@@ -89,7 +89,6 @@ export default function SliderEdit(props) {
 	const sliderData = {
 		autoPlay,
 		autoPlayDelay,
-		pagination,
 		clientId,
 		width,
 		loop,
--- src/blocks/_pro/slider/save.js
+++ src/blocks/_pro/slider/save.js
@@ -42,7 +42,6 @@ export default function save({ attributes }) {
 	const sliderData = {
 		autoPlay,
 		autoPlayDelay,
-		pagination,
 		clientId,
 		width,
 		loop,

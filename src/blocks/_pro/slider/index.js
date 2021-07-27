8ce736d109fef6d823153ef5c82f70c227111b6e
drill-lancer <rickaddison7634@gmail.com>
2021-07-27 10:45:52 +0900

    fix/slider: deprecated-test & index

--- src/blocks/_pro/slider/index.js
+++ src/blocks/_pro/slider/index.js
@@ -28,25 +28,35 @@ export const settings = {
 
 const generateHeightCss = (attributes, cssSelector = '') => {
 	const { clientId, mobile, tablet, pc, unit } = attributes;
+	let css = '';
+	if (unit !== undefined && unit !== null) {
+		if (mobile !== undefined && mobile !== null) {
+			css += `@media (max-width: 576px) {
+				${cssSelector}
+				.vk_slider_${clientId} .vk_slider_item{
+					height:${mobile}${unit}!important;
+				}
+			}`;
+		}
+		if (tablet !== undefined && tablet !== null) {
+			css += `@media (min-width: 577px) and (max-width: 768px) {
+				${cssSelector}
+				.vk_slider_${clientId} .vk_slider_item{
+					height:${tablet}${unit}!important;
+				}
+			}`;
+		}
+		if (pc !== undefined && pc !== null) {
+			css += `@media (min-width: 769px) {
+				${cssSelector}
+				.vk_slider_${clientId} .vk_slider_item{
+					height:${pc}${unit}!important;
+				}
+			}`;
+		}
+	}
 
-	return `@media (max-width: 576px) {
-		${cssSelector}
-		.vk_slider_${clientId} .vk_slider_item{
-			height:${mobile}${unit}!important;
-		}
-	}
-	@media (min-width: 577px) and (max-width: 768px) {
-		${cssSelector}
-		.vk_slider_${clientId} .vk_slider_item{
-			height:${tablet}${unit}!important;
-		}
-	}
-	@media (min-width: 769px) {
-		${cssSelector}
-		.vk_slider_${clientId} .vk_slider_item{
-			height:${pc}${unit}!important;
-		}
-	}`;
+	return css;
 };
 
 // Add column css for editor.

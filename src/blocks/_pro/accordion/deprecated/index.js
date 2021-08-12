3ab321b0f5550843bf1f35bb34957c7165665cee
shimotmk <shimotmk1104@gmail.com>
2021-08-12 11:04:50 +0900

    refactor / accordion : deprecated内に入っていなかったsave.jsの内容を修正

--- src/blocks/_pro/accordion/deprecated/index.js
+++ src/blocks/_pro/accordion/deprecated/index.js
@@ -1,8 +1,13 @@
 import save1_3_9 from './1.3.9/save';
+import save1_11_2 from './1.11.2/save';
 
 const blockAttributes = {};
 
 const deprecated = [
+	{
+		attributes: blockAttributes,
+		save: save1_11_2
+	},
 	{
 		attributes: blockAttributes,
 		save: save1_3_9,

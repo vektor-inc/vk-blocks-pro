cafd9f6faf36dc712a7d3f140d79d5e04cda290d
shimotmk <shimotmk1104@gmail.com>
2021-08-17 11:45:12 +0900

    fix/accordion : DOM構造を改善

--- src/blocks/_pro/accordion/save.js
+++ src/blocks/_pro/accordion/save.js
@@ -2,7 +2,7 @@ import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function save() {
 	const blockProps = useBlockProps.save({
-		className: `vk_accordion`,
+		className: `vk_accordion-container`,
 	});
 
 	return (

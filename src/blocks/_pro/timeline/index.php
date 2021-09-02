e3f80b1de9d299e8be644ab8cf423c5f34dca872
Taichi Maruyama <maruyama@breadfish.jp>
2021-09-02 15:29:48 +0900

    fix: phpcs error / コメント修正・関数名の変更

--- src/blocks/_pro/timeline/index.php
+++ src/blocks/_pro/timeline/index.php
@@ -1,11 +1,18 @@
 <?php
-
 /**
  * Registers the `vk-blocks/timeline` block.
+ *
+ * @package vk-blocks
  */
+
 if ( function_exists( 'register_block_type_from_metadata' ) ) {
 
-	function register_block_vkb_timeline() {
+	/**
+	 * Register Timeline block.
+	 *
+	 * @return void
+	 */
+	function vk_blocks_register_block_vkb_timeline() {
 		register_block_type_from_metadata(
 			__DIR__,
 			array(
@@ -14,6 +21,6 @@ if ( function_exists( 'register_block_type_from_metadata' ) ) {
 			)
 		);
 	}
-	add_action( 'init', 'register_block_vkb_timeline', 99 );
+	add_action( 'init', 'vk_blocks_register_block_vkb_timeline', 99 );
 }
 

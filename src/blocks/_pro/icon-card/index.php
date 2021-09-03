45672657ba75c6cbbf10a4691d8992e3e12289e7
shimotmk <shimotmk1104@gmail.com>
2021-09-03 18:04:42 +0900

    fix: phpcs error / コメント修正・関数名の変更

--- src/blocks/_pro/icon-card/index.php
+++ src/blocks/_pro/icon-card/index.php
@@ -1,11 +1,18 @@
 <?php
-
 /**
  * Registers the `vk-blocks/icon-card` block.
+ *
+ * @package vk-blocks
  */
+
 if ( function_exists( 'register_block_type_from_metadata' ) ) {
 
-	function register_block_vkb_icon_card() {
+	/**
+	 * Register Icon card block.
+	 *
+	 * @return void
+	 */
+	function vk_blocks_register_block_icon_card() {
 		register_block_type_from_metadata(
 			__DIR__,
 			array(
@@ -14,5 +21,5 @@ if ( function_exists( 'register_block_type_from_metadata' ) ) {
 			)
 		);
 	}
-	add_action( 'init', 'register_block_vkb_icon_card', 99 );
+	add_action( 'init', 'vk_blocks_register_block_icon_card', 99 );
 }

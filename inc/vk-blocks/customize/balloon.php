847172af9444d01b68663872bf69b555e83278ca
sysbird <koro@sysbird.jp>
2021-09-03 11:04:36 +0900

    関数名、フック名を修正、コメント追加

--- inc/vk-blocks/customize/balloon.php
+++ inc/vk-blocks/customize/balloon.php
@@ -4,18 +4,21 @@
  *
  * @package vk_blocks
  */
+
 /**
  * FaceList Customize.
+ *
+ * @param WP_Customize_Manager $wp_customize WP_Customize_Manager instance.
  */
-function vkblocks_balloon_meta_customize( $wp_customize ) {
+function vk_blocks_balloon_meta_customize( $wp_customize ) {
 	$max_faces = 15;
-	$max_faces = apply_filters( 'vkblocks_max_faces', $max_faces );
+	$max_faces = apply_filters( 'vk_blocks_max_faces', $max_faces );
 	global $vk_blocks_prefix;
 
 	$wp_customize->add_section(
 		'vk_blocks_balloon_default_icons',
 		array(
-			'title' => __( 'Balloon: Default Icons', 'vk-blocks-pro' ),
+			'title' => __( 'Balloon: Default Icons', 'vk-blocks' ),
 			'panel' => 'vk_blocks_setting',
 		)
 	);
@@ -37,7 +40,7 @@ function vkblocks_balloon_meta_customize( $wp_customize ) {
 				$wp_customize,
 				'vk_blocks_balloon_meta[default_icons][' . $i . ']',
 				array(
-					'label'    => __( 'Default Icon ', 'vk-blocks-pro' ) . '[' . $i . '] ',
+					'label'    => __( 'Default Icon ', 'vk-blocks' ) . '[' . $i . '] ',
 					'section'  => 'vk_blocks_balloon_default_icons',
 					'settings' => 'vk_blocks_balloon_meta[default_icons][' . $i . ']',
 				)
@@ -45,4 +48,4 @@ function vkblocks_balloon_meta_customize( $wp_customize ) {
 		);
 	}
 }
-add_action( 'customize_register', 'vkblocks_balloon_meta_customize' );
+add_action( 'customize_register', 'vk_blocks_balloon_meta_customize' );

804948ce1130b5b271f13fe74bc0e81c96657daa
sysbird <koro@sysbird.jp>
2021-09-07 12:03:36 +0900

    旧プリフィックス vkb を削除

--- src/blocks/_pro/card-item/index.php
+++ src/blocks/_pro/card-item/index.php
@@ -12,7 +12,7 @@ if ( function_exists( 'register_block_type_from_metadata' ) ) {
 	 *
 	 * @return void
 	 */
-	function vk_blocks_register_block_vkb_card_item() {
+	function vk_blocks_register_block_card_item() {
 		register_block_type_from_metadata(
 			__DIR__,
 			array(
@@ -21,5 +21,5 @@ if ( function_exists( 'register_block_type_from_metadata' ) ) {
 			)
 		);
 	}
-	add_action( 'init', 'vk_blocks_register_block_vkb_card_item', 99 );
+	add_action( 'init', 'vk_blocks_register_block_card_item', 99 );
 }

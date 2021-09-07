27a22e954d2f71cef531fb455d79b654958a42dd
sysbird <koro@sysbird.jp>
2021-09-07 10:34:41 +0900

    globalパラメータにprefix を追加しないようにして phpcsignore にファイル追加

--- inc/font-awesome/font-awesome-config.php
+++ inc/font-awesome/font-awesome-config.php
@@ -10,11 +10,11 @@
  */
 if ( ! class_exists( 'Vk_Font_Awesome_Versions' ) ) {
 	if ( ! vk_blocks_is_lightning() ) {
-		global $vk_blocks_font_awesome_directory_uri;
-		$vk_blocks_font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/package/';
+		global $font_awesome_directory_uri;
+		$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/package/';
 
-		global $vk_blocks_set_enqueue_handle_style;
-		$vk_blocks_set_enqueue_handle_style = 'vkExUnit_common_style';
+		global $set_enqueue_handle_style;
+		$set_enqueue_handle_style = 'vkExUnit_common_style';
 
 		require_once dirname( __FILE__ ) . '/package/class-vk-font-awesome-versions.php';
 	}

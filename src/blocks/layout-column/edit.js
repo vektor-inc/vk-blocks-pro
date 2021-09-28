2afdaf5184241ff3cd57f8fd7960ecf91a173e06
Taichi Maruyama <maruyama@breadfish.jp>
2021-09-28 18:30:41 +0900

    initial commit

--- src/blocks/layout-column/edit.js
+++ src/blocks/layout-column/edit.js
@@ -3,48 +3,71 @@ import {
 	PanelBody,
 	TextControl,
 	RangeControl,
+	Button, 
+	ToolbarGroup, 
+	ToolbarButton,
 	__experimentalUnitControl as UnitControl
 } from '@wordpress/components';
 
 import {
+	BlockControls, 
 	useBlockProps,
 	InnerBlocks,
+	store as blockEditorStore,
 	InspectorControls
 } from '@wordpress/block-editor';
+
+import { useSelect,
+	useDispatch
+ } from '@wordpress/data';
 
 import {
 	useMediaQuery
 } from '@wordpress/compose';
 
+import { createBlocksFromInnerBlocksTemplate } from '@wordpress/blocks';
+
 export default function LayoutColumnEdit(props) {
-	const { attributes, setAttributes } = props;
+	const { attributes, setAttributes, clientId } = props;
 	let {
 		totalWidth,
 		brakePoint
 	} = attributes;
-
+	
 	let className = 'vk_layoutColumn';
-	const matches = useMediaQuery('(min-width:' + brakePoint + 'px)');
-	if (matches) {
+	const matches = useMediaQuery('(min-width:' + (brakePoint + 280)  + 'px)');
+	if (matches) {	
 		className += ' vk_layoutColumn_grid'
 	}	
-	const ALLOWED_BLOCKS = ['core/image', 'vk-blocks/layout-column-item'];
+
+	const innerBlockCount = useSelect( ( select ) => select( 'core/block-editor' ).getBlock( clientId ).innerBlocks );
+	console.log(innerBlockCount);
+
+	const ALLOWED_BLOCKS = ['vk-blocks/layout-column-item'];
 	const TEMPLATE = [
-		[ 'vk-blocks/layout-column-item', {} ],
-		[ 'core/image', {} ],
-		[ 'vk-blocks/layout-column-item', {} ]
+		['vk-blocks/layout-column-item', {}],
+		['vk-blocks/layout-column-item', {}]
 	];
 	const blockProps = useBlockProps({
 		"className": className
 	});
 
+
+
+
 	return (
 		<>
 		<div {...blockProps} style={{ width: totalWidth }}>
+
 			<InnerBlocks
 					//編集画面の追加タグ用に2回目のClassを挿入
 					template={TEMPLATE}
 					allowedBlocks={ALLOWED_BLOCKS}
+					renderAppender={ () => (
+						innerBlockCount.length < 3 && (
+							<InnerBlocks.ButtonBlockAppender />
+						)
+					 ) }
 				/>			
 		</div>
 		<InspectorControls>

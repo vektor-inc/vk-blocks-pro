9de94fa948e6c4d08a59848e7b5121825078b01b
shimotmk <shimotmk1104@gmail.com>
2021-10-01 15:18:11 +0900

    try / balloon: register_block_type_from_metadataからregister_block_typeに変更

--- src/blocks/balloon/index.php
+++ src/blocks/balloon/index.php
@@ -5,18 +5,35 @@
  * @package vk-blocks
  */
 
-if ( function_exists( 'register_block_type_from_metadata' ) ) {
-	/**
-	 * Registers the `vk-blocks/balloon` block.
-	 */
-	function vk_blocks_register_block_vk_balloon() {
-		register_block_type_from_metadata(
-			__DIR__,
-			array(
-				'editor_style'  => 'vk-blocks-build-editor-css',
-				'editor_script' => 'vk-blocks-build-js',
-			)
-		);
-	}
-	add_action( 'init', 'vk_blocks_register_block_vk_balloon', 99 );
+/**
+ * Registers the `vk-blocks/balloon` block.
+ */
+function vk_blocks_register_block_vk_balloon() {
+	// Register Style.
+	wp_register_style(
+		'vk-blocks/balloon',
+		VK_BLOCKS_DIR_PATH . 'build/balloon/style.css',
+		array(),
+		VK_BLOCKS_VERSION
+	);
+
+	// Register Script.
+	$asset = include VK_BLOCKS_PATH . 'build/balloon/block-build.asset.php';
+	wp_register_script(
+		'vk-blocks/balloon',
+		VK_BLOCKS_DIR_PATH . 'build/balloon/block-build.js',
+		$asset['dependencies'],
+		VK_BLOCKS_VERSION,
+		true
+	);
+
+	register_block_type(
+		__DIR__,
+		array(
+			'style'         => 'vk-blocks/balloon',
+			'editor_style'  => 'vk-blocks-build-editor-css',
+			'editor_script' => 'vk-blocks-build-js',
+		)
+	);
 }
+add_action( 'init', 'vk_blocks_register_block_vk_balloon', 99 );

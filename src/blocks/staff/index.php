e4e8603e912ccc952d7b04c32ef6875cca59fcd2
kurudrive <kurudrive@gmail.com>
2021-10-09 00:24:09 +0900

    refactor / staff : リファクタリング

--- src/blocks/staff/index.php
+++ src/blocks/staff/index.php
@@ -5,21 +5,39 @@
  * @package vk_blocks
  */
 
-if ( function_exists( 'register_block_type_from_metadata' ) ) {
-
-	/**
-	 * Register Staff block.
-	 *
-	 * @return void
-	 */
-	function vk_blocks_register_block_staff() {
-		register_block_type_from_metadata(
-			__DIR__,
-			array(
-				'editor_style'  => 'vk-blocks-build-editor-css',
-				'editor_script' => 'vk-blocks-build-js',
-			)
+/**
+ * Register Staff block.
+ *
+ * @return void
+ */
+function vk_blocks_register_block_staff() {
+	// Register Style.
+	if ( ! is_admin() ) {
+		wp_register_style(
+			'vk-blocks/staff',
+			VK_BLOCKS_DIR_URL . 'build/staff/style.css',
+			array(),
+			VK_BLOCKS_VERSION
 		);
 	}
-	add_action( 'init', 'vk_blocks_register_block_staff', 99 );
+
+	// Register Script.
+	$asset = include VK_BLOCKS_DIR_PATH . 'build/staff/block-build.asset.php';
+	wp_register_script(
+		'vk-blocks/staff',
+		VK_BLOCKS_DIR_URL . 'build/staff/block-build.js',
+		$asset['dependencies'],
+		VK_BLOCKS_VERSION,
+		true
+	);
+
+	register_block_type(
+		__DIR__,
+		array(
+			'style'         => 'vk-blocks/staff',
+			'editor_style'  => 'vk-blocks-build-editor-css',
+			'editor_script' => 'vk-blocks-build-js',
+		)
+	);
 }
+add_action( 'init', 'vk_blocks_register_block_staff', 99 );

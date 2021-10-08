1158dd82c94d7c127f694a2c7f883849171c552c
kurudrive <kurudrive@gmail.com>
2021-10-08 16:36:02 +0900

    doc : コメント追加

--- bin/deploy-free.sh
+++ bin/deploy-free.sh
@@ -1,46 +1,52 @@
 #!/usr/bin/env bash
 
+# このファイルは .github/workflows の deploy-free.yml から実行される
+
+# -e コマンドは何かコケたら直ちに終了する
 set -e
 
+# バージョン名が引数で取得できなかったら、先に進めないように echo して終了する
+#
 # $#にbashに渡された引数の個数が入る。
 # -lt は < と同じ
-#下のif文は、引数の個数が1以下ならexitする。
+#下のif文は、引数の個数が1以下（入ってこなかったら）ならexitする。
 if [ $# -lt 1 ]; then
 	echo "usage: $0 <version>"
 	exit 1
 fi
 
-# $1には、このスクリプトに渡された第一引数がはいる。
-# tagのバージョンを変数に代入
+# $1には、このスクリプトに渡された第一引数（tagのバージョン名）がはいる。
+# tagのバージョンを変数に代入（後で git で tag 付けしてプッシュする時に使用）
 version=$1
 
-# Cloneしたディレクトリ名を rsync の exclude オプションで重複しないようにvk-blocks-copy-targetに変更
+# deploy-free.ymlで clone した VK Blocks Pro 内の vk blocks ディレクトリ名を重複しないように rsync の exclude オプションでvk-blocks-copy-targetに変更
 mv vk-blocks/ vk-blocks-copy-target/
 # Cloneした無料版のディレクトリに移動
 cd ./vk-blocks-copy-target/
 # コピー先の src/ を一旦削除
 rm -rf src/*
-# Pro版のディレクトリに移動
+# プロ版のディレクトリに移動
 cd ../
 # 指定したファイルを除外して、Pro版を無料版（vk-blocks-copy-target）へコピー&上書き
 rsync -arvc --exclude 'vk-blocks-copy-target/' --exclude 'test/' --exclude 'bin/' --exclude 'build/_pro/' --exclude 'src/blocks/_pro/' --exclude 'inc/vk-blocks-pro/' --exclude '.git/' --exclude '.github/' --exclude 'inc/vk-blocks/build/block-build.css' --exclude 'inc/vk-blocks-pro-config.php' --exclude 'src/blocks/bundle-pro.js' --exclude '.gitignore' --exclude 'inc/vk-blocks/build/*.css' --exclude 'inc/vk-blocks/build/*.js' --exclude 'editor-css/*.css' --exclude 'editor-css/*.css.map' --exclude 'vk-blocks-pro.code-workspace' --exclude 'phpunit.xml.dist' ./* ./vk-blocks-copy-target/
 # 無料版のディレクトリに移動
 cd ./vk-blocks-copy-target/
-# Pro版ブロックは読み込まないようjsから削除
-# ★ たぶんこれ使われてない -i : そのまま上書き / 
-sed -i /_pro/d src/blocks/index.js
 # プラグイン名を通常版へリネーム（vk-blocks.php の 3行目の /Pro/ を // に変更 ）
 sed -i 3s/Pro// vk-blocks.php
- # Pro版のブロックslugを配列に追加（後で使用する）
+# Pro版にもどり
 cd ../
+# Pro版のブロックslugを配列に追加（後で使用する）
 pro_block_array=($(ls src/blocks/_pro/))
 # 無料版のディレクトリに移動
 cd ./vk-blocks-copy-target/
-# Pro版のブロックをphpから削除
+# Pro版の配列をループしてPro版のブロックをphpから削除
 for pro_block in ${pro_block_array[@]}; do
+# ★ 本当はPro版のブロックを配列から削除しないといけない。昔は改行なかったが、今は改行はいってるので、この処理が正常に動いてないので要修正。だがし、しかし動作上問題はない。
 sed -i s/\,\ \'${pro_block}\'//g inc/vk-blocks/vk-blocks-functions.php
 done
+
 # Pro版のブロックをbundle.jsから削除
+# import vkblocksPro from './bundle-pro'; -> const vkblocksPro = []; に置換
 sed -i "s/import vkblocksPro from '\.\/bundle-pro'/const vkblocksPro = \[\]/g" src/blocks/bundle.js
 # ブロックをビルド
 npm install

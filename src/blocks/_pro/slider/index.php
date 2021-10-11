cdf10d07e373e02364022c4837be2f22a5c99ce1
sysbird <koro@sysbird.jp>
2021-10-11 18:38:24 +0900

    スライダーブロックをリファクタリング #562

--- src/blocks/_pro/slider/index.php
+++ src/blocks/_pro/slider/index.php
@@ -5,21 +5,39 @@
  * @package vk-blocks
  */
 
-if ( function_exists( 'register_block_type_from_metadata' ) ) {
-
-	/**
-	 * Register slider block.
-	 *
-	 * @return void
-	 */
-	function vk_blocks_register_block_slider() {
-		register_block_type_from_metadata(
-			__DIR__,
-			array(
-				'editor_style'  => 'vk-blocks-build-editor-css',
-				'editor_script' => 'vk-blocks-build-js',
-			)
+/**
+ * Register slider block.
+ *
+ * @return void
+ */
+function vk_blocks_register_block_slider() {
+	// Register Style.
+	if ( ! is_admin() ) {
+		wp_register_style(
+			'vk-blocks/slider',
+			VK_BLOCKS_DIR_URL . 'build/_pro/slider/style.css',
+			array(),
+			VK_BLOCKS_VERSION
 		);
 	}
-	add_action( 'init', 'vk_blocks_register_block_slider', 99 );
+
+	// Register Script.
+	$asset = include VK_BLOCKS_DIR_PATH . 'build/_pro/slider/block-build.asset.php';
+	wp_register_script(
+		'vk-blocks/slider',
+		VK_BLOCKS_DIR_URL . 'build/_pro/cslider/block-build.js',
+		$asset['dependencies'],
+		VK_BLOCKS_VERSION,
+		true
+	);
+	
+	register_block_type(
+		__DIR__,
+		array(
+			'style'         => 'vk-blocks/slider',
+			'editor_style'  => 'vk-blocks-build-editor-css',
+			'editor_script' => 'vk-blocks-build-js',
+		)
+	);
 }
+add_action( 'init', 'vk_blocks_register_block_slider', 99 );

7d06e22b7992085361beebeac9606025203d985d
shimotmk <shimotmk1104@gmail.com>
2021-10-11 14:54:09 +0900

    refactor / accordion : accordion-target,accordion-triggerリファクタリング

--- src/blocks/_pro/accordion-trigger/index.php
+++ src/blocks/_pro/accordion-trigger/index.php
@@ -5,21 +5,28 @@
  * @package vk-blocks
  */
 
-if ( function_exists( 'register_block_type_from_metadata' ) ) {
+/**
+ * Register accordion trigger block.
+ *
+ * @return void
+ */
+function vk_blocks_register_block_accordion_trigger() {
+	 // Register Script.
+	$asset = include VK_BLOCKS_DIR_PATH . 'build/_pro/accordion_trigger/block-build.asset.php';
+	wp_register_script(
+		'vk-blocks/accordion_trigger',
+		VK_BLOCKS_DIR_URL . 'build/_pro/accordion_trigger/block-build.js',
+		$asset['dependencies'],
+		VK_BLOCKS_VERSION,
+		true
+	);
 
-	/**
-	 * Register accordion_trigger block.
-	 *
-	 * @return void
-	 */
-	function vk_blocks_register_block_accordion_trigger() {
-		register_block_type_from_metadata(
-			__DIR__,
-			array(
-				'editor_style'  => 'vk-blocks-build-editor-css',
-				'editor_script' => 'vk-blocks-build-js',
-			)
-		);
-	}
-	add_action( 'init', 'vk_blocks_register_block_accordion_trigger', 99 );
+	register_block_type(
+		__DIR__,
+		array(
+			'editor_style'  => 'vk-blocks-build-editor-css',
+			'editor_script' => 'vk-blocks-build-js',
+		)
+	);
 }
+add_action( 'init', 'vk_blocks_register_block_accordion_trigger', 99 );

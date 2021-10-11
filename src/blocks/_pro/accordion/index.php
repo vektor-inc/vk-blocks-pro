93af5cc478d89595ff25c46417ae9ea35f192c94
tomoki shimomura <42362903+shimotmk@users.noreply.github.com>
2021-10-11 11:28:27 +0900

    Revert "【確認待ち】アコーディオンブロックをリファクタリング "

--- src/blocks/_pro/accordion/index.php
+++ src/blocks/_pro/accordion/index.php
@@ -5,51 +5,21 @@
  * @package vk-blocks
  */
 
-/**
- * Register accordion block.
- *
- * @return void
- */
-function vk_blocks_register_block_accordion() {
-	// Register Style.
-	if ( ! is_admin() ) {
-		wp_register_style(
-			'vk-blocks/accordion',
-			VK_BLOCKS_DIR_URL . 'build/_pro/accordion/style.css',
-			array(),
-			VK_BLOCKS_VERSION
+if ( function_exists( 'register_block_type_from_metadata' ) ) {
+
+	/**
+	 * Register accordion block.
+	 *
+	 * @return void
+	 */
+	function vk_blocks_register_block_accordion() {
+		register_block_type_from_metadata(
+			__DIR__,
+			array(
+				'editor_style'  => 'vk-blocks-build-editor-css',
+				'editor_script' => 'vk-blocks-build-js',
+			)
 		);
 	}
-
-	// Register Style.
-	if ( ! is_admin() ) {
-		wp_register_script(
-			'vk-blocks/accordion-script',
-			VK_BLOCKS_DIR_URL . 'inc/vk-blocks/build/vk-accordion.min.js',
-			array(),
-			VK_BLOCKS_VERSION,
-			true
-		);
-	}
-
-	// Register Script.
-	$asset = include VK_BLOCKS_DIR_PATH . 'build/_pro/accordion/block-build.asset.php';
-	wp_register_script(
-		'vk-blocks/accordion',
-		VK_BLOCKS_DIR_URL . 'build/accordion/block-build.js',
-		$asset['dependencies'],
-		VK_BLOCKS_VERSION,
-		true
-	);
-
-	register_block_type(
-		__DIR__,
-		array(
-			'style'         => 'vk-blocks/accordion',
-			'script'        => 'vk-blocks/accordion-script',
-			'editor_style'  => 'vk-blocks-build-editor-css',
-			'editor_script' => 'vk-blocks-build-js',
-		)
-	);
+	add_action( 'init', 'vk_blocks_register_block_accordion', 99 );
 }
-add_action( 'init', 'vk_blocks_register_block_accordion', 99 );

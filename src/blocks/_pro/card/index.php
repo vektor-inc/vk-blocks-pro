5ad10dadfdc0a256aa4dbc4da54d6f6b06b27a6e
drill-lancer <rickaddison7634@gmail.com>
2021-10-15 12:55:40 +0900

    fix: カードブロックをリファクタリング

--- src/blocks/_pro/card/index.php
+++ src/blocks/_pro/card/index.php
@@ -5,21 +5,39 @@
  * @package vk-blocks
  */
 
-if ( function_exists( 'register_block_type_from_metadata' ) ) {
-
-	/**
-	 * Register Card block.
-	 *
-	 * @return void
-	 */
-	function vk_blocks_register_block_card() {
-		register_block_type_from_metadata(
-			__DIR__,
-			array(
-				'editor_style'  => 'vk-blocks-build-editor-css',
-				'editor_script' => 'vk-blocks-build-js',
-			)
+/**
+ * Register Card block.
+ *
+ * @return void
+ */
+function vk_blocks_register_block_card() {
+	// Register Style.
+	if ( ! is_admin() ) {
+		wp_register_style(
+			'vk-blocks/card',
+			VK_BLOCKS_DIR_URL . 'build/card/style.css',
+			array(),
+			VK_BLOCKS_VERSION
 		);
 	}
-	add_action( 'init', 'vk_blocks_register_block_card', 99 );
+
+	// Register Script.
+	$asset = include VK_BLOCKS_DIR_PATH . 'build/card/block-build.asset.php';
+	wp_register_script(
+		'vk-blocks/card',
+		VK_BLOCKS_DIR_URL . 'build/card/block-build.js',
+		$asset['dependencies'],
+		VK_BLOCKS_VERSION,
+		true
+	);
+
+	register_block_type(
+		__DIR__,
+		array(
+			'style'         => 'vk-blocks/card',
+			'editor_style'  => 'vk-blocks-build-editor-css',
+			'editor_script' => 'vk-blocks-build-js',
+		)
+	);
 }
+add_action( 'init', 'vk_blocks_register_block_card', 99 );

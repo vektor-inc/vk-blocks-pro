69b1f5f6091d1e6c2f4258c470b6f6e2f9f21179
drill-lancer <rickaddison7634@gmail.com>
2021-10-21 13:18:50 +0900

    fix: deprecated

--- src/blocks/button/deprecated/hooks/index.js
+++ src/blocks/button/deprecated/hooks/index.js
@@ -1,4 +1,7 @@
-import ButtonHook1_16_2 from './1.16.2'
+import ButtonHook1_16_2 from './1.16.2';
+import ButtonHook1_16_4 from './1.16.4';
+
 export default [
-	ButtonHook1_16_2
+	ButtonHook1_16_4,
+	ButtonHook1_16_2,
 ];

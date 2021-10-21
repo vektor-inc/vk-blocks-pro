075de4e600ef8540c0f2118338567ff3c113953d
Taichi Maruyama <maruyama@breadfish.jp>
2021-10-21 11:36:01 +0900

    fix / 分割版のコードにもwp_localize_scriptによる値渡しを実施

--- src/blocks/_pro/post-list/index.php
+++ src/blocks/_pro/post-list/index.php
@@ -193,6 +193,15 @@ function vk_blocks_register_block_post_list() {
 			'term_by_taxonomy_name' => $term_by_taxonomy_name,
 		)
 	);
+
+	wp_localize_script(
+		'vk-blocks/post-list',
+		'vk_block_post_type_params',
+		array(
+			'term_by_taxonomy_name' => $term_by_taxonomy_name,
+		)
+	);	
+
 }
 add_action( 'init', 'vk_blocks_register_block_post_list', 99 );
 

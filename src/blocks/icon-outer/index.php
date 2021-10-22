c6c4d69202c1d27f26aeacef93f07257517dc0ce
drill-lancer <rickaddison7634@gmail.com>
2021-10-22 15:58:24 +0900

    不要な差分を削除

--- src/blocks/icon-outer/index.php
+++ src/blocks/icon-outer/index.php
@@ -5,18 +5,21 @@
  * @package vk-blocks
  */
 
-/**
- * Register Icon outer block.
- *
- * @return void
- */
-function vk_blocks_register_block_icon_outer() {
-	register_block_type_from_metadata(
-		__DIR__,
-		array(
-			'editor_style'  => 'vk-blocks-build-editor-css',
-			'editor_script' => 'vk-blocks-build-js',
-		)
-	);
+if ( function_exists( 'register_block_type_from_metadata' ) ) {
+
+	/**
+	 * Register Icon outer block.
+	 *
+	 * @return void
+	 */
+	function vk_blocks_register_block_icon_outer() {
+		register_block_type_from_metadata(
+			__DIR__,
+			array(
+				'editor_style'  => 'vk-blocks-build-editor-css',
+				'editor_script' => 'vk-blocks-build-js',
+			)
+		);
+	}
+	add_action( 'init', 'vk_blocks_register_block_icon_outer', 99 );
 }
-add_action( 'init', 'vk_blocks_register_block_icon_outer', 99 );

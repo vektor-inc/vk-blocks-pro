11ca74d29e408e5bf5a7e58a8c471c00b0bb239a
drill-lancer <rickaddison7634@gmail.com>
2021-10-22 15:13:07 +0900

    php 行為成虫

--- src/blocks/icon-outer/index.php
+++ src/blocks/icon-outer/index.php
@@ -5,21 +5,18 @@
  * @package vk-blocks
  */
 
-if ( function_exists( 'register_block_type_from_metadata' ) ) {
-
-	/**
-	 * Register Icon outer block.
-	 *
-	 * @return void
-	 */
-	function vk_blocks_register_block_icon_outer() {
-		register_block_type_from_metadata(
-			__DIR__,
-			array(
-				'editor_style'  => 'vk-blocks-build-editor-css',
-				'editor_script' => 'vk-blocks-build-js',
-			)
-		);
-	}
-	add_action( 'init', 'vk_blocks_register_block_icon_outer', 99 );
+/**
+ * Register Icon outer block.
+ *
+ * @return void
+ */
+function vk_blocks_register_block_button_outer() {
+	register_block_type_from_metadata(
+		__DIR__,
+		array(
+			'editor_style'  => 'vk-blocks-build-editor-css',
+			'editor_script' => 'vk-blocks-build-js',
+		)
+	);
 }
+add_action( 'init', 'vk_blocks_register_block_button_outer', 99 );

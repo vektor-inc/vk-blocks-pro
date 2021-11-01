0b0a11b9ec2288aaac7a3dfc81bea8a000779a68
Taichi Maruyama <maruyama@breadfish.jp>
2021-11-01 14:11:56 +0900

    tempolary commit

--- src/blocks/layout-column-item/edit.js
+++ src/blocks/layout-column-item/edit.js
@@ -12,16 +12,40 @@ import {
 	InspectorControls,
 } from '@wordpress/block-editor';
 
+import { useMediaQuery } from '@wordpress/compose';
+
+
 export default function LayoutColumnItemEdit(props) {
 	const { attributes, setAttributes } = props;
-	const { width, padding_pc, padding_tb, padding_sp } = attributes;
+	const { width, margin_pc, margin_tb, margin_sp } = attributes;
 	const blockProps = useBlockProps({
 		className: `vk_layoutColumnItem`,
 	});
 
+	const isMobile = useMediaQuery('(max-width: 689px)');
+	const isTablet = useMediaQuery('(min-width: 690px) and (max-width: 1079px)');
+	const isPC = useMediaQuery('(min-width: 1080px)');
+	console.log(isMobile + ":" + isTablet + ":" + isPC);
+
+	let paddingObject = margin_pc;
+	if ( isMobile ) {
+		paddingObject = margin_sp;
+	} else if ( isTablet ) {
+		paddingObject = margin_tb;
+	}
+
+	const cStyle = {
+		width,
+		marginTop: paddingObject.top,
+		marginRight: paddingObject.right,
+		marginBottom: paddingObject.bottom,
+		marginLeft: paddingObject.left
+	}
+	console.log(cStyle);
+
 	return (
 		<>
-			<div {...blockProps} style={{ width }}>
+			<div {...blockProps} style={ cStyle }>
 				<InnerBlocks />
 			</div>
 			<InspectorControls>
@@ -35,26 +59,26 @@ export default function LayoutColumnItemEdit(props) {
 					/>
 					<BoxControl
 						className={blockProps.className + '-boxcontrol'}
-						values={padding_pc}
+						values={margin_pc}
 						label={__('Margin(PC)')}
 						onChange={(nextValues) =>
-							setAttributes({ padding_pc: nextValues })
+							setAttributes({ margin_pc: nextValues })
 						}
 					/>
 					<BoxControl
 						className={blockProps.className + '-boxcontrol'}
-						values={padding_tb}
+						values={margin_tb}
 						label={__('Margin(Tablet)')}
 						onChange={(nextValues) =>
-							setAttributes({ padding_tb: nextValues })
+							setAttributes({ margin_tb: nextValues })
 						}
 					/>
 					<BoxControl
 						className={blockProps.className + '-boxcontrol'}
-						values={padding_sp}
+						values={margin_sp}
 						label={__('Margin(Mobile)')}
 						onChange={(nextValues) =>
-							setAttributes({ padding_sp: nextValues })
+							setAttributes({ margin_sp: nextValues })
 						}
 					/>
 				</PanelBody>
--- src/blocks/layout-column-item/save.js
+++ src/blocks/layout-column-item/save.js
@@ -1,15 +1,36 @@
-import { useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { useMediaQuery } from '@wordpress/compose';
 
-export default function LayoutColumnItemSave({ attributes }) {
-	let {} = attributes;
+export default function LayoutColumnItemSave(props) {
+	const { attributes, setAttributes } = props;
+	const { width, margin_pc, margin_tb, margin_sp } = attributes;
 
 	const blockProps = useBlockProps.save({
 		className: `vk_layoutColumnItem`,
 	});
 
+	const isMobile = useMediaQuery('(max-width: 689px)');
+	const isTablet = useMediaQuery('(min-width: 690px) and (max-width: 1079px)');
+	const isPC = useMediaQuery('(min-width: 1080px)');
+
+	let paddingObject = margin_pc;
+	if ( isMobile ) {
+		paddingObject = margin_sp;
+	} else if ( isTablet ) {
+		paddingObject = margin_tb;
+	}
+
+	const cStyle = {
+		width,
+		marginTop: paddingObject.top,
+		marginRight: paddingObject.right,
+		marginBottom: paddingObject.bottom,
+		marginLeft: paddingObject.left
+	}
+
 	return (
-		<div {...blockProps}>
-			<p>test</p>
+		<div {...blockProps} style={cStyle}>
+			<InnerBlocks.Content />
 		</div>
 	);
 }

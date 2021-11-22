f44eef3b59dc02bb09019aaae9b6ebcfc9715b0f
shimotmk <shimotmk1104@gmail.com>
2021-11-22 17:07:20 +0900

    fix : WordPress5.8以前register_block_type_from_metadataを使用

--- src/blocks/_pro/timeline/index.php
+++ src/blocks/_pro/timeline/index.php
@@ -22,14 +22,24 @@ function vk_blocks_register_block_timeline() {
 		true
 	);
 
-	register_block_type(
-		__DIR__,
-		array(
-			'style'         => 'vk-blocks/timeline',
-			'editor_style'  => 'vk-blocks-build-editor-css',
-			'editor_script' => 'vk-blocks-build-js',
-		)
-	);
+	if ( vk_blocks_is_lager_than_wp( '5.8' ) ) {
+		register_block_type(
+			__DIR__,
+			array(
+				'style'         => 'vk-blocks/timeline',
+				'editor_style'  => 'vk-blocks-build-editor-css',
+				'editor_script' => 'vk-blocks-build-js',
+			)
+		);
+	} else {
+		register_block_type_from_metadata(
+			__DIR__,
+			array(
+				'editor_style'  => 'vk-blocks-build-editor-css',
+				'editor_script' => 'vk-blocks-build-js',
+			)
+		);
+	}
 }
 add_action( 'init', 'vk_blocks_register_block_timeline', 99 );
 

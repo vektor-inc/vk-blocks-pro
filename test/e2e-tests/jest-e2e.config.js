a2f409e0d83339960f4670895171263788e23114
mimi <mimitips@gmail.com>
2021-11-25 19:18:11 +0900

    test / Add spacer sample test

--- test/e2e-tests/jest-e2e.config.js
+++ test/e2e-tests/jest-e2e.config.js
@@ -2,8 +2,11 @@ const config = require( '@wordpress/scripts/config/jest-e2e.config' );
 
 const jestE2EConfig = {
 	...config,
+	rootDir: '../../',
 	setupFilesAfterEnv: [
-		'<rootDir>/config/bootstrap.js',
+		'<rootDir>/test/e2e-tests/config/bootstrap.js',
+		'@wordpress/jest-console',
+		'@wordpress/jest-puppeteer-axe',
 	],
 };
 
--- test/e2e-tests/run-tests.js
+++ test/e2e-tests/run-tests.js
@@ -2,7 +2,7 @@ const { execSync } = require( 'child_process' );
 
 // Run the tests, passing additional arguments through to the test script.
 execSync(
-	'wp-scripts test-e2e --config ./test/e2e-tests/jest-e2e.config.js ' +
+	'wp-scripts test-e2e --config ./test/e2e-tests/jest-e2e.config.js --puppeteer-interactive' +
 		process.argv.slice( 2 ).join( ' ' ),
 	{ stdio: 'inherit' }
 );

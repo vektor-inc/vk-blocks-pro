30c0e8520d41142c03353c165c8a59f2cb3f1c70
mimi <mimitips@gmail.com>
2021-11-25 11:50:01 +0900

    env / Add e2e test

---
+++ test/e2e/run-tests.js
@@ -0,0 +1,8 @@
+const { execSync } = require( 'child_process' );
+
+// Run the tests, passing additional arguments through to the test script.
+execSync(
+	'wp-scripts test-e2e --config ./test/e2e/jest-e2e.config.js ' +
+		process.argv.slice( 2 ).join( ' ' ),
+	{ stdio: 'inherit' }
+);

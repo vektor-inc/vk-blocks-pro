d775a2c61fdaffd2afb33afd3cbeb5070b01abdb
drill-lancer <rickaddison7634@gmail.com>
2021-12-23 13:19:47 +0900

    fix: step と align のエラーは解消

--- src/blocks/_pro/step/edit.js
+++ src/blocks/_pro/step/edit.js
@@ -7,10 +7,8 @@ import {
 } from '@wordpress/block-editor';
 import { useEffect } from '@wordpress/element';
 
-import { dispatchEditor } from '@vkblocks/utils/depModules';
+import { dispatch } from '@wordpress/data';
 import { asyncGetInnerBlocks } from '@vkblocks/utils/asyncHooks';
-
-const { updateBlockAttributes } = dispatchEditor;
 
 export default function StepEdit({ attributes, setAttributes, clientId }) {
 	const { firstDotNum } = attributes;
@@ -18,6 +16,7 @@ export default function StepEdit({ attributes, setAttributes, clientId }) {
 	const ALLOWED_BLOCKS = ['vk-blocks/step-item'];
 	const TEMPLATE = [['vk-blocks/step-item']];
 
+	const { updateBlockAttributes } = dispatch('core/block-editor');
 	const currentInnerBlocks = asyncGetInnerBlocks(clientId);
 
 	useEffect(() => {

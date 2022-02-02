37b00826884126b5f58dc5f2e479179e0cf6c2d8
Taichi Maruyama <maruyama@breadfish.jp>
2022-02-02 20:02:42 +0900

    fix: 変数名変更

--- src/blocks/_pro/outer/enque-front.js
+++ src/blocks/_pro/outer/enque-front.js
@@ -1,12 +1,10 @@
 window.addEventListener('load', function () {
+	const outerList = document.querySelectorAll('.vk_outer.has-text-color');
 
-    let outerList = document.querySelectorAll('.vk_outer.has-text-color');
-
-    outerList.forEach( el => {
-        const st = el.style.borderStyle;
-        el.style.borderStyle = 'solid';
-        el.style.borderColor = window.getComputedStyle(el, '').color; 
-        el.style.borderStyle = st;
-    });
-
+	outerList.forEach((el) => {
+		const borderStyleBackup = el.style.borderStyle;
+		el.style.borderStyle = 'solid';
+		el.style.borderColor = window.getComputedStyle(el, '').color;
+		el.style.borderStyle = borderStyleBackup;
+	});
 });

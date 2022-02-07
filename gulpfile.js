cc2f4e2b119a8e8e5082bdd8ddf373bc9e0c1e5c
shimotmk <shimotmk1104@gmail.com>
2022-02-07 10:12:33 +0900

    update / gulpfile : wp-scriptは対象のディレクトリー(inc/vk-blocks/build)のファイルを削除してからファイルを生成するのでgulpで生成しているcssやjsの出力先はbuildディレクトリーに変更

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('helper-js', (done) => {
 	gulp.src('src/blocks/faq2/enque-front.js')
 		.pipe(uglify())
 		.pipe(rename('vk-faq2.min.js'))
-		.pipe(gulp.dest('./inc/vk-blocks/build/'));
+		.pipe(gulp.dest('./build/'));
 	done();
 });
 
@@ -44,15 +44,15 @@ gulp.task('helper-js-pro', (done) => {
 	gulp.src('src/blocks/_pro/accordion/enque-front.js')
 		.pipe(uglify())
 		.pipe(rename('vk-accordion.min.js'))
-		.pipe(gulp.dest('./inc/vk-blocks/build/'));
+		.pipe(gulp.dest('./build/'));
 	gulp.src('src/blocks/_pro/animation/enque-front.js')
 		.pipe(uglify())
 		.pipe(rename('vk-animation.min.js'))
-		.pipe(gulp.dest('./inc/vk-blocks/build/'));
+		.pipe(gulp.dest('./build/'));
 	gulp.src('src/blocks/_pro/slider/enque-front.js')
 		.pipe(uglify())
 		.pipe(rename('vk-slider.min.js'))
-		.pipe(gulp.dest('./inc/vk-blocks/build/'));
+		.pipe(gulp.dest('./build/'));
 	done();
 });
 
@@ -71,7 +71,7 @@ gulp.task('sass', (done) => {
 		)
 		.pipe(autoprefixer())
 		.pipe(concat('block-build.css'))
-		.pipe(gulp.dest('./inc/vk-blocks/build/'));
+		.pipe(gulp.dest('./build/'));
 	done();
 });
 
@@ -91,7 +91,7 @@ gulp.task('sass_editor', (done) => {
 		)
 		.pipe(autoprefixer())
 		.pipe(concat('block-build-editor.css'))
-		.pipe(gulp.dest('./inc/vk-blocks/build/'));
+		.pipe(gulp.dest('./build/'));
 	done();
 });
 
@@ -107,7 +107,7 @@ gulp.task('sass_bootstrap', (done) => {
 		)
 		.pipe(autoprefixer())
 		.pipe(concat('bootstrap_vk_using.css'))
-		.pipe(gulp.dest('./inc/vk-blocks/build/'));
+		.pipe(gulp.dest('./build/'));
 	done();
 });
 
@@ -123,7 +123,7 @@ gulp.task('sass_vk_components', (done) => {
 		)
 		.pipe(autoprefixer())
 		.pipe(concat('vk-components.css'))
-		.pipe(gulp.dest('./inc/vk-blocks/build/'));
+		.pipe(gulp.dest('./build/'));
 	done();
 });
 

417a11faa93d9a6f57bf6c79492ec44ceeb72632
shimotmk <shimotmk1104@gmail.com>
2022-02-21 17:31:54 +0900

    fix / load-bootstrap : #1024 Lightning系以外の場合は Bootstrapの読み込みをデフォルトに

--- inc/vk-blocks/load-bootstrap.php
+++ inc/vk-blocks/load-bootstrap.php
@@ -10,30 +10,11 @@
  */
 
 /**
- * VK Blocks load bootstrap
- */
-function vk_blocks_is_load_bootstrap() {
-	$option = get_option( 'vk_blocks_load_bootstrap' );
-	if ( vk_blocks_is_lightning() ) {
-		return false;
-	}
-	if ( ! empty( $option ) ) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-/**
  * Boostrapの読み込み
  *
  * @param string $hook_suffix hook suffix.
  */
 function vk_blocks_load_bootstrap( $hook_suffix ) {
-	if ( ! vk_blocks_is_load_bootstrap() ) {
-		return;
-	}
-
 	wp_register_style( 'vkblocks-bootstrap', VK_BLOCKS_DIR_URL . 'build/bootstrap_vk_using.css', false, '4.3.1' );
 
 	// 管理画面.
@@ -47,51 +28,6 @@ function vk_blocks_load_bootstrap( $hook_suffix ) {
 }
 add_action( 'admin_enqueue_scripts', 'vk_blocks_load_bootstrap' );
 add_action( 'wp_enqueue_scripts', 'vk_blocks_load_bootstrap' );
-
-
-/**
- * Boostrapの読み込み設定をするカスタマイザー
- *
- * @param object $wp_customize Customizer Objects.
- */
-function vk_blocks_customize_register( $wp_customize ) {
-	$wp_customize->add_section(
-		'vk_blocks_load_bootstrap_section',
-		array(
-			'title'    => __( 'VK Blocks Bootstrap Setting', 'vk-blocks' ),
-			'priority' => 30,
-		)
-	);
-
-	$wp_customize->add_setting(
-		'vk_blocks_load_bootstrap',
-		array(
-			'default'           => false,
-			'type'              => 'option',
-			'capability'        => 'edit_theme_options',
-			'sanitize_callback' => 'vk_blocks_sanitize_checkbox',
-		)
-	);
-
-	$wp_customize->add_control(
-		new WP_Customize_Color_Control(
-			$wp_customize,
-			'vk_blocks_load_bootstrap',
-			array(
-				'label'       => __( 'Loading Bootstrap4', 'vk-blocks' ),
-				'description' => __( 'Check here to load Bootstrap4. If your theme or plugins loading Bootstrap4, uncheck here.', 'vk-blocks' ),
-				'type'        => 'checkbox',
-				'section'     => 'vk_blocks_load_bootstrap_section',
-				'settings'    => 'vk_blocks_load_bootstrap',
-			)
-		)
-	);
-}
-
-// Lightning 系じゃない時にカスタマイズパネルを表示.
-if ( ! vk_blocks_is_lightning() ) {
-	add_action( 'customize_register', 'vk_blocks_customize_register' );
-}
 
 /**
  * VK Blocks add setting link

653ca3b1247882e9001f4521ddde6576e4898ebe
kurudrive <kurudrive@gmail.com>
2022-02-22 20:35:53 +0900

    fix / 古いオプション値の削除処理を追加

--- inc/vk-blocks/load-bootstrap.php
+++ inc/vk-blocks/load-bootstrap.php
@@ -28,3 +28,26 @@ function vk_blocks_load_bootstrap( $hook_suffix ) {
 }
 add_action( 'admin_enqueue_scripts', 'vk_blocks_load_bootstrap' );
 add_action( 'wp_enqueue_scripts', 'vk_blocks_load_bootstrap' );
+
+/**
+ * Delete old load bootstrap options
+ *
+ * 以前はBootstrapを読み組むかどうかの設定値 vk_blocks_load_bootstrap があったが、
+ * 環境に応じて自動読み込みに変更したので、vk_blocks_load_bootstrap を削除処理追加
+ *
+ * @since 1.25.0
+ * @param string $hook_suffix : display screen.
+ * @return void
+ */
+function vk_blocks_delete_load_bootstrap_options( $hook_suffix ) {
+	if ( 'settings_page_vk_blocks_options' === $hook_suffix ) {
+		$options = get_option( 'vk_blocks_options' );
+		if ( ! empty( $options['load_bootstrap'] ) ) {
+			$options['load_bootstrap'] = true;
+			update_option( 'vk_blocks_options', $options );
+			// Delite abolished option.
+			delete_option( 'vk_blocks_load_bootstrap' );
+		}
+	}
+}
+add_action( 'admin_enqueue_scripts', 'vk_blocks_delete_load_bootstrap_options' );

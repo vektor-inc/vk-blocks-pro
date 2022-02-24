c9d84c3d1163d7163923db41e6190a182ac39c44
Taichi Maruyama <maruyama@breadfish.jp>
2022-02-24 19:40:55 +0900

    fix: 変数を利用せずに処理するよう変更

--- src/blocks/_pro/outer/enque-front.js
+++ src/blocks/_pro/outer/enque-front.js
@@ -1,36 +1,34 @@
 window.addEventListener('load', function () {
-	const outerList = document.querySelectorAll('.vk_outer.has-text-color');
-
-	outerList.forEach((el) => {
+	document.querySelectorAll('.vk_outer.has-text-color').forEach((el) => {
 		const borderStyleBackup = el.style.borderStyle;
 		el.style.borderStyle = 'solid';
 		el.style.borderColor = window.getComputedStyle(el, '').color;
 		el.style.borderStyle = borderStyleBackup;
 	});
 
-	const outerList2 = document.querySelectorAll(
-		'.vk_outer.has-background-color'
-	);
-	outerList2.forEach((el) => {
-		const cssStyleDeclaration = window.getComputedStyle(el);
-		const opacity = cssStyleDeclaration.getPropertyValue('opacity');
-		let color = cssStyleDeclaration.getPropertyValue('background-color');
-		const url = cssStyleDeclaration
-			.getPropertyValue('background-image')
-			.replace(/^url|[\(\)]/g, '');
-		color = color.replace(/rgb|\(|\)/g, '');
-		color =
-			'linear-gradient(rgba(' +
-			color +
-			', ' +
-			opacity +
-			'),rgba(' +
-			color +
-			', ' +
-			opacity +
-			'))';
-		color += ', url(' + url + ')';
-		el.style.opacity = 'initial';
-		el.style.background = color;
-	});
+	document
+		.querySelectorAll('.vk_outer.has-background-color')
+		.forEach((el) => {
+			const cssStyleDeclaration = window.getComputedStyle(el);
+			const opacity = cssStyleDeclaration.getPropertyValue('opacity');
+			let color =
+				cssStyleDeclaration.getPropertyValue('background-color');
+			const url = cssStyleDeclaration
+				.getPropertyValue('background-image')
+				.replace(/^url|[\(\)]/g, '');
+			color = color.replace(/rgb|\(|\)/g, '');
+			color =
+				'linear-gradient(rgba(' +
+				color +
+				', ' +
+				opacity +
+				'),rgba(' +
+				color +
+				', ' +
+				opacity +
+				'))';
+			color += ', url(' + url + ')';
+			el.style.opacity = 'initial';
+			el.style.background = color;
+		});
 });

b4ac9b5eabf768f8699ba3fb658ab05db80cae47
shimotmk <shimotmk1104@gmail.com>
2022-03-03 14:27:33 +0900

    fix / breadcrumb lint

--- src/blocks/_pro/breadcrumb/edit.js
+++ src/blocks/_pro/breadcrumb/edit.js
@@ -2,15 +2,12 @@ import ServerSideRender from '@wordpress/server-side-render';
 import { useBlockProps } from '@wordpress/block-editor';
 
 export default function BreadcrumbEdit() {
-
 	const blockProps = useBlockProps();
 
 	return (
 		<>
 			<div {...blockProps}>
-				<ServerSideRender
-					block="vk-blocks/breadcrumb"
-				/>
+				<ServerSideRender block="vk-blocks/breadcrumb" />
 			</div>
 		</>
 	);

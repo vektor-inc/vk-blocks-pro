3f915e9ca28e811dbf5bbd6986b92b80b4c8d163
kurudrive <kurudrive@gmail.com>
2022-04-26 14:58:43 +0900

    lint

--- src/blocks/_pro/gridcolcard/edit.js
+++ src/blocks/_pro/gridcolcard/edit.js
@@ -171,7 +171,10 @@ export default function Edit(props) {
 					/>
 					<br />
 					<UnitControl
-						label={__('Column min width (Tablet / Optional)', 'vk-blocks')}
+						label={__(
+							'Column min width (Tablet / Optional)',
+							'vk-blocks'
+						)}
 						value={colWidthMinTablet}
 						onChange={(value) =>
 							setAttributes({ colWidthMinTablet: value })
@@ -179,7 +182,10 @@ export default function Edit(props) {
 					/>
 					<br />
 					<UnitControl
-						label={__('Column min width (PC / Optional)', 'vk-blocks')}
+						label={__(
+							'Column min width (PC / Optional)',
+							'vk-blocks'
+						)}
 						value={colWidthMinPC}
 						onChange={(value) =>
 							setAttributes({ colWidthMinPC: value })

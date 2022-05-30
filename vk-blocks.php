177b8e38eeb9d0a4a43a60b922a880f69be111de
kurudrive <kurudrive@gmail.com>
2022-05-30 18:39:54 +0900

    fix : 無料版無効化処理不具合修正

--- vk-blocks.php
+++ vk-blocks.php
@@ -204,94 +204,98 @@ if ( function_exists( 'vk_blocks_is_pro' ) && vk_blocks_is_pro() ) {
 
 	add_action( 'after_setup_theme', 'vk_blocks_update_checker' );
 
-	/**
-	 * Update alert message
-	 *
-	 * @param object $update_checker .
-	 * @return void
-	 */
-	function vk_blocks_the_update_messsage( $update_checker ) {
-		$state  = $update_checker->getUpdateState();
-		$update = $state->getUpdate();
+	if ( ! function_exists( 'vk_blocks_the_update_messsage' ) ) {
+		/**
+		 * Update alert message
+		 *
+		 * @param object $update_checker .
+		 * @return void
+		 */
+		function vk_blocks_the_update_messsage( $update_checker ) {
+			$state  = $update_checker->getUpdateState();
+			$update = $state->getUpdate();
 
-		$options = get_option( 'vk_blocks_options' );
-		if ( ! empty( $options['vk_blocks_pro_license_key'] ) ) {
-			$license = esc_html( $options['vk_blocks_pro_license_key'] );
-		} else {
-			$license = '';
-		}
+			$options = get_option( 'vk_blocks_options' );
+			if ( ! empty( $options['vk_blocks_pro_license_key'] ) ) {
+				$license = esc_html( $options['vk_blocks_pro_license_key'] );
+			} else {
+				$license = '';
+			}
 
-		$notice_title = '';
+			$notice_title = '';
 
-		// ライセンスキーが未入力の場合.
-		if ( empty( $license ) && wp_get_theme()->Template !== 'katawara' ) {
-			$notice_title = __( 'License Key has no registered.', 'vk-blocks' );
-		} elseif ( ! empty( $update ) && empty( $update->download_url ) ) {
+			// ライセンスキーが未入力の場合.
+			if ( empty( $license ) && wp_get_theme()->Template !== 'katawara' ) {
+				$notice_title = __( 'License Key has no registered.', 'vk-blocks' );
+			} elseif ( ! empty( $update ) && empty( $update->download_url ) ) {
 
-			// ライセンスが切れている あるいは 無効な場合.
-			// アップデートは存在するがURLが帰ってこなかった場合.
-			$notice_title = __(
-				'The VK Blocks Pro license is invalid.',
-				'vk-blocks'
-			);
-		}
+				// ライセンスが切れている あるいは 無効な場合.
+				// アップデートは存在するがURLが帰ってこなかった場合.
+				$notice_title = __(
+					'The VK Blocks Pro license is invalid.',
+					'vk-blocks'
+				);
+			}
 
-		if ( empty( $notice_title ) ) {
-			return;
-		}
+			if ( empty( $notice_title ) ) {
+				return;
+			}
 
-		$link_url = wp_nonce_url(
-			add_query_arg(
-				array(
-					'puc_check_for_updates' => 1,
-					'puc_slug'              => $update_checker->slug,
+			$link_url = wp_nonce_url(
+				add_query_arg(
+					array(
+						'puc_check_for_updates' => 1,
+						'puc_slug'              => $update_checker->slug,
+					),
+					self_admin_url( 'plugins.php' )
 				),
-				self_admin_url( 'plugins.php' )
-			),
-			'puc_check_for_updates'
-		);
+				'puc_check_for_updates'
+			);
 
-		$alert_html  = '';
-		$alert_html .= '<div class="error">';
-		$alert_html .= '<h4>VK Blocks Pro : ' . $notice_title . '</h4>';
-		$alert_html .= '<p>' . __(
-			'Enter a valid license key for any of the following products on the settings screen.',
-			'vk-blocks'
-		) . '</p>';
-		$alert_html .= '<ul>';
-		$alert_html .= '<li><a href="https://vws.vektor-inc.co.jp/product/lightning-g3-pro-pack/?rel=vk-blocks-pro-alert" target="_blank">Lightning G3 Pro Pack</a></li>';
-		$alert_html .= '<li><a href="https://vws.vektor-inc.co.jp/product/lightning-pro-update-license?rel=vk-blocks-pro-alert" target="_blank">Lightning Pro</a></li>';
-		$alert_html .= '</ul>';
+			$alert_html  = '';
+			$alert_html .= '<div class="error">';
+			$alert_html .= '<h4>VK Blocks Pro : ' . $notice_title . '</h4>';
+			$alert_html .= '<p>' . __(
+				'Enter a valid license key for any of the following products on the settings screen.',
+				'vk-blocks'
+			) . '</p>';
+			$alert_html .= '<ul>';
+			$alert_html .= '<li><a href="https://vws.vektor-inc.co.jp/product/lightning-g3-pro-pack/?rel=vk-blocks-pro-alert" target="_blank">Lightning G3 Pro Pack</a></li>';
+			$alert_html .= '<li><a href="https://vws.vektor-inc.co.jp/product/lightning-pro-update-license?rel=vk-blocks-pro-alert" target="_blank">Lightning Pro</a></li>';
+			$alert_html .= '</ul>';
 
-		$alert_html .= '<p><a href="' . admin_url( '/options-general.php?page=vk_blocks_options' ) . '" class="button button-primary">' . __( 'Enter the license key', 'vk-blocks' ) . '</a></p>';
+			$alert_html .= '<p><a href="' . admin_url( '/options-general.php?page=vk_blocks_options' ) . '" class="button button-primary">' . __( 'Enter the license key', 'vk-blocks' ) . '</a></p>';
 
-		$alert_html .= '<p>';
-		$alert_html .= __( 'If this display does not disappear even after entering a valid license key, re-acquire the update.', 'vk-blocks' );
-		$alert_html .= ' [ <a href="' . $link_url . '">' . __( 'Re-acquisition of updates', 'vk-blocks' ) . '</a> ]';
-		$alert_html .= '</p>';
+			$alert_html .= '<p>';
+			$alert_html .= __( 'If this display does not disappear even after entering a valid license key, re-acquire the update.', 'vk-blocks' );
+			$alert_html .= ' [ <a href="' . $link_url . '">' . __( 'Re-acquisition of updates', 'vk-blocks' ) . '</a> ]';
+			$alert_html .= '</p>';
 
-		$alert_html .= '</div>';
-		echo wp_kses_post( $alert_html );
+			$alert_html .= '</div>';
+			echo wp_kses_post( $alert_html );
+		}
 	}
 
-	/**
-	 * Register update license key
-	 *
-	 * @param array $query_args : updatechacker array.
-	 * @return $query_args
-	 */
-	function vk_blocks_get_license_check_query_arg( $query_args ) {
-		$options = get_option( 'vk_blocks_options' );
-		$license = '';
-		if ( ! empty( $options['vk_blocks_pro_license_key'] ) ) {
-			$license = esc_html( $options['vk_blocks_pro_license_key'] );
-		}
+	if ( ! function_exists( 'vk_blocks_get_license_check_query_arg' ) ) {
+		/**
+		 * Register update license key
+		 *
+		 * @param array $query_args : updatechacker array.
+		 * @return $query_args
+		 */
+		function vk_blocks_get_license_check_query_arg( $query_args ) {
+			$options = get_option( 'vk_blocks_options' );
+			$license = '';
+			if ( ! empty( $options['vk_blocks_pro_license_key'] ) ) {
+				$license = esc_html( $options['vk_blocks_pro_license_key'] );
+			}
 
-		if ( ! empty( $license ) ) {
-			$query_args['vk-blocks-pro-license-key'] = $license;
-		}
-		$query_args['template'] = wp_get_theme()->Template;
+			if ( ! empty( $license ) ) {
+				$query_args['vk-blocks-pro-license-key'] = $license;
+			}
+			$query_args['template'] = wp_get_theme()->Template;
 
-		return $query_args;
+			return $query_args;
+		}
 	}
 }

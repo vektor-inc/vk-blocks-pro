e80549a6d4bc8e6b408a9d6e800a6603701ad535
drill-lancer <rickaddison7634@gmail.com>
2022-06-03 14:01:04 +0900

    fix: タイムラインアイテムの padding-bottom の調整機能を追加

--- src/blocks/_pro/timeline-item/edit.js
+++ src/blocks/_pro/timeline-item/edit.js
@@ -4,6 +4,7 @@ import {
 	BaseControl,
 	SelectControl,
 	TextControl,
+	__experimentalUnitControl as UnitControl,
 } from '@wordpress/components';
 import {
 	InnerBlocks,
@@ -15,7 +16,7 @@ import { AdvancedColorPalette } from '@vkblocks/components/advanced-color-palett
 
 export default function TimelineItemEdit(props) {
 	const { attributes, setAttributes } = props;
-	const { label, color, style, styleLine } = attributes;
+	const { label, color, style, styleLine, outerPaddingBottom } = attributes;
 
 	const containerClass = ' vk_timeline_item';
 
@@ -54,6 +55,7 @@ export default function TimelineItemEdit(props) {
 
 	const blockProps = useBlockProps({
 		className: containerClass + styleLineClass,
+		style: { paddingBottom: outerPaddingBottom },
 	});
 
 	return (
@@ -104,6 +106,19 @@ export default function TimelineItemEdit(props) {
 									label: __('None', 'vk-blocks'),
 								},
 							]}
+						/>
+					</BaseControl>
+					<BaseControl
+						id="outer-padding-bottom"
+						label="Outer Padding Bottom"
+					>
+						<UnitControl
+							value={outerPaddingBottom}
+							onChange={(value) =>
+								setAttributes({
+									outerPaddingBottom: value ? value : null,
+								})
+							}
 						/>
 					</BaseControl>
 				</PanelBody>

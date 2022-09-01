343fa14e8cee0e0de2a07cd51b3c58082ec8bf40
kurudrive <kurudrive@gmail.com>
2022-09-01 15:48:44 +0900

    change/vk-admin : js 制御でサイドバーの固定解除

--- inc/vk-admin/package/class-vk-admin.php
+++ inc/vk-admin/package/class-vk-admin.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'Vk_Admin' ) ) {
 	*/
 	class Vk_Admin {
 
-		public static $version = '2.2.0';
+		public static $version = '2.3.0';
 
 		static function init() {
 			add_action( 'admin_enqueue_scripts', array( __CLASS__, 'admin_common_css' ) );

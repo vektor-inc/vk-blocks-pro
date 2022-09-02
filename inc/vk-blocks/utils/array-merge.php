e33fdaf869f1a733940f5d8a04f2f8108eb0511d
shimotmk <shimotmk1104@gmail.com>
2022-09-02 14:49:15 +0900

    fix / vk_blocks_array_merge array merge order value

--- inc/vk-blocks/utils/array-merge.php
+++ inc/vk-blocks/utils/array-merge.php
@@ -6,6 +6,26 @@
  */
 
 /**
+ * 順番を揃える
+ *
+ * @param array $args Value to merge with $defaults.
+ * @param array $defaults Array that serves as the defaults.
+ *
+ * @return array Array with aligned order.
+ */
+function vk_blocks_merge_order( $args, $defaults ) {
+	$merged = $defaults;
+	foreach ( $args as $key => $value ) {
+		if ( ! is_array( $value ) && array_key_exists( $key, $defaults ) ) {
+			$merged[ $key ] = $value;
+		} elseif ( is_array( $value ) && is_array( $defaults[ $key ] ) ) {
+			$merged[ $key ] = vk_blocks_merge_order( $value, $defaults[ $key ] );
+		}
+	}
+	return $merged;
+}
+
+/**
  * Merges user defined arguments into defaults array.
  *
  * $argsにキーが存在したらそのまま存在しない時に$defaultsの配列をマージする
@@ -19,14 +39,28 @@
  *
  * @return array Merged user defined values with defaults.
  */
-function vk_blocks_array_merge( $args, $defaults = array() ) {
-	$parsed_args = $args;
+function vk_blocks_merge_value( $args, $defaults ) {
+	$merged = $args;
 	foreach ( $defaults as $key => $value ) {
 		if ( empty( $args[ $key ] ) ) {
-			$parsed_args[ $key ] = $value;
+			$merged[ $key ] = $value;
 		} elseif ( is_array( $value ) && is_array( $args[ $key ] ) ) {
-			$parsed_args[ $key ] = vk_blocks_array_merge( $args[ $key ], $value );
+			$merged[ $key ] = vk_blocks_merge_value( $args[ $key ], $value );
 		}
 	}
+	return $merged;
+}
+
+/**
+ * 順番と値をマージする関数を呼び出す関数
+ *
+ * @param array $args Value to merge with $defaults.
+ * @param array $defaults Optional. Array that serves as the defaults.
+ *
+ * @return array Merged user defined values with defaults.
+ */
+function vk_blocks_array_merge( $args, $defaults = array() ) {
+	$order       = vk_blocks_merge_order( $args, $defaults );
+	$parsed_args = vk_blocks_merge_value( $order, $defaults );
 	return $parsed_args;
 }

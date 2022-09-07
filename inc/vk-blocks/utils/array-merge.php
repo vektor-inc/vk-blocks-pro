feb8461e449892abc7fae75c9291bb3116048ad0
shimotmk <shimotmk1104@gmail.com>
2022-09-07 14:54:20 +0900

    fix / vk_blocks_array_merge array merge order value

--- inc/vk-blocks/utils/array-merge.php
+++ inc/vk-blocks/utils/array-merge.php
@@ -6,29 +6,11 @@
  */
 
 /**
- * 順番を揃える
- *
- * @param array $args Value to merge with $defaults.
- * @param array $defaults Array that serves as the defaults.
- *
- * @return array Array with aligned order.
- */
-function vk_blocks_merge_order( $args, $defaults ) {
-	$merged = $defaults;
-	foreach ( $args as $key => $value ) {
-		if ( ! is_array( $value ) && array_key_exists( $key, $defaults ) ) {
-			$merged[ $key ] = $value;
-		} elseif ( is_array( $value ) && is_array( $defaults[ $key ] ) ) {
-			$merged[ $key ] = vk_blocks_merge_order( $value, $defaults[ $key ] );
-		}
-	}
-	return $merged;
-}
-
-/**
  * Merges user defined arguments into defaults array.
  *
  * $argsにキーが存在したらそのまま存在しない時に$defaultsの配列をマージする
+ *
+ * 順番はdefaultsに合わせる
  *
  * wp_parse_args配列のマージに再帰処理を追加した関数
  *
@@ -39,28 +21,14 @@ function vk_blocks_merge_order( $args, $defaults ) {
  *
  * @return array Merged user defined values with defaults.
  */
-function vk_blocks_merge_value( $args, $defaults ) {
-	$merged = $args;
-	foreach ( $defaults as $key => $value ) {
-		if ( empty( $args[ $key ] ) ) {
+function vk_blocks_array_merge( $args, $defaults ) {
+	$merged = $defaults;
+	foreach ( $args as $key => $value ) {
+		if ( ! is_array( $value ) && array_key_exists( $key, $defaults ) ) {
 			$merged[ $key ] = $value;
-		} elseif ( is_array( $value ) && is_array( $args[ $key ] ) ) {
-			$merged[ $key ] = vk_blocks_merge_value( $args[ $key ], $value );
+		} elseif ( is_array( $value ) && is_array( $defaults[ $key ] ) ) {
+			$merged[ $key ] = vk_blocks_array_merge( $value, $defaults[ $key ] );
 		}
 	}
 	return $merged;
-}
-
-/**
- * 順番と値をマージする関数を呼び出す関数
- *
- * @param array $args Value to merge with $defaults.
- * @param array $defaults Optional. Array that serves as the defaults.
- *
- * @return array Merged user defined values with defaults.
- */
-function vk_blocks_array_merge( $args, $defaults = array() ) {
-	$order       = vk_blocks_merge_order( $args, $defaults );
-	$parsed_args = vk_blocks_merge_value( $order, $defaults );
-	return $parsed_args;
 }

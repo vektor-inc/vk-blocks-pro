ba01ec81f6aabb5d73040a33a15dbe110e7c2572
Taichi Maruyama <maruyama@breadfish.jp>
2022-10-12 14:52:48 +0900

    fix: fine tuning

--- test/phpunit/pro/test-rest-api.php
+++ test/phpunit/pro/test-rest-api.php
@@ -15,9 +15,9 @@ class RestAPITest extends WP_UnitTestCase {
     static $server;
 
     /**
-    * Create a item for our test and initiate REST API.
+    * initiate REST API.
     */
-    public static function wpSetUpBeforeClass() {
+    public static function wpSetUpBeforeClass():void {
 
         // Initiating the REST API.
         global $wp_rest_server;
@@ -27,12 +27,31 @@ class RestAPITest extends WP_UnitTestCase {
     }
 
     /**
+    * Permission check for get_vk_blocks_options
+    *
+    * @return void.
+    */
+    public function test_get_vk_options_permition():void  {
+
+        $request = new WP_REST_Request( 'GET', '/vk-blocks/v1/get_vk_blocks_options' );
+        $response = self::$server->dispatch( $request );
+        $actual = $response->get_data();
+
+        print '------------------------------------' . PHP_EOL;
+        print 'RestAPITest::test_get_vk_options_permition()' . PHP_EOL;
+        print '------------------------------------' . PHP_EOL;
+
+        // 権限がなければ拒否しなければならない
+        $this->assertSame( 401, $response->status );
+    }        
+
+    /**
     * Test the endpoint for get_vk_blocks_options
     *
     * @return void.
     */
+    public function test_get_vk_options_value() {
 
-    public function test_get_vk_options() {
         $this->set_current_user( 'administrator' );
         $request = new WP_REST_Request( 'GET', '/vk-blocks/v1/get_vk_blocks_options' );
         $response = self::$server->dispatch( $request );
@@ -42,10 +61,11 @@ class RestAPITest extends WP_UnitTestCase {
         $vk_balloon_meta_expect = VK_Blocks_Options::get_balloon_meta_options();
 
         print '------------------------------------' . PHP_EOL;
-        print 'RestAPITest::test_get_vk_options()' . PHP_EOL;
+        print 'RestAPITest::test_get_vk_options_value()' . PHP_EOL;
         print '------------------------------------' . PHP_EOL;
-        $this->assertSame( $vk_options_expect, $actual[ 'vkBlocksOption' ] );
-        $this->assertSame( $vk_balloon_meta_expect, $actual[ 'vkBlocksBalloonMeta' ] );
+ 
+        $this->assertSame( $vk_options_expect, $actual['vkBlocksOption'] );
+        $this->assertSame( $vk_balloon_meta_expect, $actual['vkBlocksBalloonMeta'] );
 
     }
 
@@ -55,19 +75,13 @@ class RestAPITest extends WP_UnitTestCase {
     * @param  string $role administrator, editor, author, contributor ...
     * @return void
     */
-
     public function set_current_user( $role ) {
         $user = self::factory()->user->create_and_get(
             array(
                 'role' => $role,
             )
         );
-
-        /*
-        * Set $user as current user
-        */
         wp_set_current_user( $user->ID, $user->user_login );
     }
-
 }
 ;

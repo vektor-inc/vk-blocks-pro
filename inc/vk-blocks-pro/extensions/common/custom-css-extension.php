80cd8a76493e316fdcbe56b1373966cbc44f0fb9
shimotmk <shimotmk1104@gmail.com>
2022-10-13 11:18:03 +0900

    fix / custom css 条件をシンプルに

--- inc/vk-blocks-pro/extensions/common/custom-css-extension.php
+++ inc/vk-blocks-pro/extensions/common/custom-css-extension.php
@@ -24,18 +24,16 @@ function vk_blocks_custom_css_extension( $block_content, $block ) {
 			// selectorをUniqueクラスに変換
 			$css = preg_replace( '/selector/', '.' . $custom_css_class, $css );
 			// vk_custom_cssをUniqueクラスに変換
-			$content = preg_replace( '/vk_custom_css/', $custom_css_class, $block_content, 1 );
-		} else {
-			$content = $block_content;
+			$block_content = preg_replace( '/vk_custom_css/', $custom_css_class, $block_content, 1 );
 		}
 		$css = vk_blocks_minify_css( $css );
 		if ( ! empty( $css ) ) {
 			if ( function_exists( 'wp_enqueue_block_support_styles' ) ) {
 				wp_enqueue_block_support_styles( $css );
-				return $content;
+				return $block_content;
 				// 5.8のサポートを切るならelse内は削除する
 			} else {
-				return '<style>' . $css . '</style>' . $content;
+				return '<style>' . $css . '</style>' . $block_content;
 			}
 		}
 		return $block_content;

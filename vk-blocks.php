6758226b288463ce5884adb57146b8a901379305
shimotmk <shimotmk1104@gmail.com>
2022-10-25 15:14:48 +0900

    upgrade : upgrade Requires at least WordPress version

--- vk-blocks.php
+++ vk-blocks.php
@@ -5,7 +5,7 @@
  * Description: This is a plugin that extends Gutenberg's blocks.
  * Version: 1.44.0.0
  * Stable tag: 1.43.0.2
- * Requires at least: 5.8
+ * Requires at least: 5.9
  * Author: Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
  * Text Domain: vk-blocks

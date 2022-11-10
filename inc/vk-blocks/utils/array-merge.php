937827c180800a0e5d01f0b1ff6877f17837cd86
shimotmk <shimotmk1104@gmail.com>
2022-11-10 16:47:33 +0900

    add / vk-admin-block-manager

--- inc/vk-blocks/utils/array-merge.php
+++ inc/vk-blocks/utils/array-merge.php
@@ -23,13 +23,11 @@
  */
 function vk_blocks_array_merge( $args, $defaults ) {
 	$merged = $defaults;
-	if ( null !== $args && is_array( $args ) ) {
-		foreach ( $args as $key => $value ) {
-			if ( is_array( $value ) && isset( $defaults[ $key ] ) && is_array( $defaults[ $key ] ) ) {
-				$merged[ $key ] = vk_blocks_array_merge( $value, $defaults[ $key ] );
-			} else {
-				$merged[ $key ] = $value;
-			}
+	foreach ( $args as $key => $value ) {
+		if ( is_array( $value ) && isset( $defaults[ $key ] ) && is_array( $defaults[ $key ] ) ) {
+			$merged[ $key ] = vk_blocks_array_merge( $value, $defaults[ $key ] );
+		} else {
+			$merged[ $key ] = $value;
 		}
 	}
 	return $merged;

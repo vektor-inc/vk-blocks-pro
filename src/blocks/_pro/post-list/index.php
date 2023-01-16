3fd122200e5763008b81fb502f6c6a13e79723d4
drill-lancer <rickaddison7634@gmail.com>
2023-01-16 09:54:35 +0900

    fix: reaadme

--- src/blocks/_pro/post-list/index.php
+++ src/blocks/_pro/post-list/index.php
@@ -6,6 +6,108 @@
  */
 
 /**
+ * Post List Get Block Data
+ * 
+ * @return array
+ */
+function vk_blocks_post_list_get_block_data() {
+
+	// キャッシュを取得
+	$transients = get_transient( 'vk_blocks_post_list_block_data' );
+
+	// キャッシュがあればそれを返す
+	if ( ! empty( $transients ) ) {
+		return $transients;
+	}
+
+	// Create $post_type_option ///////////////////////////
+	$the_post_types = get_post_types(
+		// Only public post types.
+		array(
+			'public' => true,
+		),
+		'objects',
+		'and'
+	);
+
+	$post_type_option = array();
+	foreach ( $the_post_types as $post_type ) {
+		$post_type_option[] = array(
+			'label' => $post_type->label,
+			'slug'  => $post_type->name,
+		);
+	}
+
+	// Set up taxonomies ///////////////////////////
+	$the_taxonomies = get_taxonomies(
+		// Only public taxonomies.
+		array(
+			'public' => true,
+		),
+		'objects',
+		'and'
+	);
+
+	$term_by_taxonomy_name = array();
+	foreach ( $the_taxonomies as $the_taxonomy ) {
+		$terms                                        = array_values( get_terms( $the_taxonomy->name, array( 'hide_empty' => false ) ) );
+		$term_by_taxonomy_name[ $the_taxonomy->name ] = array_map(
+			function( $term ) {
+				return array(
+					'term_id' => $term->term_id,
+					'name'    => $term->name,
+				);
+			},
+			$terms
+		);
+	}
+
+	$data = array(
+		'post_type_option'      => $post_type_option,
+		'term_by_taxonomy_name' => $term_by_taxonomy_name,
+	);
+
+	set_transient( 'vk_blocks_post_list_block_data', $data, 60 * 60 * 24 );
+
+	return $data;
+
+}
+
+/**
+ * 編集画面を開いた時点で条件付きでキャッシュをクリア
+ */
+function vk_blocks_post_list_editor_refresh_block_data() {
+
+	// オプションを取得
+	$options = VK_Blocks_Options::get_options();
+
+	// キャッシュの有効時間（秒）
+	$cache_time = 60 * 60;
+
+	// 最後にキャッシュされた時間を取得
+	$last_cached = ! empty( $options['last-block-data-cached'] ) ? $options['last-block-data-cached'] : '1970-01-01 00:00:00' ;
+
+	// 現在の時刻を取得
+	$current_time = date( 'Y-m-d H:i:s' );
+
+	// 差分を取得・キャッシュが初めてならキャッシュの有効時間が経過したものとみなす
+	$diff = strtotime( $current_time ) -  strtotime( $last_cached );
+
+	// フラグがなければパターンのデータのキャッシュをパージ
+	if ( $diff > $cache_time  ) {
+		// パターンのデータのキャッシュをパージ
+		delete_transient( 'vkfs_block_data' );
+		// 最後にキャッシュされた時間を更新
+		$options['last-block-data-cached'] = $current_time;
+		// 最低１時間はキャッシュを保持
+		update_option( 'vk_blocks_options', $options );
+	}
+}
+add_action( 'load-post.php', 'vk_blocks_post_list_editor_refresh_block_data' );
+add_action( 'load-post-new.php', 'vk_blocks_post_list_editor_refresh_block_data' );
+add_action( 'load-site-editor.php', 'vk_blocks_post_list_editor_refresh_block_data' );
+
+/**
  * Post list render callback
  *
  * @param array $attributes Block attributes.
@@ -164,47 +266,8 @@ function vk_blocks_register_block_post_list() {
 		)
 	);
 
-	// Create $post_type_option ///////////////////////////
-	$the_post_types = get_post_types(
-		// Only public post types.
-		array(
-			'public' => true,
-		),
-		'objects',
-		'and'
-	);
-
-	$post_type_option = array();
-	foreach ( $the_post_types as $post_type ) {
-		$post_type_option[] = array(
-			'label' => $post_type->label,
-			'slug'  => $post_type->name,
-		);
-	}
-
-	// Set up taxonomies ///////////////////////////
-	$the_taxonomies = get_taxonomies(
-		// Only public taxonomies.
-		array(
-			'public' => true,
-		),
-		'objects',
-		'and'
-	);
-
-	$term_by_taxonomy_name = array();
-	foreach ( $the_taxonomies as $the_taxonomy ) {
-		$terms                                        = array_values( get_terms( $the_taxonomy->name, array( 'hide_empty' => false ) ) );
-		$term_by_taxonomy_name[ $the_taxonomy->name ] = array_map(
-			function( $term ) {
-				return array(
-					'term_id' => $term->term_id,
-					'name'    => $term->name,
-				);
-			},
-			$terms
-		);
-	}
+	// キャッシュからデータを取得
+	$block_data = vk_blocks_post_list_get_block_data();
 
 	// PHPで作った投稿タイプとタクソノミー情報をjsに渡す ///////////////////////////
 	// Pass post type and taxonomy information created in PHP to js /////////
@@ -212,8 +275,8 @@ function vk_blocks_register_block_post_list() {
 		'vk-blocks-build-js',
 		'vk_block_post_type_params',
 		array(
-			'post_type_option'      => $post_type_option,
-			'term_by_taxonomy_name' => $term_by_taxonomy_name,
+			'post_type_option'      => $block_data['post_type_option'],
+			'term_by_taxonomy_name' => $block_data['term_by_taxonomy_name'],
 		)
 	);
 }

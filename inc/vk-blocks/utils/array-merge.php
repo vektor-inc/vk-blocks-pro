1499cffc1f985af9c04616c2e8a231d7023970d6
shimotmk <shimotmk1104@gmail.com>
2023-01-30 10:27:57 +0900

    fix / balloon option 初期値は空配列に修正

--- inc/vk-blocks/utils/array-merge.php
+++ inc/vk-blocks/utils/array-merge.php
@@ -27,51 +27,11 @@ function vk_blocks_array_merge( $args, $defaults ) {
 	}
 	$merged = $defaults;
 	foreach ( $args as $key => $value ) {
-		if ( is_array( $value ) && isset( $defaults[ $key ] ) && is_array( $defaults[ $key ] ) && ! empty( $value ) && vk_blocks_multidimensional_min_count( $value ) !== vk_blocks_multidimensional_min_count( $defaults[ $key ] ) ) {
+		if ( is_array( $value ) && isset( $defaults[ $key ] ) && is_array( $defaults[ $key ] ) && ! empty( $value ) ) {
 			$merged[ $key ] = vk_blocks_array_merge( $value, $defaults[ $key ] );
 		} else {
 			$merged[ $key ] = $value;
 		}
 	}
 	return $merged;
-}
-
-/**
- * Vk_blocks_array_kind
- * 配列の種類
- *
- * @param array $array array.
- *
- * @return string associative(連想配列)| multidimensional(多次元配列) | array(配列)
- */
-function vk_blocks_array_kind( $array ) {
-	if ( ! is_array( $array ) ) {
-		return false;
-	}
-	if ( array_values( $array ) !== $array ) {
-		return 'associative';
-	}
-	return count( $array ) !== count( $array, COUNT_RECURSIVE ) ? 'multidimensional' : 'array';
-}
-
-/**
- * Vk_blocks_multidimensional_min_count
- * 多次元配列の中の配列の数の最小を調べる関数
- *
- * @param array $array array.
- *
- * @return number|array
- */
-function vk_blocks_multidimensional_min_count( $array ) {
-	if ( vk_blocks_array_kind( $array ) !== 'multidimensional' ) {
-		return $array;
-	}
-
-	$count = 0;
-	foreach ( $array as $value ) {
-		if ( 0 === $count || $count > count( $value ) ) {
-			$count = count( $value );
-		}
-	}
-	return $count;
 }

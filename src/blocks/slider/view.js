7259852139943f2ab9214c822c6adc2a820bce68
drill-lancer <rickaddison7634@gmail.com>
2023-02-08 10:23:19 +0900

    fix: view

--- src/blocks/slider/view.js
+++ src/blocks/slider/view.js
@@ -55,9 +55,6 @@ document.defaultView.addEventListener('load', function () {
 			}
 
 			if (attributes.effect !== 'fade') {
-				if (attributes.centeredSlides !== undefined) {
-					SwiperSetting += `centeredSlides: ${attributes.centeredSlides},`;
-				}
 				if (attributes.slidesPerViewMobile) {
 					SwiperSetting += `slidesPerView: ${attributes.slidesPerViewMobile},`;
 					if (

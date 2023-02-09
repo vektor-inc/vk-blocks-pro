21342943f06d135175423e0e21fa8d01385ecc3e
shimotmk <shimotmk1104@gmail.com>
2023-02-09 15:58:18 +0900

    change / custom-block-style editor-css 後方互換

--- inc/vk-blocks-pro/extensions/common/custom-block-style.php
+++ inc/vk-blocks-pro/extensions/common/custom-block-style.php
@@ -32,6 +32,9 @@ function vk_blocks_register_custom_block_style() {
 		if ( ! empty( $custom_block_style['property_transform_inline_style'] ) ) {
 			$editor_style .= preg_replace( '/selector/', '.is-style-' . $custom_block_style['property_name'], $custom_block_style['property_transform_inline_style'] );
 			$editor_css   .= $editor_style;
+		} else {
+			// 後方互換: property_transform_inline_styleがないバージョンで作られた時のエディター用CSS
+			$editor_css .= $inline_style;
 		}
 	}
 	// エディターiframe内で読み込ませるため

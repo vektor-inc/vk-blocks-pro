1c7d6f5fdd781d76fe958bc72191565f60be5ef4
drill-lancer <rickaddison7634@gmail.com>
2023-02-10 11:29:52 +0900

    fix: space rand unit land control

---
+++ src/utils/unit-options.js
@@ -0,0 +1,86 @@
+import { __ } from '@wordpress/i18n';
+
+const unitOption = [
+	{
+		value: 'px',
+		label: __('px', 'vk-blocks'),
+	},
+	{
+		value: 'em',
+		label: __('em', 'vk-blocks'),
+	},
+	{
+		value: 'rem',
+		label: __('rem', 'vk-blocks'),
+	},
+	{
+		value: 'vw',
+		label: __('vw', 'vk-blocks'),
+	},
+	{
+		value: 'svw',
+		label: __('svw', 'vk-blocks'),
+	},
+	{
+		value: 'lvw',
+		label: __('lvw', 'vk-blocks'),
+	},
+	{
+		value: 'dvw',
+		label: __('dvw', 'vk-blocks'),
+	},
+	{
+		value: 'vh',
+		label: __('vh', 'vk-blocks'),
+	},
+	{
+		value: 'svh',
+		label: __('svh', 'vk-blocks'),
+	},
+	{
+		value: 'lvh',
+		label: __('lvh', 'vk-blocks'),
+	},
+	{
+		value: 'dvh',
+		label: __('dvh', 'vk-blocks'),
+	},
+	{
+		value: 'vmin',
+		label: __('vmin', 'vk-blocks'),
+	},
+	{
+		value: 'svmin',
+		label: __('svmin', 'vk-blocks'),
+	},
+	{
+		value: 'lvmin',
+		label: __('lvmin', 'vk-blocks'),
+	},
+	{
+		value: 'dvmin',
+		label: __('dvmin', 'vk-blocks'),
+	},
+
+	{
+		value: 'vmax',
+		label: __('vmax', 'vk-blocks'),
+	},
+
+	{
+		value: 'svmax',
+		label: __('svmax', 'vk-blocks'),
+	},
+
+	{
+		value: 'lvmax',
+		label: __('lvmax', 'vk-blocks'),
+	},
+
+	{
+		value: 'dvmax',
+		label: __('dvmax', 'vk-blocks'),
+	},
+];
+
+export default unitOption;

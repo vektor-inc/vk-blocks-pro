bf3dda92fe2bff79688e235deefd65fcba6ce217
kurudrive <kurudrive@gmail.com>
2023-05-16 02:54:13 +0900

    lint

--- append-code.php
+++ append-code.php
@@ -8,5 +8,5 @@
  */
 $vk_exunit_path = plugin_dir_path( __FILE__ ) . 'tests/plugins/vk-all-in-one-expansion-unit/vkExUnit.php';
 if ( file_exists( $vk_exunit_path ) ) {
-    require_once $vk_exunit_path;
+	require_once $vk_exunit_path;
 }

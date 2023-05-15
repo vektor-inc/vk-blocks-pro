3139214b1fae17671827b2d42a313fcc4b8fe303
kurudrive <kurudrive@gmail.com>
2023-05-15 18:15:59 +0900

    test .sh からのプラグインインストールテスト（多分権限でコケる）

--- bin/install-plugins-for-playwrite.sh
+++ bin/install-plugins-for-playwrite.sh
@@ -6,7 +6,13 @@ set -ex
 # -s ファイル出力時の進捗状況を非表示にする(エラーも非表示)
 # curl -s $WP_THEME -o theme.zip
 curl -s https://downloads.wordpress.org/plugins/vk-all-in-one-expansion-unit.zip -o exunit.zip
+
+ls 
+
 # -d 指定したディレクトリに展開
 unzip exunit.zip -d /var/www/html/wp-content/plugins
+
+ls /var/www/html/wp-content/plugins
+
 # 展開したのでもとのzipファイルを削除 -f はエラーメッセージを表示しない
 rm -f exunit.zip

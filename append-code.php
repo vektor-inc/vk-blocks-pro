6896bcb6c0d2d435d8bf296df05fa71feca26793
kurudrive <kurudrive@gmail.com>
2023-05-16 02:53:06 +0900

    lint

--- append-code.php
+++ append-code.php
@@ -1,8 +1,12 @@
 <?php
 /**
+ * @package vk-blocks-pro
+ */
+
+/**
  * vk-blocks.php に追加するコード
  */
 $vk_exunit_path = plugin_dir_path( __FILE__ ) . 'tests/plugins/vk-all-in-one-expansion-unit/vkExUnit.php';
 if ( file_exists( $vk_exunit_path ) ) {
-	require_once $vk_exunit_path;
+    require_once $vk_exunit_path;
 }

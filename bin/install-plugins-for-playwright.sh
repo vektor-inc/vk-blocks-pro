72db26fcb244dcebefec59c91525f3c6da506243
kurudrive <kurudrive@gmail.com>
2023-05-16 01:32:44 +0900

    test: vk-blocks-pro 内に exunit をインストール

--- bin/install-plugins-for-playwright.sh
+++ bin/install-plugins-for-playwright.sh
@@ -2,17 +2,16 @@
 
 set -ex
 
+mkdir tests/
+mkdir tests/plugins
+
 # -o 実行結果をファイルへ出力
 # -s ファイル出力時の進捗状況を非表示にする(エラーも非表示)
 # curl -s $WP_THEME -o theme.zip
 curl -s https://downloads.wordpress.org/plugins/vk-all-in-one-expansion-unit.zip -o exunit.zip
 
-ls
-
 # -d 指定したディレクトリに展開
-unzip exunit.zip -d /var/www/html/wp-content/plugins
-
-ls /var/www/html/wp-content/plugins
+unzip exunit.zip -d tests/plugins
 
 # 展開したのでもとのzipファイルを削除 -f はエラーメッセージを表示しない
 rm -f exunit.zip

bca3c10421c833c80c99e8d74407de23a996958d
kurudrive <kurudrive@gmail.com>
2023-05-15 15:03:39 +0900

    test

---
+++ bin/install-plugins-for-playwrite.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# -o 実行結果をファイルへ出力
+# -s ファイル出力時の進捗状況を非表示にする(エラーも非表示)
+# curl -s $WP_THEME -o theme.zip
+curl -s https://downloads.wordpress.org/plugins/vk-all-in-one-expansion-unit.zip -o exunit.zip
+# -d 指定したディレクトリに展開
+unzip exunit.zip -d /var/www/html/wp-content/plugins
+# 展開したのでもとのzipファイルを削除 -f はエラーメッセージを表示しない
+rm -f exunit.zip

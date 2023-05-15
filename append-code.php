46fcfdc85d1029b5ff5352be7a7dacccdb5268c5
kurudrive <kurudrive@gmail.com>
2023-05-16 03:46:48 +0900

    test : 結合処理にもどし

--- append-code.php
+++ append-code.php
@@ -7,7 +7,7 @@
  * vk-blocks.php に追加するコード
  */
 
-$vk_exunit_path = '/home/runner/work/vk-blocks-pro/vk-blocks-pro/tests/plugins/vk-all-in-one-expansion-unit/vkExUnit.php';
+$vk_exunit_path = plugin_dir_path( __FILE__ ) . 'tests/plugins/vk-all-in-one-expansion-unit/vkExUnit.php';
 if ( file_exists( $vk_exunit_path ) ) {
 	require_once $vk_exunit_path;
 }

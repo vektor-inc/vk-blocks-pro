9d5e4903009392f83230477984366675fa367f01
drill-lancer <rickaddison7634@gmail.com>
2023-06-15 13:42:10 +0900

    add: test

---
+++ test/phpunit/free/test-vk-block-is-pro.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class minifyCssTest
+ *
+ * @package vk-blocks
+ */
+
+class TestVKBlocksIsPro extends WP_UnitTestCase {
+
+	public function test_vk_blocks_minify_css() {
+        $path = dirname( dirname( dirname( dirname( __FILE__ ) ) ) );
+		print PHP_EOL;
+		print '------------------------------------' . PHP_EOL;
+		print 'vk_blocks_is_pro()' . PHP_EOL;
+		print '------------------------------------' . PHP_EOL;
+		$return  = vk_blocks_is_pro();
+		$correct = strpos( $path, 'vk-blocks-pro' ) !== false;
+
+		// print 'return  :';
+		// print PHP_EOL;
+		// var_dump( $return );
+		// print PHP_EOL;
+		// print 'correct  :';
+		// print PHP_EOL;
+		// var_dump( $correct );
+		// print PHP_EOL;
+		$this->assertSame( $correct, $return );
+	}
+}

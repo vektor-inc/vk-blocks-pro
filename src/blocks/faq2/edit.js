bff5673fc24f269f4309a4acde517c1e7c2e2853
doshimaf <daikougun0923@gmail.com>
2023-07-04 15:00:29 +0900

    fix : settingContent -> jsx, add message

--- src/blocks/faq2/edit.js
+++ src/blocks/faq2/edit.js
@@ -3,7 +3,7 @@ import {
 	InspectorControls,
 	useBlockProps,
 } from '@wordpress/block-editor';
-import { PanelBody, PanelRow, SelectControl } from '@wordpress/components';
+import { BaseControl, PanelBody, SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 export default function FAQ2Edit(props) {
@@ -17,56 +17,67 @@ export default function FAQ2Edit(props) {
 
 	const TEMPLATE = [['vk-blocks/faq2-q'], ['vk-blocks/faq2-a']];
 
-	let settingContent;
-	// eslint-disable-next-line no-undef
-	if (vk_blocks_check.is_pro) {
-		settingContent = (
-			<SelectControl
-				name="vk_blocks_options[new_faq_accordion]"
-				value={showContent}
-				onChange={(value) => {
-					setAttributes({
-						showContent: value,
-					});
-				}}
-				options={[
-					{
-						label: __('Use common settings', 'vk-blocks-pro'),
-						value: 'default',
-					},
-					{
-						label: __('Disable accordion', 'vk-blocks-pro'),
-						value: 'disable',
-					},
-					{
-						label: __(
-							'Enable accordion and default open',
-							'vk-blocks-pro'
-						),
-						value: 'open',
-					},
-					{
-						label: __(
-							'Enable accordion and default close',
-							'vk-blocks-pro'
-						),
-						value: 'close',
-					},
-				]}
-			/>
-		);
-	} else {
-		settingContent = __(
-			'You can be collapsing this block at VK Blocks Pro',
-			'vk-blocks-pro'
-		);
-	}
-
 	return (
 		<>
 			<InspectorControls>
 				<PanelBody title={__('Accordion Setting', 'vk-blocks-pro')}>
-					<PanelRow>{settingContent}</PanelRow>
+					{/* eslint-disable-next-line no-undef */}
+					{vk_blocks_check.is_pro ? (
+						<BaseControl>
+							<SelectControl
+								name="vk_blocks_options[new_faq_accordion]"
+								value={showContent}
+								onChange={(value) => {
+									setAttributes({
+										showContent: value,
+									});
+								}}
+								options={[
+									{
+										label: __(
+											'Use common settings',
+											'vk-blocks-pro'
+										),
+										value: 'default',
+									},
+									{
+										label: __(
+											'Disable accordion',
+											'vk-blocks-pro'
+										),
+										value: 'disable',
+									},
+									{
+										label: __(
+											'Enable accordion and default open',
+											'vk-blocks-pro'
+										),
+										value: 'open',
+									},
+									{
+										label: __(
+											'Enable accordion and default close',
+											'vk-blocks-pro'
+										),
+										value: 'close',
+									},
+								]}
+							/>
+							<p>
+								{__(
+									'* If you want to change the accordion settings common to FAQ blocks, you can set it at Setting > VK Blocks',
+									'vk-blocks-pro'
+								)}
+							</p>
+						</BaseControl>
+					) : (
+						<p>
+							{__(
+								'You can be collapsing this block at VK Blocks Pro',
+								'vk-blocks-pro'
+							)}
+						</p>
+					)}
 				</PanelBody>
 			</InspectorControls>
 			<div {...blockProps}>

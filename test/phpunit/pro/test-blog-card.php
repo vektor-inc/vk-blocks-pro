112047a08c9b46f5488ce533e93a89961c611a52
shimotmk <shimotmk1104@gmail.com>
2023-08-09 13:47:03 +0900

    fix blog card phpunit test

--- test/phpunit/pro/test-blog-card.php
+++ test/phpunit/pro/test-blog-card.php
@@ -51,153 +51,107 @@ class BlogCard extends WP_UnitTestCase {
 		wp_delete_post( self::$attachment_id, true );
 	}
 
-	public function test_rendering_post_template() {
+	public function test_vk_blocks_blog_card_render_callback(){
+
 		print PHP_EOL;
 		print '------------------------------------' . PHP_EOL;
 		print 'vk_blocks_blog_card_render_callback' . PHP_EOL;
 		print '------------------------------------' . PHP_EOL;
 		print PHP_EOL;
+
+		$tests = array(
+			// 内部リンク
+			array(
+				'content' => '
+				<!-- wp:vk-blocks/blog-card {"url":"'.get_permalink(self::$post->ID).'"} -->
+				<div %s>
+				<!-- wp:vk-blocks/blog-card-title /-->
+				<!-- wp:vk-blocks/blog-card-featured-image /-->
+				<!-- wp:vk-blocks/blog-card-excerpt /-->
+				<!-- wp:vk-blocks/blog-card-site-logo /-->
+				<!-- wp:vk-blocks/blog-card-site-title /-->
+				</div>
+				<!-- /wp:vk-blocks/blog-card -->',
+				'expected' => '
+				<div %s>
+				<h5 class="wp-block-vk-blocks-blog-card-title"><a href="'.get_permalink(self::$post->ID).'" target="_self" >'.get_the_title( self::$post->ID ).'</a></h5>
+				<figure class="wp-block-vk-blocks-blog-card-featured-image"><a href="'. get_permalink(self::$post->ID) .'" target="_self"  ><img src=\''. get_the_post_thumbnail_url( self::$post->ID, 'large' ) .'\' style="object-fit:cover;" /></a></figure>
+				<div class="wp-block-vk-blocks-blog-card-excerpt"><p class="wp-block-vk-blocks-blog-card-excerpt__excerpt">'.get_the_excerpt( self::$post->ID ).'</p></div>
+				<figure class="wp-block-vk-blocks-blog-card-site-logo"><a href="'.home_url().'" target="_self" ><img src=\''. get_site_icon_url( 32 ) .'\' /></a></figure>
+				<p class="wp-block-vk-blocks-blog-card-site-title"><a href="'. home_url() .'" target="_self" >'. get_bloginfo( 'name' ) .'</a></p>
+				</div>
+				',
+			),
+			// 外部リンク
+			array(
+				'content' => '
+				<!-- wp:vk-blocks/blog-card {"url":"https://vektor-inc.co.jp/"} -->
+				<div %s>
+				<!-- wp:vk-blocks/blog-card-title /-->
+				<!-- wp:vk-blocks/blog-card-featured-image /-->
+				<!-- wp:vk-blocks/blog-card-excerpt /-->
+				<!-- wp:vk-blocks/blog-card-site-logo /-->
+				<!-- wp:vk-blocks/blog-card-site-title /-->
+				</div>
+				<!-- /wp:vk-blocks/blog-card -->',
+				'expected' => '
+				<div %s>
+				<h5 class="wp-block-vk-blocks-blog-card-title"><a href="https://vektor-inc.co.jp/" target="_self" >株式会社ベクトル | WordPressテーマ・プラグイン開発のクリエイティブカンパニー</a></h5>
+				<figure class="wp-block-vk-blocks-blog-card-featured-image"><a href="https://vektor-inc.co.jp/" target="_self"  ><img src=\'https://www.vektor-inc.co.jp/wp-content/uploads/2020/04/vecktor-inc_ogp2020.png\' style="object-fit:cover;" /></a></figure>
+				<div class="wp-block-vk-blocks-blog-card-excerpt"><p class="wp-block-vk-blocks-blog-card-excerpt__excerpt">WordPressテーマ・プラグイン開発のクリエイティブカンパニー</p></div>
+				<figure class="wp-block-vk-blocks-blog-card-site-logo"><a href="https://vektor-inc.co.jp" target="_self" ><img src=\'https://www.vektor-inc.co.jp/wp-content/uploads/2020/03/cropped-icon2020-32x32.png\' /></a></figure>
+				<p class="wp-block-vk-blocks-blog-card-site-title"><a href="https://vektor-inc.co.jp" target="_self" >株式会社ベクトル</a></p>
+				</div>
+				',
+			),
+			// 外部リンク 埋め込み不可
+			array(
+				'attributes' => array(
+					'url' => 'https://github.com/vektor-inc/vk-blocks-pro/blob/master/vk-blocks.php',
+				),
+				'content' => '
+				<!-- wp:vk-blocks/blog-card {"url":"https://github.com/vektor-inc/vk-blocks-pro/blob/master/vk-blocks.php"} -->
+				<div %s></div>
+				<!-- /wp:vk-blocks/blog-card -->',
+				'expected' => '<div %s>https://github.com/vektor-inc/vk-blocks-pro/blob/master/vk-blocks.php</div>',
+			),
+		);
+
 		WP_Block_Supports::init();
 		WP_Block_Supports::$block_to_render = array(
 			'blockName' => 'vk-blocks/blog-card',
 		);
-		$parsed_blocks = parse_blocks(
-			'<!-- wp:vk-blocks/blog-card {"url":"https://vektor-inc.co.jp/"} -->
-			<div class="wp-block-vk-blocks-blog-card">
-			<!-- wp:vk-blocks/blog-card-title /-->
-			<!-- wp:vk-blocks/blog-card-featured-image /-->
-			<!-- wp:vk-blocks/blog-card-excerpt /-->
-			<!-- wp:vk-blocks/blog-card-site-logo /-->
-			<!-- wp:vk-blocks/blog-card-site-title /-->
-			</div>
-			<!-- /wp:vk-blocks/blog-card -->'
-		);
-		$block         = new WP_Block( $parsed_blocks[0] );
-		$markup        = $block->render();
 
-		$expected = <<<END
-<div %s>
-<h5 class="wp-block-vk-blocks-blog-card-title"><a href="https://vektor-inc.co.jp/" target="_self" >株式会社ベクトル | WordPressテーマ・プラグイン開発のクリエイティブカンパニー</a></h5>
-<figure class="wp-block-vk-blocks-blog-card-featured-image"><a href="https://vektor-inc.co.jp/" target="_self"  ><img src='https://www.vektor-inc.co.jp/wp-content/uploads/2020/04/vecktor-inc_ogp2020.png' style="object-fit:cover;" /></a></figure>
-<div class="wp-block-vk-blocks-blog-card-excerpt"><p class="wp-block-vk-blocks-blog-card-excerpt__excerpt">WordPressテーマ・プラグイン開発のクリエイティブカンパニー</p></div>
-<figure class="wp-block-vk-blocks-blog-card-site-logo"><a href="https://vektor-inc.co.jp" target="_self" ><img src='https://www.vektor-inc.co.jp/wp-content/uploads/2020/03/cropped-icon2020-32x32.png' /></a></figure>
-<p class="wp-block-vk-blocks-blog-card-site-title"><a href="https://vektor-inc.co.jp" target="_self" >株式会社ベクトル</a></p>
-</div>
-END;
-		$get_block_wrapper_attributes = get_block_wrapper_attributes();
-		$correct = sprintf($expected, $get_block_wrapper_attributes);
+		foreach ( $tests as $test ) {
 
-		print PHP_EOL;
-		print '$correct  :';
-		var_dump($correct);
-		print PHP_EOL;
-		print PHP_EOL;
-		print '$markup  :';
-		var_dump($markup);
-		print PHP_EOL;
+			$get_block_wrapper_attributes = get_block_wrapper_attributes();
+			$parse_blocks = parse_blocks(sprintf($test['content'], $get_block_wrapper_attributes));
+			$render_block_content = render_block( $parse_blocks[1] );
+			if ( is_wp_version_compatible( '6.3' ) ) {
+				$get_block_wrapper_attributes = 'class="wp-block-vk-blocks-blog-card is-layout-flow wp-block-blog-card-is-layout-flow"';
+			} else {
+				$get_block_wrapper_attributes = 'class="wp-block-vk-blocks-blog-card"';
+			}
+			$correct = sprintf($test['expected'], $get_block_wrapper_attributes);
 
-		$this->assertSame(
-			str_replace( array( "\n", "\t" ), '', $correct ),
-			str_replace( array( "\n", "\t" ), '', $markup )
-		);
+			// print PHP_EOL;
+			// print '$test[content][1]  :';
+			// var_dump($test['content']);
+			// print PHP_EOL;
+			// print '$render_block_content  :' . $render_block_content;
+			// print PHP_EOL;
+			// print '$test[expected]  :' . $test['expected'];
+			// print PHP_EOL;
+			// print PHP_EOL;
+			// print 'get_block_wrapper_attributes()  :';
+			// var_dump(get_block_wrapper_attributes());
+			// print PHP_EOL;
+			// print PHP_EOL;
+			// print 'correct  :';
+			// var_dump($correct);
+			// print PHP_EOL;
+			$this->assertSame( $correct, $render_block_content );
+
+		}
 	}
-
-	// public function test_vk_blocks_blog_card_render_callback(){
-
-	// 	print PHP_EOL;
-	// 	print '------------------------------------' . PHP_EOL;
-	// 	print 'vk_blocks_blog_card_render_callback' . PHP_EOL;
-	// 	print '------------------------------------' . PHP_EOL;
-	// 	print PHP_EOL;
-
-	// 	$tests = array(
-	// 		// 内部リンク
-	// 		array(
-	// 			'content' => '
-	// 			<!-- wp:vk-blocks/blog-card {"url":"'.get_permalink(self::$post->ID).'"} -->
-	// 			<div %s>
-	// 			<!-- wp:vk-blocks/blog-card-title /-->
-	// 			<!-- wp:vk-blocks/blog-card-featured-image /-->
-	// 			<!-- wp:vk-blocks/blog-card-excerpt /-->
-	// 			<!-- wp:vk-blocks/blog-card-site-logo /-->
-	// 			<!-- wp:vk-blocks/blog-card-site-title /-->
-	// 			</div>
-	// 			<!-- /wp:vk-blocks/blog-card -->',
-	// 			'expected' => '
-	// 			<div %s>
-	// 			<h5 class="wp-block-vk-blocks-blog-card-title"><a href="'.get_permalink(self::$post->ID).'" target="_self" >'.get_the_title( self::$post->ID ).'</a></h5>
-	// 			<figure class="wp-block-vk-blocks-blog-card-featured-image"><a href="'. get_permalink(self::$post->ID) .'" target="_self"  ><img src=\''. get_the_post_thumbnail_url( self::$post->ID, 'large' ) .'\' style="object-fit:cover;" /></a></figure>
-	// 			<div class="wp-block-vk-blocks-blog-card-excerpt"><p class="wp-block-vk-blocks-blog-card-excerpt__excerpt">'.get_the_excerpt( self::$post->ID ).'</p></div>
-	// 			<figure class="wp-block-vk-blocks-blog-card-site-logo"><a href="'.home_url().'" target="_self" ><img src=\''. get_site_icon_url( 32 ) .'\' /></a></figure>
-	// 			<p class="wp-block-vk-blocks-blog-card-site-title"><a href="'. home_url() .'" target="_self" >'. get_bloginfo( 'name' ) .'</a></p>
-	// 			</div>
-	// 			',
-	// 		),
-	// 		// 外部リンク
-	// 		array(
-	// 			'content' => '
-	// 			<!-- wp:vk-blocks/blog-card {"url":"https://vektor-inc.co.jp/"} -->
-	// 			<div %s>
-	// 			<!-- wp:vk-blocks/blog-card-title /-->
-	// 			<!-- wp:vk-blocks/blog-card-featured-image /-->
-	// 			<!-- wp:vk-blocks/blog-card-excerpt /-->
-	// 			<!-- wp:vk-blocks/blog-card-site-logo /-->
-	// 			<!-- wp:vk-blocks/blog-card-site-title /-->
-	// 			</div>
-	// 			<!-- /wp:vk-blocks/blog-card -->',
-	// 			'expected' => '
-	// 			<div %s>
-	// 			<h5 class="wp-block-vk-blocks-blog-card-title"><a href="https://vektor-inc.co.jp/" target="_self" >株式会社ベクトル | WordPressテーマ・プラグイン開発のクリエイティブカンパニー</a></h5>
-	// 			<figure class="wp-block-vk-blocks-blog-card-featured-image"><a href="https://vektor-inc.co.jp/" target="_self"  ><img src=\'https://www.vektor-inc.co.jp/wp-content/uploads/2020/04/vecktor-inc_ogp2020.png\' style="object-fit:cover;" /></a></figure>
-	// 			<div class="wp-block-vk-blocks-blog-card-excerpt"><p class="wp-block-vk-blocks-blog-card-excerpt__excerpt">WordPressテーマ・プラグイン開発のクリエイティブカンパニー</p></div>
-	// 			<figure class="wp-block-vk-blocks-blog-card-site-logo"><a href="https://vektor-inc.co.jp" target="_self" ><img src=\'https://www.vektor-inc.co.jp/wp-content/uploads/2020/03/cropped-icon2020-32x32.png\' /></a></figure>
-	// 			<p class="wp-block-vk-blocks-blog-card-site-title"><a href="https://vektor-inc.co.jp" target="_self" >株式会社ベクトル</a></p>
-	// 			</div>
-	// 			',
-	// 		),
-	// 		// 外部リンク 埋め込み不可
-	// 		array(
-	// 			'attributes' => array(
-	// 				'url' => 'https://github.com/vektor-inc/vk-blocks-pro/blob/master/vk-blocks.php',
-	// 			),
-	// 			'content' => '
-	// 			<!-- wp:vk-blocks/blog-card {"url":"https://github.com/vektor-inc/vk-blocks-pro/blob/master/vk-blocks.php"} -->
-	// 			<div %s></div>
-	// 			<!-- /wp:vk-blocks/blog-card -->',
-	// 			'expected' => '<div %s>https://github.com/vektor-inc/vk-blocks-pro/blob/master/vk-blocks.php</div>',
-	// 		),
-	// 	);
-
-	// 	WP_Block_Supports::init();
-	// 	WP_Block_Supports::$block_to_render = array(
-	// 		'blockName' => 'vk-blocks/blog-card',
-	// 	);
-
-	// 	foreach ( $tests as $test ) {
-
-	// 		$get_block_wrapper_attributes = get_block_wrapper_attributes();
-	// 		$parse_blocks = parse_blocks(sprintf($test['content'], $get_block_wrapper_attributes));
-	// 		$render_block_content = render_block( $parse_blocks[1] );
-	// 		$correct = sprintf($test['expected'], $get_block_wrapper_attributes);
-
-	// 		// print PHP_EOL;
-	// 		// print '$test[content][1]  :';
-	// 		// var_dump($test['content']);
-	// 		// print PHP_EOL;
-	// 		// print '$render_block_content  :' . $render_block_content;
-	// 		// print PHP_EOL;
-	// 		// print '$test[expected]  :' . $test['expected'];
-	// 		// print PHP_EOL;
-	// 		// print PHP_EOL;
-	// 		// print 'get_block_wrapper_attributes()  :';
-	// 		// var_dump(get_block_wrapper_attributes());
-	// 		// print PHP_EOL;
-	// 		// print PHP_EOL;
-	// 		// print 'correct  :';
-	// 		// var_dump($correct);
-	// 		// print PHP_EOL;
-	// 		// $this->assertSame( $correct, $render_block_content );
-
-	// 	}
-	// }
 };

16d996a76a4c7474fefd681a9ecc62c5f30a91ad
shimotmk <shimotmk1104@gmail.com>
2023-08-30 09:44:04 +0900

    fix blog card expectedは埋め込みサイトのレスポンスによって変わるので複数用意

--- test/phpunit/pro/test-blog-card.php
+++ test/phpunit/pro/test-blog-card.php
@@ -82,7 +82,7 @@ class BlogCard extends WP_UnitTestCase {
 				</div>
 				',
 			),
-			// 外部リンク
+			// 外部リンク expectedは埋め込みサイトのレスポンスによって変わるので複数用意
 			array(
 				'content' => '
 				<!-- wp:vk-blocks/blog-card {"url":"https://vektor-inc.co.jp/"} -->
@@ -94,7 +94,8 @@ class BlogCard extends WP_UnitTestCase {
 				<!-- wp:vk-blocks/blog-card-site-title /-->
 				</div>
 				<!-- /wp:vk-blocks/blog-card -->',
-				'expected' => '
+				'expected' => array(
+					'can_embed' =>'
 				<div %s>
 				<h5 class="wp-block-vk-blocks-blog-card-title"><a href="https://vektor-inc.co.jp/" target="_self" >株式会社ベクトル | WordPressテーマ・プラグイン開発のクリエイティブカンパニー</a></h5>
 				<figure class="wp-block-vk-blocks-blog-card-featured-image"><a href="https://vektor-inc.co.jp/" target="_self"  ><img src=\'https://www.vektor-inc.co.jp/wp-content/uploads/2020/04/vecktor-inc_ogp2020.png\' style="object-fit:cover;" /></a></figure>
@@ -103,7 +104,8 @@ class BlogCard extends WP_UnitTestCase {
 				<p class="wp-block-vk-blocks-blog-card-site-title"><a href="https://vektor-inc.co.jp" target="_self" >株式会社ベクトル</a></p>
 				</div>
 				',
-				'cannot_embed' => '<div %s>https://vektor-inc.co.jp/</div>',
+					'cannot_embed' => '<div %s>https://vektor-inc.co.jp/</div>',
+				),
 			),
 			// 外部リンク 埋め込み不可
 			array(
@@ -130,31 +132,24 @@ class BlogCard extends WP_UnitTestCase {
 			} else {
 				$get_block_wrapper_attributes = 'class="wp-block-vk-blocks-blog-card"';
 			}
-			$correct = sprintf($test['expected'], $get_block_wrapper_attributes);
-			if (!empty($test['cannot_embed'])) {
-				$cannot_embed_correct = sprintf($test['cannot_embed'], $get_block_wrapper_attributes);
+			if ( is_array($test['expected']) ) {
+				$correct = array();
+				$correct[1] = sprintf($test['expected']['can_embed'], $get_block_wrapper_attributes);
+				$correct[2] = sprintf($test['expected']['cannot_embed'], $get_block_wrapper_attributes);
+			} else {
+				$correct = sprintf($test['expected'], $get_block_wrapper_attributes);
 			}
 
 			// print PHP_EOL;
-			// print '$test[content][1]  :';
-			// var_dump($test['content']);
-			// print PHP_EOL;
 			// print '$render_block_content  :' . $render_block_content;
-			// print PHP_EOL;
-			// print '$test[expected]  :' . $test['expected'];
-			// print PHP_EOL;
-			// print PHP_EOL;
-			// print 'get_block_wrapper_attributes()  :';
-			// var_dump(get_block_wrapper_attributes());
 			// print PHP_EOL;
 			// print PHP_EOL;
 			// print 'correct  :';
 			// var_dump($correct);
 			// print PHP_EOL;
 
-			$blog_card_data = VK_Blocks_Blog_Card::vk_get_blog_card_data('https://www.vektor-inc.co.jp/');
-			if (!empty($test['cannot_embed']) && !empty($blog_card_data['cannot_embed'])) {
-				$this->assertSame( $cannot_embed_correct, $render_block_content );
+			if ( is_array($correct) ) {
+				$this->assertContains($render_block_content, $correct);
 			} else {
 				$this->assertSame( $correct, $render_block_content );
 			}

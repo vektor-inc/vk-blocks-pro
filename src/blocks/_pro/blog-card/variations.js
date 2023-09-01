c380d415e0939d03863e4ae83894ef5786ebb7b7
shimotmk <shimotmk1104@gmail.com>
2023-09-01 13:58:08 +0900

    fix blog card variations

--- src/blocks/_pro/blog-card/variations.js
+++ src/blocks/_pro/blog-card/variations.js
@@ -14,6 +14,111 @@ import {
 
 const variations = [
 	{
+		name: 'left-image-right-text',
+		title: __('左 画像', 'vk-blocks-pro'),
+		icon: leftImageRightText,
+		attributes: {
+			style: {
+				spacing: {
+					padding: {
+						top: '1.5rem',
+						right: '1.5rem',
+						bottom: '1.5rem',
+						left: '1.5rem',
+					},
+				},
+				border: {
+					color: '#0000001f',
+					width: '1px',
+					radius: '5px',
+				},
+			},
+		},
+		innerBlocks: [
+			[
+				'core/columns',
+				{
+					lock: {
+						move: false,
+						remove: true,
+					},
+					style: {
+						spacing: {
+							blockGap: { top: '1.5rem', left: '1.5rem' },
+						},
+					},
+				},
+				[
+					[
+						'core/column',
+						{
+							verticalAlignment: 'top',
+							width: '33.3%',
+						},
+						[
+							[
+								'vk-blocks/blog-card-featured-image',
+								{
+									style: {
+										border: {
+											color: '#0000001f',
+											width: '1px',
+											radius: '5px',
+										},
+									},
+								},
+								[],
+							],
+						],
+					],
+					[
+						'core/column',
+						{
+							lock: {
+								move: false,
+								remove: true,
+							},
+							width: '66.6%',
+						},
+						[
+							['vk-blocks/blog-card-title', {}, []],
+							['vk-blocks/blog-card-excerpt', {}, []],
+							[
+								'core/group',
+								{
+									style: {
+										spacing: { blockGap: '6px' },
+									},
+									layout: {
+										type: 'flex',
+										flexWrap: 'nowrap',
+										verticalAlignment: 'center',
+									},
+								},
+								[
+									[
+										'vk-blocks/blog-card-site-logo',
+										{
+											style: {
+												layout: {
+													selfStretch: 'fixed',
+													flexSize: '15px',
+												},
+											},
+										},
+										[],
+									],
+									['vk-blocks/blog-card-site-title', {}, []],
+								],
+							],
+						],
+					],
+				],
+			],
+		],
+		scope: ['block'],
+	},
+	{
 		name: 'left-text-right-image',
 		title: __('右 画像', 'vk-blocks-pro'),
 		icon: leftTextRightImage,
@@ -111,111 +216,6 @@ const variations = [
 									},
 								},
 								[],
-							],
-						],
-					],
-				],
-			],
-		],
-		scope: ['block'],
-	},
-	{
-		name: 'left-image-right-text',
-		title: __('左 画像', 'vk-blocks-pro'),
-		icon: leftImageRightText,
-		attributes: {
-			style: {
-				spacing: {
-					padding: {
-						top: '1.5rem',
-						right: '1.5rem',
-						bottom: '1.5rem',
-						left: '1.5rem',
-					},
-				},
-				border: {
-					color: '#0000001f',
-					width: '1px',
-					radius: '5px',
-				},
-			},
-		},
-		innerBlocks: [
-			[
-				'core/columns',
-				{
-					lock: {
-						move: false,
-						remove: true,
-					},
-					style: {
-						spacing: {
-							blockGap: { top: '1.5rem', left: '1.5rem' },
-						},
-					},
-				},
-				[
-					[
-						'core/column',
-						{
-							verticalAlignment: 'top',
-							width: '33.3%',
-						},
-						[
-							[
-								'vk-blocks/blog-card-featured-image',
-								{
-									style: {
-										border: {
-											color: '#0000001f',
-											width: '1px',
-											radius: '5px',
-										},
-									},
-								},
-								[],
-							],
-						],
-					],
-					[
-						'core/column',
-						{
-							lock: {
-								move: false,
-								remove: true,
-							},
-							width: '66.6%',
-						},
-						[
-							['vk-blocks/blog-card-title', {}, []],
-							['vk-blocks/blog-card-excerpt', {}, []],
-							[
-								'core/group',
-								{
-									style: {
-										spacing: { blockGap: '6px' },
-									},
-									layout: {
-										type: 'flex',
-										flexWrap: 'nowrap',
-										verticalAlignment: 'center',
-									},
-								},
-								[
-									[
-										'vk-blocks/blog-card-site-logo',
-										{
-											style: {
-												layout: {
-													selfStretch: 'fixed',
-													flexSize: '15px',
-												},
-											},
-										},
-										[],
-									],
-									['vk-blocks/blog-card-site-title', {}, []],
-								],
 							],
 						],
 					],

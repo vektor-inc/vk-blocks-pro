1fb3ac5d660373c926761fb32ea60af3583400df
shimotmk <shimotmk1104@gmail.com>
2023-09-29 10:29:32 +0900

    add custom variation helpテキストを修正

--- src/extensions/common/custom-block-variation/utils.js
+++ src/extensions/common/custom-block-variation/utils.js
@@ -2,12 +2,30 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { createInterpolateElement } from '@wordpress/element';
+import { ExternalLink } from '@wordpress/components';
 
 export const SCOPE_OPTIONS = [
 	{
 		name: 'inserter',
 		label: __('インサーター', 'vk-blocks-pro'),
-		help: __('インサーターに表示されます', 'vk-blocks-pro'),
+		help: createInterpolateElement(
+			__(
+				'インサーターに表示されます。 <Link>インサーターについてさらに詳しく</Link>.',
+				'vk-blocks-pro'
+			),
+			{
+				Link: (
+					<ExternalLink
+						href={__(
+							// 'https://wordpress.org/documentation/article/adding-a-new-block/#what-is-the-inserter',
+							'https://ja.wordpress.org/support/article/adding-a-new-block/#%e3%82%a4%e3%83%b3%e3%82%b5%e3%83%bc%e3%82%bf%e3%83%bc%e3%81%a8%e3%81%af',
+							'vk-blocks-pro'
+						)}
+					/>
+				),
+			}
+		),
 	},
 	{
 		name: 'block',

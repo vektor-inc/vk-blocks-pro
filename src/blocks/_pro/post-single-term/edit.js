8d4ba7e6d36a55b489427e11018a0751b81dd1ff
Taichi Maruyama <maruyama@breadfish.jp>
2023-10-30 17:41:24 +0900

    fix: paddingの指定を可能に

--- src/blocks/_pro/post-single-term/edit.js
+++ src/blocks/_pro/post-single-term/edit.js
@@ -1,10 +1,68 @@
-import { useBlockProps } from '@wordpress/block-editor';
-export default function SingleTermEdit() {
+import { __ } from '@wordpress/i18n';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+//import { useEntityProp } from '@wordpress/core-data';
+import ServerSideRender from '@wordpress/server-side-render';
+
+import {
+	PanelBody,
+	__experimentalBoxControl as BoxControl, // eslint-disable-line @wordpress/no-unsafe-wp-apis
+} from '@wordpress/components';
+export default function SingleTermEdit(props) {
+	const { attributes, setAttributes } = props;
+	const { paddingValues } = attributes;
 	const blockProps = useBlockProps();
 
+	const units = [
+		{
+			value: 'px',
+			label: 'px',
+			default: 0,
+		},
+		{
+			value: '%',
+			label: '%',
+			default: 0,
+		},
+		{
+			value: 'vw',
+			label: 'vw',
+			default: 0,
+		},
+		{
+			value: 'vh',
+			label: 'vh',
+			default: 0,
+		},
+	];
+	const resetPaddingValues = {
+		top: '0px',
+		left: '0px',
+		right: '0px',
+		bottom: '0px',
+	};
+
 	return (
-		<div {...blockProps}>
-			<span>{'(ターム名)'}</span>
-		</div>
+		<>
+			<InspectorControls key="setting">
+				<PanelBody title={__('Padding Setting', 'bf-click-counter')}>
+					<BoxControl
+						label={__('Padding', 'bf-click-counter')}
+						values={paddingValues}
+						onChange={(value) =>
+							setAttributes({ paddingValues: value })
+						} // 保存処理
+						units={units}
+						allowReset={true}
+						resetValues={resetPaddingValues}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div {...blockProps}>
+				<ServerSideRender
+					block="vk-blocks/post-single-term"
+					attributes={attributes}
+				/>
+			</div>
+		</>
 	);
 }

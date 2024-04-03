70f0fb4cbceb5daaa8ded8988ebe019b773f69e8
mtdkei <mtdkei@gmail.com>
2024-04-03 12:55:34 +0900

    Fixed Display用に変更

--- src/blocks/_pro/fixed-display/save.js
+++ src/blocks/_pro/fixed-display/save.js
@@ -2,17 +2,13 @@ import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 import classNames from 'classnames';
 
 export default function save(props) {
-	let { effect, speed, range, once, blockId } = props.attributes;
+	let { mode, position, blockId } = props.attributes;
 	//For recovering block.
-	effect = effect ? effect : 'slide-up';
-	speed = speed ? speed : 'fast';
-	range = range ? range : 'short';
-	once = once ? true : false;
+	mode = mode ? mode : 'always-visible';
+	position = position ? position : 'right';
 
 	const sliderClasses = classNames(
-		`vk_animation vk_animation-${effect} vk_animation-speed-${speed} vk_animation-range-${range}`,
-		{ [`vk_animation-once`]: once },
-		`vk_animation-${blockId}`
+		`vk_animation vk_animation-${mode} vk_animation-position-${position} vk_animation-${blockId}`
 	);
 
 	return (

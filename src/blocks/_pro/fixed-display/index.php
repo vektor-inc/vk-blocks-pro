5a63f7cd99c30c85f32560af138f12327686c59b
mtdkei <mtdkei@gmail.com>
2024-04-04 13:22:04 +0900

    Update index.php

--- src/blocks/_pro/fixed-display/index.php
+++ src/blocks/_pro/fixed-display/index.php
@@ -1,16 +1,45 @@
 <?php
 /**
- * Registers the `vk-blocks/fixed-display` block.
+ * Registers the `vk-blocks/animation` block.
  *
  * @package vk-blocks
  */
-function vk_blocks_register_block_fixed_display() {
+
+/**
+ * Register animation block.
+ *
+ * @return void
+ */
+function vk_blocks_register_block_animation() {
+	// Register Style.
+	if ( ! is_admin() ) {
+		wp_register_style(
+			'vk-blocks/animation',
+			VK_BLOCKS_DIR_URL . 'build/_pro/animation/style.css',
+			array(),
+			VK_BLOCKS_VERSION
+		);
+	}
+
+	// Register Style.
+	if ( ! is_admin() ) {
+		wp_register_script(
+			'vk-blocks/animation-script',
+			VK_BLOCKS_DIR_URL . 'build/vk-animation.min.js',
+			array(),
+			VK_BLOCKS_VERSION,
+			true
+		);
+	}
+
 	register_block_type(
 		__DIR__,
 		array(
+			'style'         => 'vk-blocks/animation',
+			'script'        => 'vk-blocks/animation-script',
 			'editor_style'  => 'vk-blocks-build-editor-css',
 			'editor_script' => 'vk-blocks-build-js',
 		)
 	);
 }
-add_action( 'init', 'vk_blocks_register_block_fixed_display', 99 );
+add_action( 'init', 'vk_blocks_register_block_animation', 99 );

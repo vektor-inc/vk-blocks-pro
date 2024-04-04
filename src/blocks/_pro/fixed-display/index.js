47b06cee220562152b395558a75500fbf570538a
mtdkei <mtdkei@gmail.com>
2024-04-05 08:21:24 +0900

    表示領域に入った時に適用させるフィルター追加

--- src/blocks/_pro/fixed-display/index.js
+++ src/blocks/_pro/fixed-display/index.js
@@ -7,6 +7,8 @@ import metadata from './block.json';
 import edit from './edit';
 import save from './save';
 
+import { addFilter } from '@wordpress/hooks';
+
 const { name } = metadata;
 
 export { metadata, name };
@@ -17,3 +19,24 @@ export const settings = {
 	edit,
 	save,
 };
+
+/**
+ * 	表示領域に入った時に適用させるフィルター。
+ *
+ * @param {*} el
+ * @param {*} type
+ */
+const addFixedDisplayVisible = (el, type) => {
+    // `Fixed Display`ブロックの場合に特定の処理を行う
+    if ('vk-blocks/fixed-display' === type.name) {
+        // ここに特定のクラスを追加する処理などを記述
+    }
+    return el;
+};
+
+addFilter(
+    'blocks.getSaveElement',
+    'vk-blocks/fixed-display',
+    addFixedDisplayVisible,
+    11
+);

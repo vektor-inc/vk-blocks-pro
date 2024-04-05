830852c4d6a900b47febf29410449938b87f4d53
mtdkei <mtdkei@gmail.com>
2024-04-05 12:58:36 +0900

    date属性出力

--- src/blocks/_pro/fixed-display/save.js
+++ src/blocks/_pro/fixed-display/save.js
@@ -1,23 +1,19 @@
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
-import classNames from 'classnames';
 
-export default function save(props) {
-	let { mode, position, blockId } = props.attributes;
-	//For recovering block.
-	mode = mode ? mode : 'always-visible';
-	position = position ? position : 'right';
+export default function save({ attributes }) {
+  const { mode, position, scrollTiming, scrollTimingUnit, blockId } = attributes;
 
-	const sliderClasses = classNames(
-		`vk_fixed-display vk_fixed-display-${mode} vk_fixed-display-position-${position} vk_fixed-display-${blockId}`
-	);
+  const blockProps = useBlockProps.save({
+    className: `vk_fixed-display vk_fixed-display-mode-${mode} vk_fixed-display-position-${position} vk_fixed-display-${blockId}`,
+    ...(mode === 'show-on-scroll' && {
+      'data-scroll-timing': scrollTiming.toString(),
+      'data-scroll-timing-unit': scrollTimingUnit
+    }),
+  });
 
-	return (
-		<div
-			{...useBlockProps.save({
-				className: sliderClasses,
-			})}
-		>
-			<InnerBlocks.Content />
-		</div>
-	);
+  return (
+    <div {...blockProps}>
+      <InnerBlocks.Content />
+    </div>
+  );
 }

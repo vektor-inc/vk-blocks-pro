a6b6217a82fc19f66a54accdd1ca057b7ec0832a
mtdkei <mtdkei@gmail.com>
2024-04-05 11:54:38 +0900

    view.jsの内容に何故かなっていたので戻しました

--- src/blocks/_pro/fixed-display/save.js
+++ src/blocks/_pro/fixed-display/save.js
@@ -1,38 +1,23 @@
-// スクロールイベントリスナーを追加
-window.addEventListener('scroll', function() {
-	const items = document.querySelectorAll('.vk_fixed-display-mode-show-on-scroll');
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import classNames from 'classnames';
 
-	items.forEach(item => {
-		// data属性からスクロールタイミングと単位を取得
-		const timing = parseInt(item.getAttribute('data-scroll-timing'), 10);
-		const unit = item.getAttribute('data-scroll-timing-unit');
+export default function save(props) {
+	let { mode, position, blockId } = props.attributes;
+	//For recovering block.
+	mode = mode ? mode : 'always-visible';
+	position = position ? position : 'right';
 
-		// 単位に応じたピクセル値への変換
-		const timingInPixels = convertUnitToPixels(timing, unit);
+	const sliderClasses = classNames(
+		`vk_fixed-display vk_fixed-display-${mode} vk_fixed-display-position-${position} vk_fixed-display-${blockId}`
+	);
 
-		// スクロール位置が指定したタイミングを超えた場合に.is-visibleクラスを付与
-		if (window.scrollY > timingInPixels) {
-			item.classList.add('is-visible');
-		} else {
-			item.classList.remove('is-visible');
-		}
-	});
-});
-
-// 単位をピクセル値に変換する関数
-function convertUnitToPixels(value, unit) {
-	switch(unit) {
-		case 'px':
-			return value;
-		case 'em':
-			return value * parseFloat(getComputedStyle(document.documentElement).fontSize);
-		case 'rem':
-			return value * parseFloat(getComputedStyle(document.documentElement).fontSize);
-		case 'vh':
-			return value * window.innerHeight / 100;
-		case 'vw':
-			return value * window.innerWidth / 100;
-		default:
-			return value; // 単位が不明な場合はピクセル値として扱う
-	}
+	return (
+		<div
+			{...useBlockProps.save({
+				className: sliderClasses,
+			})}
+		>
+			<InnerBlocks.Content />
+		</div>
+	);
 }

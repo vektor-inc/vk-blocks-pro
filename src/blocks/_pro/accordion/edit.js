4c45190b8488a28f2cfb4f22408e244827fe6113
mtdkei <mtdkei@gmail.com>
2024-04-19 16:37:12 +0900

    Add setting items for each device

--- src/blocks/_pro/accordion/edit.js
+++ src/blocks/_pro/accordion/edit.js
@@ -1,14 +1,23 @@
 import {
-	InnerBlocks,
-	useBlockProps,
-	InspectorControls,
+    InnerBlocks,
+    useBlockProps,
+    InspectorControls,
 } from '@wordpress/block-editor';
-import { useEffect } from '@wordpress/element';
-import { PanelBody, SelectControl } from '@wordpress/components';
+import { useEffect, useState } from '@wordpress/element';
+import { PanelBody, SelectControl, ToggleControl } from '@wordpress/components';
 
 export default function AccordionEdit(props) {
 	const { attributes, setAttributes, clientId } = props;
-	const { containerClass, initialState } = attributes;
+	const { containerClass, initialState, initialStateMobile, initialStateTablet, initialStateDesktop, isDeviceSpecific } = attributes;
+	
+	// 状態管理オプションの表示を切り替えるためのトグル
+    const [userIsDeviceSpecific, setUserIsDeviceSpecific] = useState(isDeviceSpecific);
+
+	// トグルの状態を更新する関数
+    const toggleDeviceSpecific = () => {
+        setUserIsDeviceSpecific(!userIsDeviceSpecific);
+        setAttributes({ isDeviceSpecific: !userIsDeviceSpecific });
+    };
 
 	// コンソールエラー回避のため useEffect を使用（実行タイミングの問題）
 	useEffect(() => {
@@ -35,21 +44,56 @@ export default function AccordionEdit(props) {
 
 	return (
 		<div {...blockProps}>
-			<InspectorControls>
-				<PanelBody title="Accordion Setting">
+		<InspectorControls>
+			<PanelBody title="Accordion Settings">
+				<ToggleControl
+					label="Set initial state per device"
+                    checked={isDeviceSpecific}
+                    onChange={toggleDeviceSpecific}
+				/>
+				{isDeviceSpecific ? (
+					<>
+						<SelectControl
+							label="Mobile Initial State"
+							value={initialStateMobile || initialState}
+							options={[
+								{ label: 'Open', value: 'open' },
+								{ label: 'Closed', value: 'closed' }
+							]}
+							onChange={(value) => setAttributes({ initialStateMobile: value })}
+						/>
+						<SelectControl
+							label="Tablet Initial State"
+							value={initialStateTablet || initialState}
+							options={[
+								{ label: 'Open', value: 'open' },
+								{ label: 'Closed', value: 'closed' }
+							]}
+							onChange={(value) => setAttributes({ initialStateTablet: value })}
+						/>
+						<SelectControl
+							label="Desktop Initial State"
+							value={initialStateDesktop || initialState}
+							options={[
+								{ label: 'Open', value: 'open' },
+								{ label: 'Closed', value: 'closed' }
+							]}
+							onChange={(value) => setAttributes({ initialStateDesktop: value })}
+						/>
+					</>
+				) : (
 					<SelectControl
-						label="Initial State"
+						label="Default Initial State"
 						value={initialState}
 						options={[
 							{ label: 'Open', value: 'open' },
-							{ label: 'Closed', value: 'closed' },
+							{ label: 'Closed', value: 'closed' }
 						]}
-						onChange={(value) =>
-							setAttributes({ initialState: value })
-						}
+						onChange={(value) => setAttributes({ initialState: value })}
 					/>
-				</PanelBody>
-			</InspectorControls>
+				)}
+			</PanelBody>
+		</InspectorControls>
 			<InnerBlocks
 				allowedBlocks={ALLOWED_BLOCKS}
 				template={TEMPLATE}

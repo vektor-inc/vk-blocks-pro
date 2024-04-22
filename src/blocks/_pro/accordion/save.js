de0436e66844b91738c1f28badbba09b2139fdaf
mtdkei <mtdkei@gmail.com>
2024-04-22 14:08:44 +0900

    Minor adjustment

--- src/blocks/_pro/accordion/save.js
+++ src/blocks/_pro/accordion/save.js
@@ -1,7 +1,14 @@
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function save(props) {
-    const { containerClass, initialState, initialStateMobile, initialStateTablet, initialStateDesktop, isDeviceSpecific } = props.attributes;
+    const {
+		containerClass,
+		initialState,
+		initialStateMobile,
+		initialStateTablet,
+		initialStateDesktop,
+		isDeviceSpecific
+	} = props.attributes;
     const blockProps = useBlockProps.save({
         'data-initial-state': initialState,
         'data-initial-state-mobile': initialStateMobile,

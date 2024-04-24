55ca636397bf7b2eed616894476c5c38619fc1d9
mtdkei <mtdkei@gmail.com>
2024-04-24 13:54:21 +0900

    Add 1_72_1

--- src/blocks/slider-item/deprecated/index.js
+++ src/blocks/slider-item/deprecated/index.js
@@ -5,6 +5,7 @@ import save1_9_2 from './1.9.2/save';
 import save1_22_1 from './1.22.1/save';
 import save1_27_7 from './1.27.7/save';
 import save1_34_1 from './1.34.1/save';
+import save1_72_1 from './1.72.1/save';
 
 const blockAttributes = {
 	verticalAlignment: {
@@ -97,6 +98,10 @@ const blockAttributes5 = {
 */
 
 export default [
+	{
+		attributes: blockAttributes4,
+		save: save1_72_1,
+	},
 	{
 		attributes: blockAttributes4,
 		save: save1_34_1,

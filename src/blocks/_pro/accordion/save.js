79d31ff59e3d7d09cd279e29f6d2f03ad42ba599
mtdkei <mtdkei@gmail.com>
2024-04-26 15:50:24 +0900

    Convert indent to tab

--- src/blocks/_pro/accordion/save.js
+++ src/blocks/_pro/accordion/save.js
@@ -1,24 +1,26 @@
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function save(props) {
-    const {
+	const {
 		containerClass,
 		initialState,
 		initialStateMobile,
 		initialStateTablet,
 		initialStateDesktop,
+		isDeviceSpecific,
 	} = props.attributes;
-    const blockProps = useBlockProps.save({
-	className: containerClass,
-        'data-initial-state': initialState,
-        'data-initial-state-mobile': initialStateMobile,
-        'data-initial-state-tablet': initialStateTablet,
-        'data-initial-state-desktop': initialStateDesktop
-    });
+	const blockProps = useBlockProps.save({
+		className: containerClass,
+		'data-initial-state': initialState,
+		'data-initial-state-mobile': initialStateMobile,
+		'data-initial-state-tablet': initialStateTablet,
+		'data-initial-state-desktop': initialStateDesktop,
+		'data-is-device-specific': isDeviceSpecific
+	});
 
-    return (
-        <div {...blockProps}>
-            <InnerBlocks.Content />
-        </div>
-    );
+	return (
+		<div {...blockProps}>
+			<InnerBlocks.Content />
+		</div>
+	);
 }

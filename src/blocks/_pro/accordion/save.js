ebcac83768953c9095c1d6b0c9e60fde85548f78
mtdkei <mtdkei@gmail.com>
2024-05-21 13:08:31 +0900

    Add initialState

--- src/blocks/_pro/accordion/save.js
+++ src/blocks/_pro/accordion/save.js
@@ -1,9 +1,19 @@
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function save(props) {
-	const { containerClass } = props.attributes;
+	const {
+		containerClass,
+		initialState,
+		initialStateMobile,
+		initialStateTablet,
+		initialStateDesktop,
+	} = props.attributes;
 	const blockProps = useBlockProps.save({
-		className: `${containerClass}`,
+		className: containerClass,
+		'data-initial-state': initialState,
+		'data-initial-state-mobile': initialStateMobile,
+		'data-initial-state-tablet': initialStateTablet,
+		'data-initial-state-desktop': initialStateDesktop,
 	});
 
 	return (

d81fc076e6a939d02e2975b5ed93e29840d9836f
mtdkei <mtdkei@gmail.com>
2024-05-27 08:53:53 +0900

    Exact match of blockAttributes

--- src/blocks/_pro/tab/deprecated/index.js
+++ src/blocks/_pro/tab/deprecated/index.js
@@ -3,21 +3,27 @@ import save1_73_0 from './1.73.0/save';
 const blockAttributes = {
 	tabOptionJSON: {
 		type: 'string',
+		default: '{}',
 	},
 	tabSizeSp: {
 		type: 'string',
+		default: 'fitText',
 	},
 	tabSizeTab: {
 		type: 'string',
+		default: 'fitText',
 	},
 	tabSizePc: {
 		type: 'string',
+		default: 'fitText',
 	},
 	firstActive: {
 		type: 'number',
+		default: 0,
 	},
 	blockId: {
 		type: 'string',
+		default: '',
 	},
 };
 
@@ -27,4 +33,5 @@ const deprecated = [
 		save: save1_73_0,
 	},
 ];
+
 export default deprecated;

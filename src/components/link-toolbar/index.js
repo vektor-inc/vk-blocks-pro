be9a3357e957ee812e60b5d23b16cef9a9ea795f
mtdkei <mtdkei@gmail.com>
2024-05-31 14:32:56 +0900

    Add lonk toolbar component

---
+++ src/components/link-toolbar/index.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import {
+    ToolbarButton,
+    Dropdown,
+    CheckboxControl,
+    Button,
+} from '@wordpress/components';
+import { URLInput } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import { link, linkOff, keyboardReturn } from '@wordpress/icons';
+
+const LinkToolbar = ({ linkUrl, setLinkUrl, linkTarget, setLinkTarget }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <Dropdown
+            isOpen={isOpen}
+            onToggle={toggleDropdown}
+            popoverProps={{ placement: 'bottom-start' }}
+            renderToggle={({ isOpen, onToggle }) => {
+                const setLink = () => {
+                    if (isOpen && linkUrl !== '') {
+                        setLinkUrl('');
+                    }
+                    onToggle();
+                };
+                return (
+                    <ToolbarButton
+                        aria-expanded={isOpen}
+                        icon={linkUrl !== '' ? linkOff : link}
+                        isActive={linkUrl !== ''}
+                        label={linkUrl !== '' ? __('Unlink') : __('Input Link URL', 'vk-blocks-pro')}
+                        onClick={setLink}
+                    />
+                );
+            }}
+            renderContent={({ onClose }) => (
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        onClose();
+                    }}
+                >
+                    <div className="vk-block-editor-url-input-wrapper">
+                        <URLInput
+                            __nextHasNoMarginBottom
+                            value={linkUrl}
+                            onChange={(value) => setLinkUrl(value)}
+                        />
+                        <Button
+                            icon={keyboardReturn}
+                            label={__('Submit')}
+                            type="submit"
+                        />
+                    </div>
+                    <CheckboxControl
+                        label={__('Open link new tab.', 'vk-blocks-pro')}
+                        checked={linkTarget === '_blank'}
+                        onChange={(checked) => setLinkTarget(checked ? '_blank' : '')}
+                    />
+                </form>
+            )}
+        />
+    );
+};
+
+export default LinkToolbar;

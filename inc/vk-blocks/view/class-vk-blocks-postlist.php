c80cdace5c2106e1c77643575d5d322f9729f03e
mtdkei <mtdkei@gmail.com>
2024-06-12 15:17:18 +0900

    Minor corrections

--- inc/vk-blocks/view/class-vk-blocks-postlist.php
+++ inc/vk-blocks/view/class-vk-blocks-postlist.php
@@ -54,16 +54,16 @@ class Vk_Blocks_PostList {
 			'class_title'                => '',
 			'body_prepend'               => '',
 			'body_append'                => '',
-			'vkb_hidden'                 => $attributes['vkb_hidden'],
-			'vkb_hidden_xxl'             => $attributes['vkb_hidden_xxl'],
-			'vkb_hidden_xl'              => $attributes['vkb_hidden_xl'],
-			'vkb_hidden_xl_v2'           => $attributes['vkb_hidden_xl_v2'],
-			'vkb_hidden_lg'              => $attributes['vkb_hidden_lg'],
-			'vkb_hidden_md'              => $attributes['vkb_hidden_md'],
-			'vkb_hidden_sm'              => $attributes['vkb_hidden_sm'],
-			'vkb_hidden_xs'              => $attributes['vkb_hidden_xs'],
-			'marginTop'                  => $attributes['marginTop'],
-			'marginBottom'               => $attributes['marginBottom'],
+			'vkb_hidden'                 => isset( $attributes['vkb_hidden'] ) ? $attributes['vkb_hidden'] : '',
+			'vkb_hidden_xxl'             => isset( $attributes['vkb_hidden_xxl'] ) ? $attributes['vkb_hidden_xxl'] : '',
+			'vkb_hidden_xl'              => isset( $attributes['vkb_hidden_xl'] ) ? $attributes['vkb_hidden_xl'] : '',
+			'vkb_hidden_xl_v2'           => isset( $attributes['vkb_hidden_xl_v2'] ) ? $attributes['vkb_hidden_xl_v2'] : '',
+			'vkb_hidden_lg'              => isset( $attributes['vkb_hidden_lg'] ) ? $attributes['vkb_hidden_lg'] : '',
+			'vkb_hidden_md'              => isset( $attributes['vkb_hidden_md'] ) ? $attributes['vkb_hidden_md'] : '',
+			'vkb_hidden_sm'              => isset( $attributes['vkb_hidden_sm'] ) ? $attributes['vkb_hidden_sm'] : '',
+			'vkb_hidden_xs'              => isset( $attributes['vkb_hidden_xs'] ) ? $attributes['vkb_hidden_xs'] : '',
+			'marginTop'                  => isset( $attributes['marginTop'] ) ? $attributes['marginTop'] : '',
+			'marginBottom'               => isset( $attributes['marginBottom'] ) ? $attributes['marginBottom'] : '',
 		);
 
 		$elm = VK_Component_Posts::get_loop( $wp_query, $options, $options_loop );
@@ -128,8 +128,8 @@ class Vk_Blocks_PostList {
 	public static function get_loop_query( $attributes ) {
 		$is_checked_post_type = json_decode( $attributes['isCheckedPostType'], true );
 
-		$is_checked_terms     = json_decode( $attributes['isCheckedTerms'], true );
-		$tax_query_relation   = isset( $attributes['taxQueryRelation'] ) ? $attributes['taxQueryRelation'] : 'OR';
+		$is_checked_terms   = json_decode( $attributes['isCheckedTerms'], true );
+		$tax_query_relation = isset( $attributes['taxQueryRelation'] ) ? $attributes['taxQueryRelation'] : 'OR';
 
 		if ( empty( $is_checked_post_type ) ) {
 			return false;

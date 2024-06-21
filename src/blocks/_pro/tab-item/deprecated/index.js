916277261f26e1767081710a7983a58babb2f1c9
mtdkei <mtdkei@gmail.com>
2024-06-21 15:12:37 +0900

    Add deprecated files

---
+++ src/blocks/_pro/tab-item/deprecated/index.js
@@ -0,0 +1,13 @@
+import save1_76_0 from './1.76.0/save';
+
+const blockAttributes = {
+};
+
+const deprecated = [
+	{
+		attributes: blockAttributes,
+		save: save1_76_0,
+	},
+];
+
+export default deprecated;

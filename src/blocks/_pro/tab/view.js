2701372b074b700c23f9806c1dbd481e9344a02f
mtdkei <mtdkei@gmail.com>
2024-06-21 12:24:31 +0900

    Added hidden attribute

--- src/blocks/_pro/tab/view.js
+++ src/blocks/_pro/tab/view.js
@@ -165,7 +165,7 @@ document.addEventListener('DOMContentLoaded', function () {
 					const activeTabBody = vkTabBodies.querySelector(
 						`#vk_tab_bodys_body-${TabId}`
 					);
-					const activeTabBodyStyle =
+						const activeTabBodyStyle =
 						window.getComputedStyle(activeTabBody);
 					newActiveLabel.style.backgroundColor =
 						activeTabBodyStyle.borderTopColor || '';
@@ -175,7 +175,8 @@ document.addEventListener('DOMContentLoaded', function () {
 					`#vk_tab_bodys_body-${TabId}`
 				);
 				newActiveBody.classList.add('vk_tab_bodys_body-state-active');
-
+				newActiveBody.removeAttribute('hidden');
+				
 				e.target.setAttribute('tabindex', '0');
 				e.target.setAttribute('aria-selected', 'true');
 

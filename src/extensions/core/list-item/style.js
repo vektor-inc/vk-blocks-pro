278b420836493157cc6e0f4b7e7650dc7c5b6001
mtdkei <mtdkei@gmail.com>
2024-06-25 08:26:04 +0900

    Add list-item

---
+++ src/extensions/core/list-item/style.js
@@ -0,0 +1,78 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { addFilter } from '@wordpress/hooks';
+import { PanelBody, Button } from '@wordpress/components';
+import { InspectorControls } from '@wordpress/block-editor';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { select, dispatch } from '@wordpress/data';
+
+const isValidBlockType = (name) => {
+	const validBlockTypes = ['core/list-item'];
+	return validBlockTypes.includes(name);
+};
+
+export const addAttribute = (settings) => {
+	if (isValidBlockType(settings.name)) {
+		settings.attributes = {
+			...settings.attributes,
+			returnToList: {
+				type: 'boolean',
+				default: false,
+			},
+		};
+	}
+	return settings;
+};
+addFilter(
+	'blocks.registerBlockType',
+	'vk-blocks/list-item-style',
+	addAttribute
+);
+
+export const addBlockControl = createHigherOrderComponent(
+	(BlockEdit) => (props) => {
+		const { name, attributes, setAttributes } = props;
+		const { returnToList } = attributes;
+
+		if (!isValidBlockType(name)) {
+			return <BlockEdit {...props} />;
+		}
+
+		return (
+			<>
+				<BlockEdit {...props} />
+				<InspectorControls>
+					<PanelBody
+						title={__('List Item Settings', 'vk-blocks-pro')}
+						initialOpen={true}
+					>
+						<Button
+							isSecondary
+							onClick={() => {
+								setAttributes({ returnToList: !returnToList }); // トグルとして使用
+								// 親ブロック (list) に戻る処理
+								const clientId =
+									select(
+										'core/block-editor'
+									).getSelectedBlockClientId();
+								const parentBlock =
+									select(
+										'core/block-editor'
+									).getBlockHierarchyRootClientId(clientId);
+								dispatch('core/block-editor').selectBlock(
+									parentBlock
+								);
+							}}
+						>
+							{__('Return to List', 'vk-blocks-pro')}
+						</Button>
+					</PanelBody>
+				</InspectorControls>
+			</>
+		);
+	},
+	'addMyCustomBlockControls'
+);
+addFilter('editor.BlockEdit', 'vk-blocks/list-item-style', addBlockControl);

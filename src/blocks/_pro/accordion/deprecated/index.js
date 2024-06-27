4e6df6022b59da71a572e9b60ffa95f17a90cd7e
mtdkei <mtdkei@gmail.com>
2024-06-27 12:31:34 +0900

    Add deprecated file for ver.1.76.0

--- src/blocks/_pro/accordion/deprecated/index.js
+++ src/blocks/_pro/accordion/deprecated/index.js
@@ -1,7 +1,9 @@
 import save1_3_9 from './1.3.9/save';
 import save1_12_0 from './1.12.0/save';
+import save1_76_0 from './1.76.0/save';
 
 const blockAttributes = {};
+
 
 /**
  * 1.12.x で値を追加
@@ -13,9 +15,40 @@ const blockAttributes2 = {
 	},
 };
 
+/**
+ * 1.76.0 で値を追加
+ */
+const blockAttributes3 = {
+	...blockAttributes2,
+	initialState: {
+		type: "string",
+		default: "close"
+	},
+	initialStateMobile: {
+		type: "string",
+		default: ""
+	},
+	initialStateTablet: {
+		type: "string",
+		default: ""
+	},
+	initialStateDesktop: {
+		type: "string",
+		default: ""
+	},
+	isDeviceSpecific: {
+		type: "boolean",
+		default: false
+	}
+};
+
 const deprecated = [
 	{
-		attributes: blockAttributes,
+		attributes: blockAttributes3,
+		save: save1_76_0
+	},
+	{
+		attributes: blockAttributes2,
 		save: save1_12_0
 	},
 	{

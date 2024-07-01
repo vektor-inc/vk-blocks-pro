545333ea2b6c823364089b5f5ef025a836fa75d4
sysbird <koro@sysbird.jp>
2024-07-01 18:31:57 +0900

    add textContent at close button #2049

--- src/blocks/_pro/table-of-contents-new/view.js
+++ src/blocks/_pro/table-of-contents-new/view.js
@@ -4,4 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.querySelectorAll('.vk_tableOfContents_list li').forEach((item) => {
 		item.innerHTML = item.innerHTML.replace(/\uFFFC/g, ''); // U+FFFCはOBJのUnicodeです
 	});
+
+	// 開/閉 切り替え
+	const openButton = document.getElementById('vk-tab-label');
+	openButton.addEventListener('click', function () {
+		const openButton = document.getElementById('vk-tab-label');
+		const statusText = openButton.textContent.trim();
+		if ('OPEN' === statusText) {
+			openButton.textContent = 'CLOSE';
+		} else {
+			openButton.textContent = 'OPEN';
+		}
+	});
 });

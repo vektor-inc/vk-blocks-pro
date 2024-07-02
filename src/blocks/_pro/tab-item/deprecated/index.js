7a82c7f35364ea96feed29bd246ab4dafc9eb0b4
mtdkei <mtdkei@gmail.com>
2024-07-03 08:45:42 +0900

    Revert "Add deprecated file for ver.1.76.2"
    This reverts commit d4b3eb10d1e0c104e28ed7a77b4935e7642d5f6d.

--- src/blocks/_pro/tab-item/deprecated/index.js
+++ src/blocks/_pro/tab-item/deprecated/index.js
@@ -1,13 +1,8 @@
 import save1_75_0 from './1.75.0/save';
-import save1_76_2 from './1.76.2/save';
 
 const blockAttributes = {};
 
 const deprecated = [
-	{
-		attributes: blockAttributes,
-		save: save1_76_2,
-	},
 	{
 		attributes: blockAttributes,
 		save: save1_75_0,

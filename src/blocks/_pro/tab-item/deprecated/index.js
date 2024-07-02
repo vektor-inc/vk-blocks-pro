b48860c5c02fcd8743edde807070fb2349356e04
mtdkei <mtdkei@gmail.com>
2024-07-03 08:42:33 +0900

    Update index.js

--- src/blocks/_pro/tab-item/deprecated/index.js
+++ src/blocks/_pro/tab-item/deprecated/index.js
@@ -1,8 +1,13 @@
 import save1_75_0 from './1.75.0/save';
+import save1_76_2 from './1.76.2/save';
 
 const blockAttributes = {};
 
 const deprecated = [
+	{
+		attributes: blockAttributes,
+		save: save1_76_2,
+	},
 	{
 		attributes: blockAttributes,
 		save: save1_75_0,

a88ac38e263098105c290672491f8a97ecc53a3d
mtdkei <mtdkei@gmail.com>
2024-07-05 14:56:55 +0900

    Add deprecated file for ver.1.78.0

--- src/blocks/_pro/outer/deprecated/save/index.js
+++ src/blocks/_pro/outer/deprecated/save/index.js
@@ -20,6 +20,7 @@ import save1_61_2 from './1.61.2/save';
 import save1_64_0 from './1.64.0/save';
 import save1_71_0 from './1.71.0/save';
 import save1_76_0 from './1.76.0/save';
+import save1_78_0 from './1.76.0/save';
 
 const blockAttributes = {
 	bgColor: {
@@ -193,6 +194,10 @@ const blockAttributes8 = {
 */
 
 const deprecated = [
+	{
+		attributes: blockAttributes7,
+		save: save1_78_0,
+	},
 	{
 		attributes: blockAttributes7,
 		save: save1_76_0,

2e36474fc11be3a020726149ff9d0fb85a06c166
mtdkei <mtdkei@gmail.com>
2024-07-05 14:33:09 +0900

    Minor correction

--- src/blocks/_pro/tab/deprecated/index.js
+++ src/blocks/_pro/tab/deprecated/index.js
@@ -1,7 +1,7 @@
 import save1_73_0 from './1.73.0/save';
 import save1_74_0 from './1.74.0/save';
 import save1_75_0 from './1.75.0/save';
-import save1_75_0 from './1.78.0/save';
+import save1_78_0 from './1.78.0/save';
 
 const blockAttributes = {
 	tabOptionJSON: {

c3ded932fea130ace3b739890216fb1a0c473f01
mtdkei <mtdkei@gmail.com>
2024-07-11 15:27:46 +0900

    Minor correction

--- src/blocks/_pro/outer/deprecated/save/index.js
+++ src/blocks/_pro/outer/deprecated/save/index.js
@@ -20,7 +20,7 @@ import save1_61_2 from './1.61.2/save';
 import save1_64_0 from './1.64.0/save';
 import save1_71_0 from './1.71.0/save';
 import save1_76_0 from './1.76.0/save';
-import save1_78_0 from './1.76.0/save';
+import save1_78_0 from './1.78.0/save';
 
 const blockAttributes = {
 	bgColor: {

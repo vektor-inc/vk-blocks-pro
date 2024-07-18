a708b82662f9c2d87d2f11279649b7c59550a49e
drill-lancer <rickaddison7634@gmail.com>
2024-07-18 09:26:29 +0900

    add: icon

--- src/blocks/alert/save.js
+++ src/blocks/alert/save.js
@@ -1,11 +1,36 @@
-import { RichText, useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps, RichText } from '@wordpress/block-editor';
+import parse from 'html-react-parser';
 
 export default function save({ attributes }) {
-	const { style, content } = attributes;
+	const { style, icon, iconText } = attributes;
+
+	const blockProps = useBlockProps.save({
+		className: `alert alert-${style}`,
+	});
+
+	let alertIcon = '';
+	if (
+		icon !== '' &&
+		icon !== undefined
+	) {
+		alertIcon = (
+			<div className="alert-icon">
+				<div className="alert-icon-icon">{parse(icon)}</div>
+				<div className="alert-icon-text">
+					<RichText.Content
+						tagName="span"
+						value={iconText}
+						
+					/>
+				</div>
+			</div>
+		);
+	}
 
 	return (
-		<div {...useBlockProps.save({ className: `alert alert-${style}` })}>
-			<RichText.Content tagName={'p'} value={content} />
+		<div {...blockProps}>
+			{alertIcon}
+			<InnerBlocks.Content />
 		</div>
 	);
 }

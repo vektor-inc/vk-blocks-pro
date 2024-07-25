2489974d76dff5aec1d83a058fd37cf747477d6f
松田啓 <mt@matsudakeinoMacBook-Air.local>
2024-07-25 11:51:04 +0900

    Fix error

--- inc/vk-blocks/view/class-vk-blocks-postlist.php
+++ inc/vk-blocks/view/class-vk-blocks-postlist.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class VK Blocks PostList
  *
@@ -24,16 +25,17 @@ class PostList
      *
      * @return string
      */
-    public static function render_post_list($attributes, $wp_query, $options_loop)
+    public static function renderPostList($attributes, $wp_query, $options_loop)
     {
         if (!empty($attributes['className'])) {
             $options_loop['class_loop_outer'] .= ' ' . $attributes['className'];
         }
 
         if (!isset($wp_query) || false === $wp_query || 'false' === $wp_query || empty($wp_query->posts)) {
-            return wp_kses_post(self::get_render_no_post($wp_query));
+            return wp_kses_post(self::getRenderNoPost($wp_query));
         }
-        $options = array(
+
+        $options = [
             'layout'                     => esc_html($attributes['layout']),
             'slug'                       => '',
             'display_image'              => esc_html($attributes['display_image']),
@@ -60,17 +62,17 @@ class PostList
             'class_title'                => '',
             'body_prepend'               => '',
             'body_append'                => '',
-            'vkb_hidden'                 => isset($attributes['vkb_hidden']) ? $attributes['vkb_hidden'] : '',
-            'vkb_hidden_xxl'             => isset($attributes['vkb_hidden_xxl']) ? $attributes['vkb_hidden_xxl'] : '',
-            'vkb_hidden_xl'              => isset($attributes['vkb_hidden_xl']) ? $attributes['vkb_hidden_xl'] : '',
-            'vkb_hidden_xl_v2'           => isset($attributes['vkb_hidden_xl_v2']) ? $attributes['vkb_hidden_xl_v2'] : '',
-            'vkb_hidden_lg'              => isset($attributes['vkb_hidden_lg']) ? $attributes['vkb_hidden_lg'] : '',
-            'vkb_hidden_md'              => isset($attributes['vkb_hidden_md']) ? $attributes['vkb_hidden_md'] : '',
-            'vkb_hidden_sm'              => isset($attributes['vkb_hidden_sm']) ? $attributes['vkb_hidden_sm'] : '',
-            'vkb_hidden_xs'              => isset($attributes['vkb_hidden_xs']) ? $attributes['vkb_hidden_xs'] : '',
-            'margin_top'                  => isset($attributes['marginTop']) ? $attributes['marginTop'] : '',
-            'margin_bottom'               => isset($attributes['marginBottom']) ? $attributes['marginBottom'] : '',
-        );
+            'vkb_hidden'                 => $attributes['vkb_hidden'] ?? '',
+            'vkb_hidden_xxl'             => $attributes['vkb_hidden_xxl'] ?? '',
+            'vkb_hidden_xl'              => $attributes['vkb_hidden_xl'] ?? '',
+            'vkb_hidden_xl_v2'           => $attributes['vkb_hidden_xl_v2'] ?? '',
+            'vkb_hidden_lg'              => $attributes['vkb_hidden_lg'] ?? '',
+            'vkb_hidden_md'              => $attributes['vkb_hidden_md'] ?? '',
+            'vkb_hidden_sm'              => $attributes['vkb_hidden_sm'] ?? '',
+            'vkb_hidden_xs'              => $attributes['vkb_hidden_xs'] ?? '',
+            'margin_top'                 => $attributes['marginTop'] ?? '',
+            'margin_bottom'              => $attributes['marginBottom'] ?? '',
+        ];
 
         $elm = VK_Component_Posts::get_loop($wp_query, $options, $options_loop);
 
@@ -86,12 +88,9 @@ class PostList
      *
      * @return bool
      */
-    private function is_array_exist($arr)
+    private function isArrayExist($arr)
     {
-        if (!$arr) {
-            return false;
-        }
-        return true;
+        return !empty($arr);
     }
 
     /**
@@ -103,11 +102,11 @@ class PostList
      *
      * @return array $return : tax_query
      */
-    private static function format_terms($tax_query_relation, $is_checked_terms, $post_type)
+    private static function formatTerms($tax_query_relation, $is_checked_terms, $post_type)
     {
-        $return = array(
+        $return = [
             'relation' => $tax_query_relation,
-        );
+        ];
 
         foreach ($is_checked_terms as $term_id) {
             $term = get_term($term_id);
@@ -117,11 +116,11 @@ class PostList
             $post_type_taxonomies = get_object_taxonomies($post_type);
 
             if (in_array($term->taxonomy, $post_type_taxonomies, true)) {
-                $new_array = array(
+                $new_array = [
                     'taxonomy' => $term->taxonomy,
                     'field'    => 'term_id',
                     'terms'    => $term_id,
-                );
+                ];
                 $return[]  = $new_array;
             }
         }
@@ -135,53 +134,53 @@ class PostList
      *
      * @return WP_Query|bool WP_Query object or false.
      */
-    public static function get_loop_query($attributes)
+    public static function getLoopQuery($attributes)
     {
         $is_checked_post_type = json_decode($attributes['isCheckedPostType'], true);
 
         $is_checked_terms   = json_decode($attributes['isCheckedTerms'], true);
-        $tax_query_relation = isset($attributes['taxQueryRelation']) ? $attributes['taxQueryRelation'] : 'OR';
+        $tax_query_relation = $attributes['taxQueryRelation'] ?? 'OR';
 
         if (empty($is_checked_post_type)) {
             return false;
         }
 
-        $post__not_in = array();
+        $post__not_in = [];
         if (!empty($attributes['selfIgnore'])) {
-            $post__not_in = array(get_the_ID());
+            $post__not_in = [get_the_ID()];
         }
 
         $offset = isset($attributes['offset']) ? intval($attributes['offset']) : 0;
 
-        $date_query = array();
+        $date_query = [];
         if (!empty($attributes['targetPeriod'])) {
             switch ($attributes['targetPeriod']) {
                 case 'from-today':
-                    $date_query = array(
-                        array(
+                    $date_query = [
+                        [
                             'column'    => 'post_date_gmt',
                             'after'     => gmdate('Y-m-d'),
                             'inclusive' => true,
-                        ),
-                    );
+                        ],
+                    ];
                     break;
                 case 'from-now':
-                    $date_query = array(
-                        array(
+                    $date_query = [
+                        [
                             'column'    => 'post_date_gmt',
                             'after'     => gmdate('Y-m-d H:i:s'),
                             'inclusive' => true,
-                        ),
-                    );
+                        ],
+                    ];
                     break;
                 case 'from-tomorrow':
-                    $date_query = array(
-                        array(
+                    $date_query = [
+                        [
                             'column'    => 'post_date_gmt',
                             'after'     => gmdate('Y-m-d', strtotime('+1 day')),
                             'inclusive' => true,
-                        ),
-                    );
+                        ],
+                    ];
                     break;
             }
         }
@@ -197,12 +196,12 @@ class PostList
             $paged = $wp_query->query_vars['paged'];
         }
 
-        $all_posts    = array();
+        $all_posts    = [];
         $offset_count = 0;
 
         foreach ($is_checked_post_type as $post_type) {
             $posts_per_page = 100; // 一度に取得する投稿件数を制限
-            $args = array(
+            $args = [
                 'post_type'      => $post_type,
                 'paged'          => $paged,
                 'posts_per_page' => $posts_per_page,
@@ -211,13 +210,8 @@ class PostList
                 'post__not_in'   => $post__not_in,
                 'date_query'     => $date_query,
                 // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_tax_query
-                'tax_query'      => !empty($is_checked_terms) ? self::format_terms($tax_query_relation, $is_checked_terms, $post_type) : array(),
-            );
-
-            if (!empty($is_checked_terms)) {
-                $args['tax_query'] = self::format_terms
-                ($tax_query_relation, $is_checked_terms, $post_type);
-            }
+                'tax_query'      => !empty($is_checked_terms) ? self::formatTerms($tax_query_relation, $is_checked_terms, $post_type) : [],
+            ];
 
             while (true) {
                 $temp_query = new WP_Query($args);
@@ -278,15 +272,15 @@ class PostList
      *
      * @return WP_Query|bool WP_Query object or false.
      */
-    public static function get_loop_query_child($attributes)
+    public static function getLoopQueryChild($attributes)
     {
-        // ParentIdを指定.
+        // ParentIdを指定
         if (isset($attributes['selectId']) && 'false' !== $attributes['selectId']) {
             $select_id = ($attributes['selectId'] > 0) ? $attributes['selectId'] : get_the_ID();
 
-            $post__not_in = array();
+            $post__not_in = [];
             if (!empty($attributes['selfIgnore'])) {
-                $post__not_in = array(get_the_ID());
+                $post__not_in = [get_the_ID()];
             }
 
             $offset = '';
@@ -294,17 +288,16 @@ class PostList
                 $offset = intval($attributes['offset']);
             }
 
-            $args = array(
+            $args = [
                 'post_type'      => 'page',
-                'paged'          => 0,
-                // 0で全件取得
+                'paged'          => 0, // 0で全件取得
                 'posts_per_page' => -1,
                 'order'          => 'ASC',
                 'orderby'        => 'menu_order',
                 'post_parent'    => intval($select_id),
                 'offset'         => $offset,
                 'post__not_in'   => $post__not_in,
-            );
+            ];
             return new WP_Query($args);
         } else {
             return false;
@@ -317,7 +310,7 @@ class PostList
      * @param object $wp_query @since 1.27.0.
      * @return string
      */
-    public static function get_render_no_post($wp_query = null)
+    public static function getRenderNoPost($wp_query = null)
     {
         $name = '';
         if (!empty($wp_query->query['post_type'])) {

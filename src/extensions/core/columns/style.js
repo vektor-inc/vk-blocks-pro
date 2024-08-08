26d53210d34a43cbfc42202b146776c289ac3ffc
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-08-08 15:21:52 +0900

    Add commentout

--- src/extensions/core/columns/style.js
+++ src/extensions/core/columns/style.js
@@ -54,7 +54,6 @@ export const addBlockControl = createHigherOrderComponent((BlockEdit) => {
 		const { reverse, className, linkUrl, linkTarget } = attributes;
 
 		if (isColumnsBlock(props.name) && props.isSelected) {
-
 			// カラムの方向設定
 			// アイコン設定
 			let iconStyle = {

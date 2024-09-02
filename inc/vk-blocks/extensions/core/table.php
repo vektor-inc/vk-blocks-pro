539358535e324f4a915ba3f9b50f9d1cb2237ff8
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-09-02 13:12:41 +0900

    Fix: Merge changes after rebase and apply scroll hint feature

--- inc/vk-blocks/extensions/core/table.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Extensions core table block style .
- *
- * @package vk-blocks
- */
-
-// VK_Blocks_ScrollHintRenderer クラスをインクルード
-require_once plugin_dir_path( __FILE__ ) . '../../view/class-vk-blocks-scroll-hint-renderer.php';

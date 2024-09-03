b4eb9d1843e58cdf2d2ff2da576629e4e27f67a8
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-09-03 09:02:49 +0900

    Fix: Ensure scroll hint attributes are correctly rendered for custom icon classes

--- inc/vk-blocks/view/class-vk-blocks-scrollhintrenderer.php
+++ inc/vk-blocks/view/class-vk-blocks-scrollhintrenderer.php
@@ -57,46 +57,68 @@ class VK_Blocks_ScrollHintRenderer {
 	 */
 	public static function generate_scroll_hint( $block ) {
 		$scroll_message_text = ! empty( $block['attrs']['scrollMessageText'] ) ? $block['attrs']['scrollMessageText'] : __( 'You can scroll', 'vk-blocks-pro' );
-		$scroll_icon_left    = ! empty( $block['attrs']['scrollIconLeft'] ) ? self::extract_icon_class( $block['attrs']['scrollIconLeft'] ) : 'fa-solid fa-caret-left';
-		$scroll_icon_right   = ! empty( $block['attrs']['scrollIconRight'] ) ? self::extract_icon_class( $block['attrs']['scrollIconRight'] ) : 'fa-solid fa-caret-right';
 
-		// スクロールブレイクポイントのデフォルト値を設定
-		$scroll_breakpoint   = self::determine_scroll_breakpoint( $block );
+		// クラス名の取得を修正：設定されているクラスをそのまま使用
+		$scroll_icon_left    = ! empty( $block['attrs']['scrollIconLeft'] ) ? $block['attrs']['scrollIconLeft'] : 'fa-solid fa-caret-left';
+		$scroll_icon_right   = ! empty( $block['attrs']['scrollIconRight'] ) ? $block['attrs']['scrollIconRight'] : 'fa-solid fa-caret-right';
 
-		return sprintf(
-			'<div class="vk-scroll-hint" data-scroll-breakpoint="%s" data-hint-icon-left="%s" data-hint-icon-right="%s">
-				<i class="%s"></i>
-				<span>%s</span>
-				<i class="%s"></i>
-			</div>',
-			esc_attr( $scroll_breakpoint ),
-			esc_attr( $scroll_icon_left ),
-			esc_attr( $scroll_icon_right ),
-			esc_attr( $scroll_icon_left ),
-			esc_html( $scroll_message_text ),
-			esc_attr( $scroll_icon_right )
-		);
+		// クラス名に基づいて data-scroll-breakpoint を設定
+		$scroll_breakpoint = self::determine_scroll_breakpoint( $block );
+
+		$scroll_hint = '<div class="vk-scroll-hint"';
+
+		// data-scroll-breakpoint 属性を追加
+		$scroll_hint .= ' data-scroll-breakpoint="' . esc_attr( $scroll_breakpoint ) . '"';
+
+		// data-hint-icon-left 属性を追加（設定されているクラスをそのまま使用）
+		if ( ! empty( $scroll_icon_left ) ) {
+			$scroll_hint .= ' data-hint-icon-left="' . esc_attr( $scroll_icon_left ) . '"';
+		}
+
+		// data-hint-icon-right 属性を追加（設定されているクラスをそのまま使用）
+		if ( ! empty( $scroll_icon_right ) ) {
+			$scroll_hint .= ' data-hint-icon-right="' . esc_attr( $scroll_icon_right ) . '"';
+		}
+
+		$scroll_hint .= '>';
+		$scroll_hint .= '<i class="' . esc_attr( $scroll_icon_left ) . '"></i>';
+		$scroll_hint .= '<span>' . esc_html( $scroll_message_text ) . '</span>';
+		$scroll_hint .= '<i class="' . esc_attr( $scroll_icon_right ) . '"></i>';
+		$scroll_hint .= '</div>';
+
+		return $scroll_hint;
 	}
 
 	/**
 	 * Determine the scroll breakpoint value.
 	 *
 	 * @param array $block The block data.
-	 * @return string The scroll breakpoint.
+	 * @return string The scroll breakpoint or an empty string if not found.
 	 */
 	private static function determine_scroll_breakpoint( $block ) {
-		// クラス名からプレフィックスを抽出してスクロールブレイクポイントを決定
+		// 属性で指定されたスクロールブレイクポイントが存在する場合はそれを返す
 		if ( ! empty( $block['attrs']['scrollBreakpoint'] ) ) {
 			return $block['attrs']['scrollBreakpoint'];
 		}
 
-		// デフォルトのブレイクポイントを設定 (例: table-scrollable-mobile)
-		if ( preg_match( '/is-style-vk-([a-zA-Z0-9_-]+)-scrollable/', $block['attrs']['className'], $matches ) ) {
-			return $matches[1] . '-scrollable-mobile';
+		// クラス名から 'is-style-vk-*' パターンを見つけて、それに基づいてスクロールブレイクポイントを設定
+		if ( ! empty( $block['attrs']['className'] ) && preg_match( '/is-style-vk-([a-zA-Z0-9_-]+)-scrollable/', $block['attrs']['className'], $matches ) ) {
+			$prefix = $matches[1]; // プレフィックス部分を抽出
+			// クラス名に基づく条件分岐を設定
+			if ( strpos( $block['attrs']['className'], 'vk_tab_labels--scroll-pc' ) !== false ) {
+				return $prefix . '-scrollable-pc';
+			} elseif ( strpos( $block['attrs']['className'], 'vk_tab_labels--scroll-tablet' ) !== false ) {
+				return $prefix . '-scrollable-tablet';
+			} elseif ( strpos( $block['attrs']['className'], 'vk_tab_labels--scroll-mobile' ) !== false ) {
+				return $prefix . '-scrollable-mobile';
+			}
+
+			// デフォルトのブレイクポイントを返す
+			return $prefix . '-scrollable-mobile';
 		}
 
-		// デフォルト値
-		return 'table-scrollable-mobile';
+		// 何も該当しない場合は空文字列を返す
+		return '';
 	}
 
 	/**

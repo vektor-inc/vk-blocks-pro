4d247339da60d3e530401f2580f3dc4bd90b15a0
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-09-05 15:07:47 +0900

    Add data-icon-output- setting

--- test/phpunit/pro/test-scroll-hint-renderer.php
+++ test/phpunit/pro/test-scroll-hint-renderer.php
@@ -10,10 +10,23 @@ class VK_Blocks_ScrollHintRendererTest extends VK_UnitTestCase {
 	public static function setUpBeforeClass(): void {
 		parent::setUpBeforeClass();
 
-		// 必要な初期化を行う
+		// Initialize the ScrollHintRenderer class for testing
 		VK_Blocks_ScrollHintRenderer::init();
 	}
 
+	/**
+	 * Normalize HTML by removing whitespace and line breaks for comparison.
+	 *
+	 * @param string $html The HTML content to normalize.
+	 * @return string The normalized HTML content.
+	 */
+	private function normalize_html( $html ) {
+		return preg_replace( '/\s+/', '', $html );
+	}
+
+	/**
+	 * Test render_with_scroll_hint method with scrollable block.
+	 */
 	public function test_render_with_scroll_hint() {
 		$block_content = '<div class="is-style-vk-table-scrollable">Test Content</div>';
 		$block         = array(
@@ -24,23 +37,28 @@ class VK_Blocks_ScrollHintRendererTest extends VK_UnitTestCase {
 				'scrollIconLeft'      => '<i class="fa-solid fa-caret-left"></i>',
 				'scrollIconRight'     => '<i class="fa-solid fa-caret-right"></i>',
 				'scrollBreakpoint'    => 'table-scrollable-mobile',
+				'iconOutputLeft'      => true,
+				'iconOutputRight'     => true,
 			),
 		);
 
-		// テスト実行
+		// Execute the method
 		$actual_content = VK_Blocks_ScrollHintRenderer::render_with_scroll_hint( $block_content, $block );
 
-		// 期待されるスクロールヒントのHTML
-		$expected_scroll_hint = '<div class="vk-scroll-hint" data-scroll-breakpoint="table-scrollable-mobile" data-hint-icon-left="fa-solid fa-caret-left" data-hint-icon-right="fa-solid fa-caret-right">
+		// Expected scroll hint HTML
+		$expected_scroll_hint = '<div class="vk-scroll-hint" data-scroll-breakpoint="table-scrollable-mobile" data-icon-output-left="true" data-icon-output-right="true" data-hint-icon-left="fa-solid fa-caret-left" data-hint-icon-right="fa-solid fa-caret-right">
 				<i class="fa-solid fa-caret-left"></i>
 				<span>Scroll to see more</span>
 				<i class="fa-solid fa-caret-right"></i>
 			</div>';
 
-		// スクロールヒントが挿入されたか確認
-		$this->assertStringContainsString( $expected_scroll_hint, $actual_content );
+		// Normalize and compare
+		$this->assertStringContainsString( $this->normalize_html($expected_scroll_hint), $this->normalize_html($actual_content) );
 	}
 
+	/**
+	 * Test render_with_scroll_hint method with non-scrollable block.
+	 */
 	public function test_render_without_scroll_hint() {
 		$block_content = '<div class="some-other-class">Test Content</div>';
 		$block         = array(
@@ -50,13 +68,16 @@ class VK_Blocks_ScrollHintRendererTest extends VK_UnitTestCase {
 			),
 		);
 
-		// テスト実行
+		// Execute the method
 		$actual_content = VK_Blocks_ScrollHintRenderer::render_with_scroll_hint( $block_content, $block );
 
-		// スクロールヒントが挿入されていないことを確認
-		$this->assertSame( $block_content, $actual_content );
+		// Assert the scroll hint is not inserted
+		$this->assertSame( $this->normalize_html($block_content), $this->normalize_html($actual_content) );
 	}
 
+	/**
+	 * Test generate_scroll_hint method with both icons enabled.
+	 */
 	public function test_generate_scroll_hint() {
 		$block = array(
 			'attrs' => array(
@@ -64,19 +85,74 @@ class VK_Blocks_ScrollHintRendererTest extends VK_UnitTestCase {
 				'scrollIconLeft'    => '<i class="fa-solid fa-caret-left"></i>',
 				'scrollIconRight'   => '<i class="fa-solid fa-caret-right"></i>',
 				'scrollBreakpoint'  => 'table-scrollable-mobile',
+				'iconOutputLeft'    => true,
+				'iconOutputRight'   => true,
 			),
 		);
 
-		// 期待されるスクロールヒントのHTML
-		$expected_scroll_hint = '<div class="vk-scroll-hint" data-scroll-breakpoint="table-scrollable-mobile" data-hint-icon-left="fa-solid fa-caret-left" data-hint-icon-right="fa-solid fa-caret-right">
+		// Expected scroll hint HTML with both icons
+		$expected_scroll_hint = '<div class="vk-scroll-hint" data-scroll-breakpoint="table-scrollable-mobile" data-icon-output-left="true" data-icon-output-right="true" data-hint-icon-left="fa-solid fa-caret-left" data-hint-icon-right="fa-solid fa-caret-right">
 				<i class="fa-solid fa-caret-left"></i>
 				<span>Scroll to see more</span>
 				<i class="fa-solid fa-caret-right"></i>
 			</div>';
 
-		// メソッドのテスト
+		// Execute the method
 		$actual_scroll_hint = VK_Blocks_ScrollHintRenderer::generate_scroll_hint( $block );
 
-		$this->assertSame( $expected_scroll_hint, $actual_scroll_hint );
+		$this->assertEquals( $this->normalize_html($expected_scroll_hint), $this->normalize_html($actual_scroll_hint) );
+	}
+
+	/**
+	 * Test generate_scroll_hint method with one icon disabled.
+	 */
+	public function test_generate_scroll_hint_one_icon_disabled() {
+		$block = array(
+			'attrs' => array(
+				'scrollMessageText' => 'Scroll to see more',
+				'scrollIconLeft'    => '<i class="fa-solid fa-caret-left"></i>',
+				'scrollIconRight'   => '<i class="fa-solid fa-caret-right"></i>',
+				'scrollBreakpoint'  => 'table-scrollable-mobile',
+				'iconOutputLeft'    => false,
+				'iconOutputRight'   => true,
+			),
+		);
+
+		// Expected scroll hint HTML with only the right icon
+		$expected_scroll_hint = '<div class="vk-scroll-hint" data-scroll-breakpoint="table-scrollable-mobile" data-icon-output-left="false" data-icon-output-right="true" data-hint-icon-right="fa-solid fa-caret-right">
+				<span>Scroll to see more</span>
+				<i class="fa-solid fa-caret-right"></i>
+			</div>';
+
+		// Execute the method
+		$actual_scroll_hint = VK_Blocks_ScrollHintRenderer::generate_scroll_hint( $block );
+
+		$this->assertEquals( $this->normalize_html($expected_scroll_hint), $this->normalize_html($actual_scroll_hint) );
+	}
+
+	/**
+	 * Test generate_scroll_hint method with no icons enabled.
+	 */
+	public function test_generate_scroll_hint_no_icons() {
+		$block = array(
+			'attrs' => array(
+				'scrollMessageText' => 'Scroll to see more',
+				'scrollIconLeft'    => '<i class="fa-solid fa-caret-left"></i>',
+				'scrollIconRight'   => '<i class="fa-solid fa-caret-right"></i>',
+				'scrollBreakpoint'  => 'table-scrollable-mobile',
+				'iconOutputLeft'    => false,
+				'iconOutputRight'   => false,
+			),
+		);
+
+		// Expected scroll hint HTML with no icons
+		$expected_scroll_hint = '<div class="vk-scroll-hint" data-scroll-breakpoint="table-scrollable-mobile" data-icon-output-left="false" data-icon-output-right="false">
+				<span>Scroll to see more</span>
+			</div>';
+
+		// Execute the method
+		$actual_scroll_hint = VK_Blocks_ScrollHintRenderer::generate_scroll_hint( $block );
+
+		$this->assertEquals( $this->normalize_html($expected_scroll_hint), $this->normalize_html($actual_scroll_hint) );
 	}
 }

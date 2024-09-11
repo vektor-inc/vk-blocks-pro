98ed804be4c078d65d6dc52f0465185a6eb45ef9
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-09-11 12:28:00 +0900

    Add deprecated file

--- src/blocks/_pro/outer/deprecated/save/index.js
+++ src/blocks/_pro/outer/deprecated/save/index.js
@@ -20,6 +20,7 @@ import save1_61_2 from './1.61.2/save';
 import save1_64_0 from './1.64.0/save';
 import save1_71_0 from './1.71.0/save';
 import save1_76_0 from './1.76.0/save';
+import save1_83_0 from './1.83.0/save';
 
 const blockAttributes = {
 	bgColor: {
@@ -179,22 +180,38 @@ const blockAttributes7 = {
 };
 
 // 1.71.0 から attributes を変更
-/*
 const blockAttributes8 = {
 	...blockAttributes7,
 	linkUrl: {
 		type: 'string',
+		default: '',
 	},
 	linkTarget: {
 		type: 'string',
-		default: ''
+		default: '',
 	},
 };
-*/
+
+// 1.76.0 から attributes を変更
+const blockAttributes9 = {
+	...blockAttributes8,
+	relAttribute: {
+		type: 'string',
+		default: '',
+	},
+	linkDescription: {
+		type: 'string',
+		default: '',
+	},
+};
 
 const deprecated = [
 	{
-		attributes: blockAttributes7,
+		attributes: blockAttributes9,
+		save: save1_83_0,
+	},
+	{
+		attributes: blockAttributes8,
 		save: save1_76_0,
 	},
 	{

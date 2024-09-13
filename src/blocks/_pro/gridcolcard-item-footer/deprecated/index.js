0768c8a858ee72e469fcf5371078a0e8c7ab8fc1
mtdkei <mtdkei@gmail.com>
2024-09-13 10:42:34 +0900

    Delete blockAttributes2

--- src/blocks/_pro/gridcolcard-item-footer/deprecated/index.js
+++ src/blocks/_pro/gridcolcard-item-footer/deprecated/index.js
@@ -1,5 +1,4 @@
 import save1_83_0 from './1.83.0/save';
-import save1_84_0 from './1.84.0/save';
 
 const blockAttributes = {
 	footerDisplay: {
@@ -8,20 +7,7 @@ const blockAttributes = {
 	},
 };
 
-// 1.83.0 からの変更で追加したもの
-const blockAttributes2 = {
-	...blockAttributes,
-	footerDisplay: {
-		type: 'string',
-		default: 'display'
-	},
-};
-
 const deprecated = [
-	{
-		attributes: blockAttributes2,
-		save: save1_84_0,
-	},
 	{
 		attributes: blockAttributes,
 		save: save1_83_0,

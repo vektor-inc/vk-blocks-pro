a04d59e20e5b1be7b9db979c5dc941333b3831e2
mtdkei <mtdkei@gmail.com>
2024-10-03 10:47:05 +0900

    Fix: Exclude fixed position class for top and bottom positions

--- src/blocks/_pro/fixed-display/save.js
+++ src/blocks/_pro/fixed-display/save.js
@@ -14,12 +14,17 @@ export default function save({ attributes }) {
 	} = attributes;
 
 	const blockProps = useBlockProps.save({
-		className: `vk_fixed-display vk_fixed-display-mode-${mode} vk_fixed-display-position-${position} vk_fixed-display-position-from-${fixedPositionType} vk_fixed-display-${blockId}`,
+		className: `vk_fixed-display vk_fixed-display-mode-${mode} vk_fixed-display-position-${position} ${
+			['right', 'left'].includes(position)
+				? `vk_fixed-display-position-from-${fixedPositionType}`
+				: ''
+		} vk_fixed-display-${blockId}`,
 		style: {
 			[fixedPositionType]: ['right', 'left'].includes(position)
 				? `${fixedPositionValue}${fixedPositionUnit}`
 				: undefined,
 		},
+		// モードが 'show-on-scroll' の場合、データ属性を追加
 		...(mode === 'show-on-scroll' && {
 			'data-scroll-timing': scrollTiming.toString(),
 			'data-scroll-timing-unit': scrollTimingUnit,

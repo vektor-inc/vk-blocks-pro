f553939541c8922d4db6c139b8f7b112137f5c67
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-10-03 16:26:37 +0900

    Fix: change blockProps

--- src/blocks/_pro/gridcolcard-item/save.js
+++ src/blocks/_pro/gridcolcard-item/save.js
@@ -74,15 +74,14 @@ export default function save(props) {
 		textColor && isHexColor(textColor) ? textColor : null;
 
 	const blockProps = useBlockProps.save({
-		className: classnames(
-			containerClasses,
-			{
-				'vk_gridcolcard_item-noHeader': headerDisplay === 'delete',
-				'vk_gridcolcard_item-noFooter': footerDisplay === 'delete',
-				[`vk_gridcolcard_item-header-${headerDisplay}`]: headerDisplay !== 'delete',
-				[`vk_gridcolcard_item-footer-${footerDisplay}`]: footerDisplay !== 'delete',
-			}
-		),
+		className: classnames(containerClasses, {
+			'vk_gridcolcard_item-noHeader': headerDisplay === 'delete',
+			'vk_gridcolcard_item-noFooter': footerDisplay === 'delete',
+			[`vk_gridcolcard_item-header-${headerDisplay}`]:
+				headerDisplay !== 'delete',
+			[`vk_gridcolcard_item-footer-${footerDisplay}`]:
+				footerDisplay !== 'delete',
+		}),
 		style,
 	});
 

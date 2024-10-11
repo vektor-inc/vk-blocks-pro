83490fa24fb8da845a05aba98ebd4270e832ad98
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-10-11 13:03:45 +0900

    Fix: help message

--- src/blocks/visual-embed/edit.js
+++ src/blocks/visual-embed/edit.js
@@ -1,40 +1,45 @@
 import { __ } from '@wordpress/i18n';
 import { PanelBody, TextareaControl } from '@wordpress/components';
-import { InspectorControls, useBlockProps, BlockControls } from '@wordpress/block-editor';
+import {
+	InspectorControls,
+	useBlockProps,
+	BlockControls,
+} from '@wordpress/block-editor';
 
 export default function GoogleMapEdit({ attributes, setAttributes }) {
-    const { iframeCode } = attributes;
+	const { iframeCode } = attributes;
 
-    // ブロック全体に適用するプロパティを取得
-    const blockProps = useBlockProps({
-        className: 'vk-visual-embed',
-    });
+	const blockProps = useBlockProps({
+		className: 'vk-visual-embed',
+	});
 
-    return (
-        <div {...blockProps}>
-            <BlockControls>
-                {/* AlignmentToolbarは削除することで、テキストの揃えオプションを非表示に */}
-                {/* 必要であれば他のカスタムツールバーオプションを追加できます */}
-            </BlockControls>
-            <InspectorControls>
-                <PanelBody title={__('Google Map Embed Code', 'vk-blocks-pro')}>
-                    <TextareaControl
-                        label={__('Embed Code', 'vk-blocks-pro')}
-                        value={iframeCode}
-                        onChange={(newCode) => setAttributes({ iframeCode: newCode })}
-                        help={__('Googleマップの埋め込みコードをそのまま貼り付けてください。', 'vk-blocks-pro')}
-                    />
-                </PanelBody>
-            </InspectorControls>
-            <div style={{ position: 'relative' }}>
-                {iframeCode && (
-                    <div
-                        className="vk-visual-embed-preview"
-                        dangerouslySetInnerHTML={{ __html: iframeCode }}
-                        style={{ pointerEvents: 'none' }}
-                    />
-                )}
-            </div>
-        </div>
-    );
+	return (
+		<div {...blockProps}>
+			<BlockControls></BlockControls>
+			<InspectorControls>
+				<PanelBody title={__('Google Map Embed Code', 'vk-blocks-pro')}>
+					<TextareaControl
+						label={__('Embed Code', 'vk-blocks-pro')}
+						value={iframeCode}
+						onChange={(newCode) =>
+							setAttributes({ iframeCode: newCode })
+						}
+						help={__(
+							'Please paste the iframe embed code directly. (e.g., Google Maps)',
+							'vk-blocks-pro'
+						)}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div style={{ position: 'relative' }}>
+				{iframeCode && (
+					<div
+						className="vk-visual-embed-preview"
+						dangerouslySetInnerHTML={{ __html: iframeCode }}
+						style={{ pointerEvents: 'none' }}
+					/>
+				)}
+			</div>
+		</div>
+	);
 }

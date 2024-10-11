4797b19fdc641f0feb7b25c3364314f0cd043077
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-10-11 14:46:02 +0900

    Add atag setting

--- src/blocks/visual-embed/save.js
+++ src/blocks/visual-embed/save.js
@@ -5,10 +5,7 @@ export default function save({ attributes }) {
 	const blockProps = useBlockProps.save();
 
 	// iframeCode 内の <a> タグに rel="noopener" を追加
-	const updatedIframeCode = iframeCode.replace(
-		/<a /g,
-		'<a rel="noopener" '
-	);
+	const updatedIframeCode = iframeCode.replace(/<a /g, '<a rel="noopener" ');
 
 	return (
 		<div {...blockProps}>

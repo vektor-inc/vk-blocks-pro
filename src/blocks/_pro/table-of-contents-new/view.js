b229a2630d2f944147d3916ac88fb0edd9e28b5a
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-11-07 09:47:38 +0900

    lint

--- src/blocks/_pro/table-of-contents-new/view.js
+++ src/blocks/_pro/table-of-contents-new/view.js
@@ -6,31 +6,40 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 
 	// 開/閉 切り替え (:before 疑似要素のアクセシビリティ問題に対応 #2087)
-    document.querySelectorAll('#vk-tab-label').forEach((item) => {
-        const status = item.previousElementSibling; // チェックボックス
-        const tabContent = item.closest('.tab').querySelector('.tab_content-open, .tab_content-close');
-        const initialStateOpen = tabContent.classList.contains('tab_content-open');
+	document.querySelectorAll('#vk-tab-label').forEach((item) => {
+		const status = item.previousElementSibling; // チェックボックス
+		const tabContent = item
+			.closest('.tab')
+			.querySelector('.tab_content-open, .tab_content-close');
+		const initialStateOpen =
+			tabContent.classList.contains('tab_content-open');
 
-        // 初期状態に基づいてボタンのテキストとチェックボックスの状態を設定
-        if (initialStateOpen) {
-            item.textContent = 'CLOSE';
-            status.checked = true; // チェックボックスをチェック状態に
-            item.closest('.tab').classList.add('is-open'); // 開いた状態のクラスを追加
-        } else {
-            item.textContent = 'OPEN';
-            status.checked = false; // チェックボックスを非チェック状態に
-            item.closest('.tab').classList.add('is-close'); // 閉じた状態のクラスを追加
-        }
+		// 初期状態に基づいてボタンのテキストとチェックボックスの状態を設定
+		if (initialStateOpen) {
+			item.textContent = 'CLOSE';
+			status.checked = true; // チェックボックスをチェック状態に
+			item.closest('.tab').classList.add('is-open'); // 開いた状態のクラスを追加
+		} else {
+			item.textContent = 'OPEN';
+			status.checked = false; // チェックボックスを非チェック状態に
+			item.closest('.tab').classList.add('is-close'); // 閉じた状態のクラスを追加
+		}
 
-        // ボタンクリック時にクラスをトグル
-        item.addEventListener('click', function () {
-            if (status && status.type === 'checkbox') {
-                setTimeout(() => {
-                    item.textContent = status.checked ? 'CLOSE' : 'OPEN';
-                    item.closest('.tab').classList.toggle('is-open', status.checked);
-                    item.closest('.tab').classList.toggle('is-close', !status.checked);
-                }, 0);
-            }
-        });
-    });
+		// ボタンクリック時にクラスをトグル
+		item.addEventListener('click', function () {
+			if (status && status.type === 'checkbox') {
+				setTimeout(() => {
+					item.textContent = status.checked ? 'CLOSE' : 'OPEN';
+					item.closest('.tab').classList.toggle(
+						'is-open',
+						status.checked
+					);
+					item.closest('.tab').classList.toggle(
+						'is-close',
+						!status.checked
+					);
+				}, 0);
+			}
+		});
+	});
 });

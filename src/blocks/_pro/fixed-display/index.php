206ad46b9a7b4123f606c81b51b439de8f080d36
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-11-18 16:27:12 +0900

    Move to index.php in Fixed Display block

--- src/blocks/_pro/fixed-display/index.php
+++ src/blocks/_pro/fixed-display/index.php
@@ -6,6 +6,52 @@
  */
 
 /**
+ * VK_Blocks_Fixed_Display class
+ */
+if ( ! class_exists( 'VK_Blocks_Fixed_Display' ) ) {
+
+	class VK_Blocks_Fixed_Display {
+
+		/**
+		 * インスタンスを保持するためのプロパティ
+		 *
+		 * @var VK_Blocks_Fixed_Display|null
+		 */
+		private static $instance = null;
+
+		/**
+		 * シングルトンインスタンスを取得する初期化関数
+		 *
+		 * @return VK_Blocks_Fixed_Display インスタンス
+		 */
+		public static function init() {
+			if ( ! self::$instance ) {
+				self::$instance = new self();
+				add_filter( 'render_block', array( self::$instance, 'filter_block_opacity' ), 10, 2 );
+			}
+			return self::$instance;
+		}
+
+		/**
+		 * ブロックに初期 opacity: 0 を適用
+		 *
+		 * @param string $block_content ブロックのHTMLコンテンツ.
+		 * @param array  $block ブロックの情報.
+		 * @return string 変更されたHTMLコンテンツ.
+		 */
+		public function filter_block_opacity( $block_content, $block ) {
+			if ( isset( $block['attrs']['mode'] ) && in_array( $block['attrs']['mode'], array( 'display-hide-after-time', 'show-on-scroll' ), true ) ) {
+				// 指定されたモードの場合、初期 opacity: 0 を適用
+				$block_content = preg_replace( '/(<div\b[^>]*class="[^"]*vk_fixed-display-mode-(display-hide-after-time|show-on-scroll)[^"]*")/i', '$1 style="opacity:0;"', $block_content );
+			}
+			return $block_content;
+		}
+	}
+
+	VK_Blocks_Fixed_Display::init();
+}
+
+/**
  * Register Fixed display block.
  *
  * @return void
@@ -21,7 +67,7 @@ function vk_blocks_register_block_fixed_display() {
 		);
 	}
 
-	// Register Style.
+	// Register Script.
 	if ( ! is_admin() ) {
 		wp_register_script(
 			'vk-blocks/fixed-display-script',
@@ -33,10 +79,7 @@ function vk_blocks_register_block_fixed_display() {
 	}
 
 	// クラシックテーマ & 6.5 環境で $assets = array() のように空にしないと重複登録になるため
-	// ここで初期化しておく
 	$assets = array();
-	// Attend to load separate assets.
-	// 分割読み込みが有効な場合のみ、分割読み込み用のスクリプトを登録する
 	if ( method_exists( 'VK_Blocks_Block_Loader', 'should_load_separate_assets' ) && VK_Blocks_Block_Loader::should_load_separate_assets() ) {
 		$assets = array(
 			'style'         => 'vk-blocks/fixed-display',

1a24da647db6c7c5065c8a65d9bfe8dd41c4769f
Hidekazu Ishikawa <kurudrive@gmail.com>
2024-12-04 17:50:15 +0900

    Update src/blocks/_pro/post-category-badge/index.php

--- src/blocks/_pro/post-category-badge/index.php
+++ src/blocks/_pro/post-category-badge/index.php
@@ -92,7 +92,7 @@ function vk_blocks_post_category_badge_build_variations() {
 
 	// Create and register the eligible taxonomies variations.
 	foreach ( $taxonomies as $taxonomy ) {
-		if ( ! $taxonomy->hierarchical && 'post_tag' === $taxonomy->name ) {
+		if ( 'post_tag' === $taxonomy->name ) {
 			continue;
 		}
 

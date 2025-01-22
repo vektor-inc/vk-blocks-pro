a032014417f9b12638ece59281b4fba5ab7a2fd9
Taichi Maruyama <maruyama@breadfish.jp>
2025-01-22 19:22:26 +0900

    Revert "fix: XSS対応"
    This reverts commit 4093302155e98b6ec56111498b0f3a10f8b81dce.

--- src/blocks/visual-embed/edit.js
+++ src/blocks/visual-embed/edit.js
@@ -10,71 +10,13 @@ import {
 	TextControl,
 	Notice,
 } from '@wordpress/components';
-import { useState } from '@wordpress/element';
-
-// 許可するURLパターンの配列
-const ALLOWED_URL_PATTERNS = [
-	'https://*.google.com/*',
-	'https://*.youtube.com/embed/*',
-];
 
 export default function EmbedCodeEdit({ attributes, setAttributes }) {
 	const { iframeCode, iframeWidth, iframeHeight } = attributes;
-	const [tempIframeCode, setTempIframeCode] = useState(iframeCode);
-
-	// iframeを解析する関数
-	const parseIframeCode = (code) => {
-		const parser = new window.DOMParser();
-		const doc = parser.parseFromString(code, 'text/html');
-		const iframe = doc.querySelector('iframe');
-		return iframe ? iframe : false;
-	};
-	const [isIframe, setIsIframe] = useState(!!parseIframeCode(iframeCode));
 
 	const blockProps = useBlockProps({
 		className: 'vk-visual-embed',
 	});
-
-	// iframeのsrc属性を検証する関数
-	const isAllowedSrc = (src) => {
-		if (!src) {
-			return false;
-		}
-		return ALLOWED_URL_PATTERNS.some((pattern) => {
-			// ワイルドカードパターンを正規表現に変換
-			const regexPattern = pattern
-				.replace(/\./g, '\\.')
-				.replace(/\*/g, '.*');
-			const regex = new RegExp(`^${regexPattern}$`);
-			return regex.test(src);
-		});
-	};
-
-	// iframeタグ以外を削除する関数
-	const sanitizeIframeCode = (code) => {
-		if (!code) {
-			return '';
-		}
-
-		// DOMParserが利用できない環境の場合は入力をそのまま返す
-		if (typeof window.DOMParser === 'undefined') {
-			return code;
-		}
-
-		const iframe = parseIframeCode(code);
-
-		if (!iframe) {
-			return '';
-		}
-
-		// src属性を検証
-		const src = iframe.getAttribute('src');
-		if (!isAllowedSrc(src)) {
-			return __('Only allowed URLs can be embedded.', 'vk-blocks-pro');
-		}
-
-		return iframe.outerHTML;
-	};
 
 	// iframeの属性を解析して幅と高さを取得
 	const extractIframeAttributes = (code) => {
@@ -82,7 +24,9 @@ export default function EmbedCodeEdit({ attributes, setAttributes }) {
 			return false;
 		}
 
-		const iframe = parseIframeCode(code);
+		const parser = new window.DOMParser();
+		const doc = parser.parseFromString(code, 'text/html');
+		const iframe = doc.querySelector('iframe');
 
 		if (iframe) {
 			const newWidth = iframe.getAttribute('width') || iframeWidth;
@@ -106,7 +50,9 @@ export default function EmbedCodeEdit({ attributes, setAttributes }) {
 			return;
 		}
 
-		const iframe = parseIframeCode(iframeCode);
+		const parser = new window.DOMParser();
+		const doc = parser.parseFromString(iframeCode, 'text/html');
+		const iframe = doc.querySelector('iframe');
 
 		if (iframe) {
 			if (newWidth) {
@@ -125,6 +71,9 @@ export default function EmbedCodeEdit({ attributes, setAttributes }) {
 		}
 	};
 
+	// iframeタグが存在するかをチェック
+	const isIframe = iframeCode && extractIframeAttributes(iframeCode);
+
 	return (
 		<div {...blockProps}>
 			<BlockControls />
@@ -132,99 +81,30 @@ export default function EmbedCodeEdit({ attributes, setAttributes }) {
 				<PanelBody title={__('Embed Code Settings', 'vk-blocks-pro')}>
 					<TextareaControl
 						label={__('Embed Code', 'vk-blocks-pro')}
-						value={tempIframeCode}
+						value={iframeCode}
 						onChange={(newCode) => {
-							setTempIframeCode(newCode);
-						}}
-						onBlur={() => {
-							if (!tempIframeCode) {
-								setAttributes({ iframeCode: '' });
-								setIsIframe(false);
-								return;
-							}
-							const sanitizedCode =
-								sanitizeIframeCode(tempIframeCode);
-							setAttributes({ iframeCode: sanitizedCode });
-							if (sanitizedCode) {
-								extractIframeAttributes(sanitizedCode);
-							}
-
-							setIsIframe(!!parseIframeCode(sanitizedCode));
-							setTempIframeCode(sanitizedCode);
+							setAttributes({ iframeCode: newCode });
+							extractIframeAttributes(newCode);
 						}}
 						help={__(
-							'Please paste the iframe embed code directly. Only iframe tags with allowed URLs (Google Maps, YouTube) are permitted.',
+							'Please paste the iframe embed code directly. (e.g., Google Maps)',
 							'vk-blocks-pro'
 						)}
 					/>
-					{!iframeCode && (
-						<Notice
-							status="error"
-							isDismissible={false}
-							className="vk-visual-embed_notice"
-						>
-							{__(
-								'Please enter an iframe embed code.',
-								'vk-blocks-pro'
-							)}
-						</Notice>
-					)}
-					{iframeCode && !sanitizeIframeCode(iframeCode) && (
-						<Notice
-							status="error"
-							isDismissible={false}
-							className="vk-visual-embed_notice"
-						>
-							{__(
-								'The provided URL is not allowed. Please use an approved embed source.',
-								'vk-blocks-pro'
-							)}
-						</Notice>
-					)}
 					<TextControl
 						label={__('Iframe Width', 'vk-blocks-pro')}
 						value={iframeWidth}
-						onChange={(newWidth) => {
-							setAttributes({ iframeWidth: newWidth });
-						}}
-						onBlur={() => {
-							if (!iframeWidth) {
-								extractIframeAttributes(iframeCode);
-								return;
-							}
-							if (/^\d+(px|%)?$/.test(iframeWidth)) {
-								updateIframeAttributes(
-									iframeWidth,
-									iframeHeight
-								);
-							} else {
-								setAttributes({ iframeWidth: '' });
-								updateIframeAttributes('', iframeHeight);
-							}
-						}}
+						onChange={(newWidth) =>
+							updateIframeAttributes(newWidth, iframeHeight)
+						}
 						disabled={!isIframe}
 					/>
 					<TextControl
 						label={__('Iframe Height', 'vk-blocks-pro')}
 						value={iframeHeight}
-						onChange={(newHeight) => {
-							setAttributes({ iframeHeight: newHeight });
-						}}
-						onBlur={() => {
-							if (!iframeHeight) {
-								extractIframeAttributes(iframeCode);
-								return;
-							}
-							if (/^\d+(px|%)?$/.test(iframeHeight)) {
-								updateIframeAttributes(
-									iframeWidth,
-									iframeHeight
-								);
-							} else {
-								setAttributes({ iframeHeight: '' });
-								updateIframeAttributes(iframeWidth, '');
-							}
-						}}
+						onChange={(newHeight) =>
+							updateIframeAttributes(iframeWidth, newHeight)
+						}
 						disabled={!isIframe}
 					/>
 					{!isIframe && (

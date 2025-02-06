86f91a3014c13dfc01eedd06d1b811816382eb91
mtdkei <mt@matsudakeinoMacBook-Air.local>
2025-02-06 16:26:16 +0900

    Fix: Apply WordPress Coding Standards (WPCS) to post-list-slider

--- src/blocks/_pro/post-list-slider/index.php
+++ src/blocks/_pro/post-list-slider/index.php
@@ -285,9 +285,9 @@ function vk_blocks_post_list_slider_set_data() {
 add_action( 'enqueue_block_editor_assets', 'vk_blocks_post_list_slider_set_data' );
 
 /**
- * Enqueue editor styles
+ * Register editor styles for the Post List Slider block
  */
-function vk_blocks_enqueue_editor_iframe_post_list_slider() {
+function vk_blocks_register_block_post_list_slider_editor_styles() {
 	// `vk-blocks-pro` のルートディレクトリを取得
 	$plugin_root_path = WP_PLUGIN_DIR . '/vk-blocks-pro';
 	$plugin_root_url  = WP_PLUGIN_URL . '/vk-blocks-pro';
@@ -306,4 +306,4 @@ function vk_blocks_enqueue_editor_iframe_post_list_slider() {
 		);
 	}
 }
-add_action( 'enqueue_block_assets', 'vk_blocks_enqueue_editor_iframe_post_list_slider' );
+add_action( 'enqueue_block_editor_assets', 'vk_blocks_register_block_post_list_slider_editor_styles' );

7b9cadd52c91441b7ae43f6badabe13b59c24f76
mtdkei <mt@matsudakeinoMacBook-Air.local>
2025-02-07 12:49:21 +0900

    Fix: vk_blocks_should_load_vk_components

--- inc/vk-blocks/load-vk-components.php
+++ inc/vk-blocks/load-vk-components.php
@@ -15,17 +15,20 @@ use VektorInc\VK_Component\VK_Component_Posts;
 function vk_blocks_should_load_vk_components() {
 	$template = wp_get_theme()->Template;
 	// lightning 系（ベクトル）の場合 vk-components はテーマなどで読み込むので適用しない
-	return ! in_array( $template, array( 'lightning', 'lightning-pro', 'katawara' ), true );
+	$should_load = ! in_array( $template, array( 'lightning', 'lightning-pro', 'katawara' ), true );
+
+	return apply_filters( 'vk_blocks_should_load_vk_components', $should_load );
+}
+
+// 条件に合わなければここで return することで、以降の処理を実行しない
+if ( ! vk_blocks_should_load_vk_components() ) {
+	return;
 }
 
 /**
  * VK Components のスタイルを登録 & 読み込み
  */
 function vk_blocks_register_vk_components_style() {
-	if ( ! vk_blocks_should_load_vk_components() ) {
-		return;
-	}
-
 	// VK Components のスタイルを登録
 	VK_Component_Posts::register_style( 'vk-components-style' );
 }
@@ -37,16 +40,10 @@ add_action( 'init', 'vk_blocks_register_vk_components_style' );
  * @param string $hook_suffix hook suffix.
  */
 function vk_blocks_enqueue_vk_components_style( $hook_suffix ) {
-	if ( ! vk_blocks_should_load_vk_components() ) {
-		return;
-	}
-
 	// 管理画面
-	if ( is_admin() ) {
-		if ( 'post.php' === $hook_suffix || 'post-new.php' === $hook_suffix ) {
-			wp_enqueue_style( 'vk-components-style' );
-		}
-	} else {
+	if ( is_admin() && in_array( $hook_suffix, array( 'post.php', 'post-new.php' ), true ) ) {
+		wp_enqueue_style( 'vk-components-style' );
+	} elseif ( ! is_admin() ) {
 		wp_enqueue_style( 'vk-components-style' );
 	}
 }
@@ -57,12 +54,6 @@ add_action( 'wp_enqueue_scripts', 'vk_blocks_enqueue_vk_components_style' );
  * エディター用の VK Components スタイルを適用
  */
 function vk_blocks_enqueue_vk_components_editor_style() {
-	$template = wp_get_theme()->Template;
-	// lightning 系（ベクトル）の場合 vk-components はテーマなどで読み込むので必要ない
-	if ( 'lightning' === $template || 'lightning-pro' === $template || 'katawara' === $template ) {
-		return;
-	}
-
 	// スタイルが登録されていない場合は登録
 	if ( ! wp_style_is( 'vk-components-style', 'registered' ) ) {
 		VK_Component_Posts::register_style( 'vk-components-style' );

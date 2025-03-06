c3868f2d33256ed05e3948588194abd8cca1b86e
mtdkei <mt@matsudakeinoMacBook-Air.local>
2025-03-06 10:58:59 +0900

    Add ensureImageId

--- src/components/advanced-media-upload/index.js
+++ src/components/advanced-media-upload/index.js
@@ -2,29 +2,54 @@ import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 import { MediaUpload } from '@wordpress/block-editor';
 import { dispatch } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
 import noImage from '../../../inc/vk-blocks/images/no-image.svg';
 
 export const AdvancedMediaUpload = (props) => {
-	const { schema, schemaId, clientId, setAttributes, attributes } = props;
+	const { schema, clientId, setAttributes, attributes } = props;
 
 	const deleteImgBtn = () => {
 		dispatch('core/block-editor').updateBlockAttributes(clientId, {
 			[schema]: null,
-			[schemaId]: null,
+			[schema + 'Id']: null,
 		});
 	};
+
+	const ensureImageId = (callback) => {
+		const schemaIdKey = schema + 'Id';
+	
+		if (attributes[schema] && !attributes[schemaIdKey]) {
+			wp.media.attachment(attributes[schema]).fetch().then((media) => {
+				if (media && media.id) {
+					setAttributes({ [schemaIdKey]: media.id });
+				}
+			});
+		}
+	
+		// すぐにコールバック（open）を実行
+		if (callback) callback();
+	};	
 
 	return (
 		<MediaUpload
 			onSelect={(value) => {
+				const schemaIdKey = schema + 'Id'; // 例: "bgImageId"
 				const newAttributes = { [schema]: value.url };
-				if (!attributes[schemaId]) {
-					newAttributes[schemaId] = value.id;
+			
+				if (value.id) {
+					newAttributes[schemaIdKey] = value.id;
+				} else {
+					// IDがない場合、wp.media.attachment() を使って取得
+					wp.media.attachment(value.url).fetch().then((media) => {
+						if (media && media.id) {
+							setAttributes({ [schemaIdKey]: media.id });
+						}
+					});
 				}
 				setAttributes(newAttributes);
 			}}
 			type="image"
-			value={attributes[schemaId]}
+			value={attributes[schema + 'Id']}
 			render={({ open }) => (
 				<>
 					{attributes[schema] ? (
@@ -34,20 +59,18 @@ export const AdvancedMediaUpload = (props) => {
 								className={'icon-image'}
 								src={attributes[schema]}
 							/>
-							<div className="components-button-group">
+							<div class="components-button-group">
 								<Button
 									onClick={deleteImgBtn}
-									className={
-										'image-button button button-delete'
-									}
+									className={'image-button button button-delete'}
 								>
 									{__('Delete Image', 'vk-blocks-pro')}
 								</Button>
 								<Button
-									onClick={open}
-									className={
-										'image-button button button-replace'
-									}
+									onClick={() => {
+										ensureImageId(open);
+									}}
+									className={'image-button button button-replace'}
 								>
 									{__('Replace Image', 'vk-blocks-pro')}
 								</Button>

01c5a92360718a5ea37793e4e2655a6e8f2d53cb
drill-lancer <rickaddison7634@gmail.com>
2025-04-03 15:06:45 +0900

    fix: delete unused hooks

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,52 +1,9 @@
 import { useSelect } from '@wordpress/data';
 
-export const usePostTypes = () => {
-	return useSelect((select) => {
-		return select('core').getPostTypes({ per_page: -1 }) || [];
-	}, []);
-};
-
-export const usePostType = (postType) => {
-	return useSelect(
-		(select) => {
-			return select('core').getPostType(postType) || {};
-		},
-		[postType]
-	);
-};
-
 export const useTaxonomies = () => {
 	return useSelect((select) => {
 		return select('core').getTaxonomies({ per_page: -1 }) || [];
 	}, []);
-};
-
-export const usePostTypeTaxonomies = (postType) => {
-	return useSelect(
-		(select) => {
-			return (select('core').getTaxonomies() || []).filter((taxonomy) => {
-				const postTypeTaxonomies = postType.taxonomies || [];
-				return postTypeTaxonomies.includes(taxonomy.slug);
-			});
-		},
-		[postType]
-	);
-};
-
-export const useTermsGroupbyTaxnomy = (taxonomies) => {
-	return useSelect(
-		(select) => {
-			const obj = {};
-			for (const taxonomy of taxonomies) {
-				obj[taxonomy.rest_base] =
-					select('core').getEntityRecords('taxonomy', taxonomy.slug, {
-						per_page: -1,
-					}) || [];
-			}
-			return obj;
-		},
-		[taxonomies]
-	);
 };
 
 export const usePosts = (postType, query) => {
@@ -62,18 +19,6 @@ export const usePosts = (postType, query) => {
 		},
 		[postType, query]
 	);
-};
-
-export const useCurrentPostId = () => {
-	return useSelect((select) => {
-		return select('core/editor').getCurrentPostId();
-	});
-};
-
-export const useCurrentPostType = () => {
-	return useSelect((select) => {
-		return select('core/editor').getCurrentPostType();
-	});
 };
 
 export const useCurrentBlocks = () => {

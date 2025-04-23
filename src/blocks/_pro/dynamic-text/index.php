c80ac14bc4dcb345319e9dd23e6e129b73d437da
mtdkei <mt@matsudakeinoMacBook-Air.local>
2025-04-23 15:58:36 +0900

    Add textColor

--- src/blocks/_pro/dynamic-text/index.php
+++ src/blocks/_pro/dynamic-text/index.php
@@ -30,6 +30,7 @@ function vk_blocks_dynamic_text_get_attributes_default() {
 		'isButtonStyle'            => false,
 		'buttonEffect'             => '',
 		'buttonColor'              => 'primary',
+		'buttonColorCustom'        => null,
 		'buttonType'               => '0',
 		'buttonSize'               => 'md',
 		'buttonAlign'              => 'left',
@@ -125,9 +126,7 @@ function vk_blocks_dynamic_text_custom_field_render( $attributes, $content, $blo
 						if ( preg_match( '/^[a-z0-9-]+$/', $attributes['buttonTextColorCustom'] ) ) {
 							$button_classes .= ' has-' . $attributes['buttonTextColorCustom'] . '-color';
 						}
-
-					$button_classes .= ' has-' . $attributes['buttonTextColorCustom'] . '-color';
-
+						$button_classes .= ' has-' . $attributes['buttonTextColorCustom'] . '-color';
 					}
 				} elseif ( '1' === $button_type ) {
 					// 塗りなし

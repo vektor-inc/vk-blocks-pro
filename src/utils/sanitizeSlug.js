a5fc1834cc1555d81cbd2b50f997306004bd37cd
mtdkei <mtdkei@gmail.com>
2025-04-26 09:31:58 +0900

    Fix: sanitizeSlug

--- src/utils/sanitizeSlug.js
+++ src/utils/sanitizeSlug.js
@@ -1,6 +1,5 @@
-// Sanitize function to normalize slugs
-export const sanitizeSlug = (slug) =>
-	slug
-		?.replace(/[^a-z0-9\-]/gi, '-') // Remove unwanted characters
-		.replace(/-{2,}/g, '-') // Replace multiple hyphens with a single one
-		.replace(/^-+|-+$/g, ''); // Remove leading and trailing hyphens
+import { kebabCase } from 'lodash';
+
+export const sanitizeSlug = (slug) => {
+	return kebabCase(slug);
+};

c942ec5dd6d4811c7e0d30edf79fa6e55489a267
mtdkei <mt@matsudakeinoMacBook-Air.local>
2025-05-01 13:01:44 +0900

    Fix: Add schema1_102_0

--- src/blocks/pr-blocks/deprecated/index.js
+++ src/blocks/pr-blocks/deprecated/index.js
@@ -3,6 +3,7 @@ import schema1_0_7 from './1.0.7/schema';
 import save1_20_2 from './1.20.2/save';
 import schema1_20_2 from './1.20.2/schema';
 import save1_102_0 from './1.102.0/save';
+import schema1_102_0 from './1.102.0/schema';
 
 export const deprecated = [
 	{

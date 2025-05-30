ffc08e7399bbe8d10080c6a83ff5378631d71da5
mtdkei <mtdkei@gmail.com>
2025-05-30 16:17:09 +0900

    Undo commentout

--- src/blocks/_pro/table-of-contents-new/view.js
+++ src/blocks/_pro/table-of-contents-new/view.js
@@ -97,18 +97,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// OBJ文字の除去
 	document.querySelectorAll('.vk_tableOfContents_list li').forEach((item) => {
-		item.innerHTML = item.innerHTML.replace(/\uFFFC/g, '');
+		item.innerHTML = item.innerHTML.replace(/\uFFFC/g, ''); // U+FFFCはOBJのUnicodeです
 	});
 
-	// 開/閉 切り替え
+	// 開/閉 切り替え (:before 疑似要素のアクセシビリティ問題に対応 #2087)
 	document.querySelectorAll('#vk-tab-label').forEach((item) => {
-		const status = item.previousElementSibling;
+		const status = item.previousElementSibling; // チェックボックス
 		const tabContent = item
 			.closest('.tab')
 			.querySelector('.tab_content-open, .tab_content-close');
 		const initialStateOpen =
 			tabContent.classList.contains('tab_content-open');
 
+		// 初期状態に基づいてボタンのテキストとチェックボックスの状態を設定
 		if (initialStateOpen) {
 			item.textContent = 'CLOSE';
 			status.checked = true;
@@ -117,6 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			status.checked = false;
 		}
 
+		// ボタンクリック時にテキストをトグル
 		item.addEventListener('click', function () {
 			if (status && status.type === 'checkbox') {
 				setTimeout(() => {

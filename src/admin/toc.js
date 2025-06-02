672668604fa1d3c826b1d5b794c4568d93a9e718
mtdkei <mtdkei@gmail.com>
2025-06-02 12:28:43 +0900

    Add DEFAULT_TOC_LEVELS

--- src/admin/toc.js
+++ src/admin/toc.js
@@ -3,6 +3,9 @@ import { useContext } from '@wordpress/element';
 import { AdminContext } from './index';
 import { SelectControl } from '@wordpress/components';
 
+// デフォルト値を定数として定義
+const DEFAULT_TOC_LEVELS = ['h2', 'h3', 'h4', 'h5', 'h6'];
+
 export default function AdminToc() {
 	const { vkBlocksOption, setVkBlocksOption } = useContext(AdminContext);
 
@@ -10,7 +13,7 @@ export default function AdminToc() {
 		const levels = ['h2'];
 		const levelNumbers = ['h3', 'h4', 'h5', 'h6'];
 		const maxIndex = levelNumbers.indexOf(maxLevel);
-		
+
 		if (maxIndex !== -1) {
 			levels.push(...levelNumbers.slice(0, maxIndex + 1));
 		}
@@ -23,19 +26,23 @@ export default function AdminToc() {
 
 	// 現在の最大レベルを取得
 	const getCurrentMaxLevel = () => {
-		const currentLevels = vkBlocksOption.tocHeadingLevels || ['h2', 'h3', 'h4', 'h5', 'h6'];
+		const currentLevels =
+			vkBlocksOption.tocHeadingLevels || DEFAULT_TOC_LEVELS;
 		const maxLevel = currentLevels[currentLevels.length - 1];
 		return maxLevel || 'h2';
 	};
 
 	return (
 		<section className="vk_admin_page_section" id="toc-setting">
-			<h3>{ __('Table of Contents Setting', 'vk-blocks-pro') }</h3>
+			<h3>{__('Table of Contents Setting', 'vk-blocks-pro')}</h3>
 			<p>
-				{ __('Configure the maximum heading level to include in the table of contents.', 'vk-blocks-pro') }
+				{__(
+					'Configure the maximum heading level to include in the table of contents.',
+					'vk-blocks-pro'
+				)}
 			</p>
 			<SelectControl
-				name="vk_blocks_options[toc_heading_levels]"
+				name="vk_blocks_options[tocHeadingLevels]"
 				className="vk_admin_selectControl"
 				value={getCurrentMaxLevel()}
 				options={[
@@ -43,13 +50,16 @@ export default function AdminToc() {
 					{ label: 'H3', value: 'h3' },
 					{ label: 'H4', value: 'h4' },
 					{ label: 'H5', value: 'h5' },
-					{ label: 'H6', value: 'h6' }
+					{ label: 'H6', value: 'h6' },
 				]}
 				onChange={(value) => handleMaxLevelChange(value)}
 			/>
 			<p className="description">
-				{ __('Headings from H2 up to the selected level will be included.', 'vk-blocks-pro') }
+				{__(
+					'Headings from H2 up to the selected level will be included.',
+					'vk-blocks-pro'
+				)}
 			</p>
 		</section>
 	);
-} 
+}

657f423e363dca661f3ce73162b6774d59333178
mtdkei <mtdkei@gmail.com>
2025-06-11 14:30:43 +0900

    fix: phpcs

--- inc/vk-blocks-pro/blocks/class-vk-blocks-toc.php
+++ inc/vk-blocks-pro/blocks/class-vk-blocks-toc.php
@@ -148,7 +148,10 @@ class VK_Blocks_TOC {
 	}
 
 	/**
-	 * the_content内のh2〜h6を抽出する共通メソッド
+	 * The_content内のh2〜h6を抽出する共通メソッド
+	 *
+	 * @param string $content 投稿本文
+	 * @return array 見出し情報の配列
 	 */
 	public static function get_headings_from_content( $content ) {
 		preg_match_all( '/<h([2-6])(.*?)>(.*?)<\\/h\\1>/is', $content, $matches, PREG_SET_ORDER );

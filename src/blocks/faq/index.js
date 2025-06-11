b420c522e48641c38ade1d3d113773b96197fbd1
mtdkei <mtdkei@gmail.com>
2025-06-11 15:03:46 +0900

    fix: lint

--- src/blocks/faq/index.js
+++ src/blocks/faq/index.js
@@ -74,10 +74,25 @@ export const settings = {
 				type: 'block',
 				blocks: ['vk-blocks/faq2'],
 				transform: (attributes, innerBlocks) => {
-					const questionInner = [ createBlock('core/paragraph', { content: attributes.heading }) ];
-					const questionBlock = createBlock('vk-blocks/faq2-q', {}, questionInner);
-					const answerBlock = createBlock('vk-blocks/faq2-a', {}, innerBlocks);
-					return createBlock('vk-blocks/faq2', {}, [questionBlock, answerBlock]);
+					const questionInner = [
+						createBlock('core/paragraph', {
+							content: attributes.heading,
+						}),
+					];
+					const questionBlock = createBlock(
+						'vk-blocks/faq2-q',
+						{},
+						questionInner
+					);
+					const answerBlock = createBlock(
+						'vk-blocks/faq2-a',
+						{},
+						innerBlocks
+					);
+					return createBlock('vk-blocks/faq2', {}, [
+						questionBlock,
+						answerBlock,
+					]);
 				},
 			},
 		],

fa0aad3763e694adee93add6477b9176aeffca49
mtdkei <mtdkei@gmail.com>
2025-06-12 17:26:08 +0900

    Fix: lint

--- src/extensions/core/heading/style.js
+++ src/extensions/core/heading/style.js
@@ -5,42 +5,50 @@ import { addExcludeFromTocAttribute } from '@vkblocks/utils/is-exclude-from-toc'
 
 // 目次設定属性追加
 addFilter(
-  'blocks.registerBlockType',
-  'vk-blocks-pro/core-heading-exclude-from-toc-attr',
-  (settings, name) => name === 'core/heading' ? addExcludeFromTocAttribute(settings) : settings
+	'blocks.registerBlockType',
+	'vk-blocks-pro/core-heading-exclude-from-toc-attr',
+	(settings, name) =>
+		name === 'core/heading'
+			? addExcludeFromTocAttribute(settings)
+			: settings
 );
 
 // 目次設定UI追加
-const withInspectorControls = createHigherOrderComponent((BlockEdit) => (props) => {
-  if (props.name !== 'core/heading') return <BlockEdit {...props} />;
-  return (
-    <>
-      <BlockEdit {...props} />
-      <ExcludeFromTocToggle attributes={props.attributes} setAttributes={props.setAttributes} />
-    </>
-  );
-}, 'withInspectorControls');
+const withInspectorControls = createHigherOrderComponent(
+	(BlockEdit) => (props) => {
+		if (props.name !== 'core/heading') {
+			return <BlockEdit {...props} />;
+		}
+		return (
+			<>
+				<BlockEdit {...props} />
+				<ExcludeFromTocToggle
+					attributes={props.attributes}
+					setAttributes={props.setAttributes}
+				/>
+			</>
+		);
+	},
+	'withInspectorControls'
+);
 
 addFilter(
-  'editor.BlockEdit',
-  'vk-blocks-pro/core-heading-exclude-from-toc-ui',
-  withInspectorControls
+	'editor.BlockEdit',
+	'vk-blocks-pro/core-heading-exclude-from-toc-ui',
+	withInspectorControls
 );
 
 // save時にdata-vk-toc-excludeを付与
 addFilter(
-  'blocks.getSaveContent.extraProps',
-  'vk-blocks-pro/core-heading-exclude-from-toc-save',
-  (extraProps, blockType, attributes) => {
-    if (
-      blockType.name === 'core/heading' &&
-      attributes.excludeFromToc
-    ) {
-      return {
-        ...extraProps,
-        'data-vk-toc-exclude': 'true',
-      };
-    }
-    return extraProps;
-  }
+	'blocks.getSaveContent.extraProps',
+	'vk-blocks-pro/core-heading-exclude-from-toc-save',
+	(extraProps, blockType, attributes) => {
+		if (blockType.name === 'core/heading' && attributes.excludeFromToc) {
+			return {
+				...extraProps,
+				'data-vk-toc-exclude': 'true',
+			};
+		}
+		return extraProps;
+	}
 );

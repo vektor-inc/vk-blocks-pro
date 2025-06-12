1f7421b3e873af66ceb4a7daf7e2ced31624dff4
mtdkei <mtdkei@gmail.com>
2025-06-12 14:52:19 +0900

    fix: delete commentout

--- src/utils/is-exclude-from-toc.js
+++ src/utils/is-exclude-from-toc.js
@@ -2,7 +2,6 @@ export const isExcludeFromToc = (attributes = {}) => {
 	return !!attributes.excludeFromToc;
 };
 
-// 属性追加用
 export const addExcludeFromTocAttribute = (settings) => {
 	return {
 		...settings,

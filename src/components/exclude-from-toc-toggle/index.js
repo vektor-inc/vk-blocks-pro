a3ba8c87276e42f8c96ea04b93838edbec9a7f0e
mtdkei <mtdkei@gmail.com>
2025-06-12 14:51:42 +0900

    Add useSelect

--- src/components/exclude-from-toc-toggle/index.js
+++ src/components/exclude-from-toc-toggle/index.js
@@ -1,15 +1,34 @@
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, ToggleControl } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
 
-export const ExcludeFromTocToggle = ({ attributes, setAttributes }) => (
-	<InspectorControls>
-		<PanelBody title={__('目次設定', 'vk-blocks-pro')}>
-			<ToggleControl
-				label={__('この見出しを目次に含めない', 'vk-blocks-pro')}
-				checked={attributes.excludeFromToc}
-				onChange={(value) => setAttributes({ excludeFromToc: value })}
-			/>
-		</PanelBody>
-	</InspectorControls>
-); 
+export const ExcludeFromTocToggle = ({ attributes, setAttributes }) => {
+	const hasTocBlock = useSelect((select) => {
+		const blocks = select('core/block-editor').getBlocks();
+		return blocks.some(
+			(block) => block.name === 'vk-blocks/table-of-contents-new'
+		);
+	}, []);
+
+	// h1の場合は表示しない
+	if (!hasTocBlock || attributes.level === 1) {
+		return null;
+	}
+
+	return (
+		<InspectorControls>
+			<PanelBody title={__('Table of Contents Settings', 'vk-blocks-pro')}>
+				<ToggleControl
+					label={__('Exclude this heading from Table of Contents', 'vk-blocks-pro')}
+					checked={attributes.excludeFromToc}
+					onChange={(value) => setAttributes({ excludeFromToc: value })}
+					help={__(
+						'This setting only affects the VK Table of Contents block.',
+						'vk-blocks-pro'
+					)}
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
+}; 

49780746186c061644734fb0ff84489431fea352
mtdkei <mtdkei@gmail.com>
2025-06-25 17:20:11 +0900

    add: rtrim

--- inc/vk-blocks/utils/color-slug-to-color-code.php
+++ inc/vk-blocks/utils/color-slug-to-color-code.php
@@ -18,8 +18,10 @@ function vk_blocks_get_color_code( $value ) {
 	if ( preg_match( '/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/', $value ) ) {
 		$return = $value;
 	} else {
-		// -- を - に正規化
+		// ハイフンが2つ続いている場合は1つにする(WordPressのコアではそうなっているため)
 		$slug = str_replace('--', '-', $value);
+		// 末尾のハイフンを削除
+		$slug = rtrim( $slug, '-' );
 		$return = 'var(--wp--preset--color--' . $slug . ')';
 	}
 	return $return;

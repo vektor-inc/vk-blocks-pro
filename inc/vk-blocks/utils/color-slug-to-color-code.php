31c7ab4554d2058c4fff0f013b1fc7cf6e3af51c
mtdkei <mtdkei@gmail.com>
2025-06-26 19:10:16 +0900

    fix: phpcs

--- inc/vk-blocks/utils/color-slug-to-color-code.php
+++ inc/vk-blocks/utils/color-slug-to-color-code.php
@@ -18,43 +18,34 @@ function vk_blocks_get_color_code( $value ) {
 	// 16進数の色コード
 	if ( preg_match( '/^#([a-fA-F0-9]{3}|[a-fA-F0-9]{6}|[a-fA-F0-9]{8})$/', $value ) ) {
 		$return = $value;
-	}
-	// rgb()やrgba()の色コード（スペース対応）
-	elseif ( preg_match( '/^rgba?\s*\(/', $value ) ) {
+	} elseif ( preg_match( '/^rgba?\s*\(/', $value ) ) {
 		// rgba()形式を#形式に変換
 		if ( preg_match( '/^rgba?\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(?:,\s*([\d.]+)\s*)?\)$/', $value, $matches ) ) {
 			$r = intval( $matches[1] );
 			$g = intval( $matches[2] );
 			$b = intval( $matches[3] );
 			$a = isset( $matches[4] ) ? floatval( $matches[4] ) : 1.0;
-
 			// RGBを16進数に変換
 			$hex = sprintf( '#%02x%02x%02x', $r, $g, $b );
-
 			// アルファ値がある場合は8桁の16進数に変換
 			if ( $a < 1.0 ) {
 				$alpha_hex = sprintf( '%02x', round( $a * 255 ) );
 				$hex      .= $alpha_hex;
 			}
-
 			$return = $hex;
 		} else {
 			$return = $value;
 		}
-	}
-	// ハイフンを含む場合はWordPressのカラーパレットのスラッグとして処理
-	elseif ( strpos( $value, '-' ) !== false ) {
-		// ハイフンが2つ続いている場合は1つにする(WordPressのコアではそうなっているため)
+	} elseif ( strpos( $value, '-' ) !== false ) {
+		// WordPressコアの命名規則に合わせてハイフンを正規化
 		$slug = str_replace( '--', '-', $value );
 		// 末尾のハイフンを削除
 		$slug   = rtrim( $slug, '-' );
 		$return = 'var(--wp--preset--color--' . $slug . ')';
-	}
-	// 色名（red, blueなど）- ハイフンを含まない場合のみ
-	elseif ( preg_match( '/^[a-zA-Z]+$/', $value ) ) {
+	} elseif ( preg_match( '/^[a-zA-Z]+$/', $value ) ) {
 		$return = $value;
 	} else {
-		// ハイフンが2つ続いている場合は1つにする(WordPressのコアではそうなっているため)
+		// WordPressコアの命名規則に合わせてハイフンを正規化
 		$slug = str_replace( '--', '-', $value );
 		// 末尾のハイフンを削除
 		$slug   = rtrim( $slug, '-' );

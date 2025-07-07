d753ad7d0d4aeada9bad89d71446fec5dbd6a816
mtdkei <mtdkei@gmail.com>
2025-07-07 15:24:21 +0900

    fix: lint

--- src/blocks/_pro/table-of-contents-new/save.js
+++ src/blocks/_pro/table-of-contents-new/save.js
@@ -21,19 +21,21 @@ export default function save(props) {
 		// ビット演算子を使わないハッシュ関数（ESLintのno-bitwiseルール対応）
 		const hashCode = (str) => {
 			let hash = 0;
-			if (str.length === 0) return hash;
+			if (str.length === 0) {
+				return hash;
+			}
 			for (let i = 0; i < str.length; i++) {
 				const char = str.charCodeAt(i);
-				hash = ((hash * 31) + char) % 2147483647;
+				hash = (hash * 31 + char) % 2147483647;
 			}
 			return Math.abs(hash).toString(36);
 		};
-		
+
 		// renderHtmlとその他の属性から決定的なIDを生成
 		const hashInput = `${renderHtml || ''}-${style}-${open}-${JSON.stringify(customHeadingLevels)}-${JSON.stringify(excludedHeadings)}`;
 		uniqueId = `legacy-${hashCode(hashInput)}`;
 	}
-	
+
 	const checkboxId = `chck-toc-${uniqueId}`;
 	const labelId = `vk-tab-label-toc-${uniqueId}`;
 

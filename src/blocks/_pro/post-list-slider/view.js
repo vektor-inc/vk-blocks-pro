ea0a80ed7ea9d340c409ec48cf7c242c498e58aa
mtdkei <mtdkei@gmail.com>
2025-07-08 12:14:12 +0900

    fix: Removed eval() to fix XSS vulnerability

--- src/blocks/_pro/post-list-slider/view.js
+++ src/blocks/_pro/post-list-slider/view.js
@@ -23,127 +23,64 @@ document.defaultView.addEventListener('load', function () {
 				sliderId = attributes.clientId;
 			}
 
-			let SwiperSetting = `
-			var swiper${index} = new Swiper ('.vk_post_list_slider-${sliderId}', {
-			`;
-
-			if (attributes.autoPlay) {
-				SwiperSetting += `
-				autoplay: {
-					delay: ${attributes.autoPlayDelay},
-					disableOnInteraction: ${attributes.autoPlayStop},
-					stopOnLastSlide: ${!attributes.loop},
-				},
-				`;
-			}
-
-			if (attributes.pagination !== 'hide') {
-				SwiperSetting += `
-				pagination: {
-					el: '.swiper-pagination',
-					clickable : true,
-					type: '${attributes.pagination}',
-					renderFraction: function (currentClass, totalClass) {
-						return '<span class="' + currentClass + '"></span>' + ' / ' + '<span class="' + totalClass + '"></span>';
-					},
-				},
-				`;
-			}
-
-			if (attributes.speed) {
-				SwiperSetting += `
-				speed: ${attributes.speed},
-				`;
-			}
-
-			if (attributes.effect !== 'fade') {
-				if (attributes.slidesPerViewMobile) {
-					SwiperSetting += `slidesPerView: ${attributes.slidesPerViewMobile},`;
-					if (
-						attributes.slidesPerGroup &&
-						attributes.slidesPerGroup === 'slides-per-view' &&
-						Number.isInteger(attributes.slidesPerViewMobile)
-					) {
-						SwiperSetting += `slidesPerGroup: ${attributes.slidesPerViewMobile},`;
-					} else {
-						SwiperSetting += `slidesPerGroup: 1,`;
+			// Swiper設定オブジェクトを組み立て
+			const config = {
+				autoplay: attributes.autoPlay
+					? {
+						delay: Number(attributes.autoPlayDelay) || 2500,
+						disableOnInteraction: !!attributes.autoPlayStop,
+						stopOnLastSlide: !attributes.loop,
 					}
-				} else if (attributes.slidesPerView) {
-					SwiperSetting += `slidesPerView: ${attributes.slidesPerView},`;
-					if (
-						attributes.slidesPerGroup &&
-						attributes.slidesPerGroup === 'slides-per-view' &&
-						Number.isInteger(attributes.slidesPerView)
-					) {
-						SwiperSetting += `slidesPerGroup: ${attributes.slidesPerView},`;
-					} else {
-						SwiperSetting += `slidesPerGroup: 1,`;
+					: false,
+				pagination: attributes.pagination !== 'hide'
+					? {
+						el: '.swiper-pagination',
+						clickable: true,
+						type: attributes.pagination,
+						renderFraction: function (currentClass, totalClass) {
+							return '<span class="' + currentClass + '"></span>' + ' / ' + '<span class="' + totalClass + '"></span>';
+						},
 					}
-				} else {
-					SwiperSetting += `slidesPerView: 1,`;
-					SwiperSetting += `slidesPerGroup: 1,`;
-				}
-				if (
-					attributes.slidesPerViewTablet ||
-					attributes.slidesPerViewPC
-				) {
-					// Responsive breakpoints
-					SwiperSetting += `breakpoints: {`;
-					if (attributes.slidesPerViewTablet) {
-						SwiperSetting += `576: {`;
-						SwiperSetting += `slidesPerView: ${attributes.slidesPerViewTablet},`;
-						if (
-							attributes.slidesPerGroup &&
-							attributes.slidesPerGroup === 'slides-per-view' &&
-							Number.isInteger(attributes.slidesPerViewTablet)
-						) {
-							SwiperSetting += `slidesPerGroup: ${attributes.slidesPerViewTablet},`;
-						}
-						SwiperSetting += `},`;
-					}
-					if (attributes.slidesPerViewPC) {
-						SwiperSetting += `992: {`;
-						SwiperSetting += `slidesPerView: ${attributes.slidesPerViewPC},`;
-						if (
-							attributes.slidesPerGroup &&
-							attributes.slidesPerGroup === 'slides-per-view' &&
-							Number.isInteger(attributes.slidesPerViewPC)
-						) {
-							SwiperSetting += `slidesPerGroup: ${attributes.slidesPerViewPC},`;
-						}
-						SwiperSetting += `},`;
-					}
-					SwiperSetting += `},`;
-				}
-				if (attributes.centeredSlides) {
-					SwiperSetting += `centeredSlides: ${attributes.centeredSlides},`;
-				}
-			}
-
-			if (attributes.loop) {
-				SwiperSetting += `
-				loop: ${attributes.loop},
-				`;
-			}
-
-			if (attributes.effect) {
-				SwiperSetting += `
-				effect: '${attributes.effect}',
-				`;
-			}
-
-			SwiperSetting += `
+					: undefined,
+				speed: Number(attributes.speed) || 500,
+				effect: typeof attributes.effect === 'string' ? attributes.effect : 'slide',
+				loop: !!attributes.loop,
 				navigation: {
 					nextEl: '.swiper-button-next',
 					prevEl: '.swiper-button-prev',
 				},
-			});`;
-			// eslint-disable-next-line no-eval
-			eval(SwiperSetting);
+			};
+			// breakpointsやcenteredSlidesなども必要に応じて追加
+			if (attributes.slidesPerViewMobile || attributes.slidesPerView || attributes.slidesPerViewTablet || attributes.slidesPerViewPC) {
+				config.slidesPerView = Number(attributes.slidesPerViewMobile) || Number(attributes.slidesPerView) || 1;
+				config.slidesPerGroup = 1;
+				if (attributes.slidesPerGroup && attributes.slidesPerGroup === 'slides-per-view') {
+					config.slidesPerGroup = config.slidesPerView;
+				}
+				if (attributes.slidesPerViewTablet || attributes.slidesPerViewPC) {
+					config.breakpoints = {};
+					if (attributes.slidesPerViewTablet) {
+						config.breakpoints[576] = {
+							slidesPerView: Number(attributes.slidesPerViewTablet),
+							slidesPerGroup: (attributes.slidesPerGroup === 'slides-per-view') ? Number(attributes.slidesPerViewTablet) : 1,
+						};
+					}
+					if (attributes.slidesPerViewPC) {
+						config.breakpoints[992] = {
+							slidesPerView: Number(attributes.slidesPerViewPC),
+							slidesPerGroup: (attributes.slidesPerGroup === 'slides-per-view') ? Number(attributes.slidesPerViewPC) : 1,
+						};
+					}
+				}
+			}
+			if (attributes.centeredSlides) {
+				config.centeredSlides = !!attributes.centeredSlides;
+			}
+			// Swiperインスタンスをwindow変数に格納
+			window[`swiper${index}`] = new Swiper(`.vk_post_list_slider-${sliderId}`, config);
 			// ページネーションがOFFの時非表示
-			if (attributes.pagination === 'hide') {
-				// eslint-disable-next-line no-eval
-				eval(`swiper${index}.pagination.destroy();`);
+			if (attributes.pagination === 'hide' && window[`swiper${index}`]?.pagination) {
+				window[`swiper${index}`].pagination.destroy();
 			}
 		}
 	}
